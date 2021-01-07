714af63655f3ceb1475426214947f0831888cf15
Adam Wright <adam.wright@wormbase.org>
2021-01-07 04:31:59 +0000

    got flask_restful working, logging, running server and curling PUT endpoint

--- README.md
+++ README.md
@@ -16,6 +16,38 @@ docker pull docker.elastic.co/elasticsearch/elasticsearch:7.10.1
 docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.10.1
 ```
 
-## RESTful
+# RESTful
 
 https://flask-restful.readthedocs.io/en/latest/quickstart.html
+
+# Development
+
+Setup development environment
+
+```bash
+python3 -m venv venv
+```
+
+Install Pipenv
+
+```bash
+pip3 install pipenv
+```
+
+## Running Server
+
+```bash
+pipenv run python src/main.py <port>
+```
+
+## Curling endpoint
+
+```bash
+curl http://localhost:5003/reference/PMID:4040 -d 'data={"title": "Worms abi-1"}' -X PUT
+```
+
+
+
+# Production
+
+Need to implement "waitress" server
--- src/main.py
+++ src/main.py
@@ -1,15 +1,42 @@
 import logging
+from os import path
+import sys, getopt
 
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+
+class Reference(Resource):
+    def get(self, reference_id):
+        return {'reference': reference_id}
+
+    def put(self, reference_id):
+        return {reference_id: request.form['data']}
 
 def main():
-    """  testing logger """
-    logger.info("starting")
+    """  starting app """
+    logger.info("Reading args")
 
+    api.add_resource(Reference, '/reference/<reference_id>')
+
+
+    app.run(host='localhost', port=args[2])
 
 if __name__ == "__main__":
     """ this should be in the program's main/start/run function """
     import logging.config
 
-    logging.config.fileConfig("logging.conf")
+    log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+    logging.config.fileConfig(log_file_path)
     logger = logging.getLogger(__name__)
+
+    try:
+      opts, args = getopt.getopt(sys.argv,"port:")
+    except getopt.GetoptError:
+      print('python src/main.py -p <port>')
+      sys.exit(2)
+
     main()
