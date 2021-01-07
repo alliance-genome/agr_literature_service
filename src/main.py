3f2b20124f40a24aae84f725cde61ee47244baf4
Adam Wright <adam.wright@wormbase.org>
2021-01-07 16:48:54 +0000

    got command line arguments for running server working

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import logging
 from os import path
-import sys, getopt
+import sys, argparse
 
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from waitress import serve
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,14 +17,16 @@ class Reference(Resource):
     def put(self, reference_id):
         return {reference_id: request.form['data']}
 
-def main():
+def main(args):
     """  starting app """
     logger.info("Reading args")
 
     api.add_resource(Reference, '/reference/<reference_id>')
 
-
-    app.run(host='localhost', port=args[2])
+    if args['prod']:
+       serve(app, host=args['ip_adress'], port=args['port'])
+    else:
+       app.run(host=args['ip_adress'], port=args['port'])
 
 if __name__ == "__main__":
     """ this should be in the program's main/start/run function """
@@ -33,10 +36,13 @@ if __name__ == "__main__":
     logging.config.fileConfig(log_file_path)
     logger = logging.getLogger(__name__)
 
-    try:
-      opts, args = getopt.getopt(sys.argv,"port:")
-    except getopt.GetoptError:
-      print('python src/main.py -p <port>')
-      sys.exit(2)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, help='Port to run the server on')
+    parser.add_argument('-i', '--ip-adress', type=str, help='IP address of the host', default='localhost', nargs='?')
+    parser.add_argument('--prod', help='Run WSGI server in production environment', action='store_true')
+    parser.add_argument('-v', dest='verbose', action='store_true')
 
-    main()
+    args = vars(parser.parse_args())
+
+
+    main(args)
