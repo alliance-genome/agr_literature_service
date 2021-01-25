9026eb6d07926b936ad57d36fd307d4eab658390
Adam Wright <adam.wright@wormbase.org>
2021-01-25 05:21:05 +0000

    getting it so that the db is the same one as the model is using

--- src/migrate.py
+++ src/migrate.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+from shared.models import db
+
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
-#app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
 
+db.init_app(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 
