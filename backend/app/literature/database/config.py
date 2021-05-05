0484c6287b27ebfefec981f7bf5e34344c3f3fda
Adam Wright <adam.wright@wormbase.org>
2021-05-05 15:08:52 +0000

    creating db url seperately

---
+++ backend/app/literature/database/config.py
@@ -0,0 +1,6 @@
+from literature.config import config
+
+SQLALCHEMY_DATABASE_URL = "postgresql://" \
+        + config.PSQL_USERNAME + ":" + config.PSQL_PASSWORD \
+        + "@" + config.PSQL_HOST + ":" + config.PSQL_PORT \
+        + "/" + config.PSQL_DATABASE
