c210a6445651b6193a821298bbe1acf0a4600dc9
Adam Wright <adam.wright@wormbase.org>
2021-09-30 19:55:23 +0000

    fixed typo number 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,7 +42,7 @@ from initialize import setup_resource_descriptor
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, help='Port to run the server on', default=8080, nargs='?')
-parser.add_argument('-i', '--ip-adress', type=str, help='IP address of the host', default='0.0.0.0', nargs='?')
+parser.add_argument('-i', '--ip-address', type=str, help='IP address of the host', default='0.0.0.0', nargs='?')
 parser.add_argument('-v', dest='verbose', action='store_true')
 
 args = vars(parser.parse_args())
