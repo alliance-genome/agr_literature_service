ee96ce12ec3a5a2bf08bdb5d037a9f3c1911f115
Juancarlos <azurebrd@its.caltech.edu>
2021-11-14 22:30:23 +0000

    flake8 fixes

--- src/xml_processing/xml_to_json.py
+++ src/xml_processing/xml_to_json.py
@@ -89,7 +89,7 @@ known_article_id_types = {
 #     'doi': {'pages': 'DOI', 'prefix': 'DOI:'},
 #     'pmc': {'pages': 'PMC', 'prefix': 'PMCID:'}}
 ignore_article_id_types = {'bookaccession', 'mid', 'pii', 'pmcid'}
-unknown_article_id_types = set() # type: Set
+unknown_article_id_types = set()   # type: Set
 
 
 def represents_int(s):
@@ -725,7 +725,7 @@ if __name__ == "__main__":
 
     # when iterating manually through list of PMIDs from PubMed XML CommentsCorrections, and wanting to exclude PMIDs that have already been looked at from original alliance DQM input, or previous iterations.
     previous_pmids = []
-    previous_pmids_files = [] # type: List
+    previous_pmids_files = []   # type: List
     # previous_pmids_files = ['inputs/alliance_pmids', 'inputs/comcor_add1', 'inputs/comcor_add2', 'inputs/comcor_add3']
     # previous_pmids_files = ['inputs/alliance_pmids', 'inputs/comcor_add1', 'inputs/comcor_add2', 'inputs/comcor_add3', 'inputs/comcor_add4', 'inputs/comcor_add5', 'inputs/comcor_add6', 'inputs/comcor_add7', 'inputs/comcor_add8', 'inputs/comcor_add9', 'inputs/comcor_add10', 'inputs/comcor_add11']
     for previous_pmids_file in previous_pmids_files:
