25108e27f19c88e44d39fa59558309bbcaa0430a
Juancarlos <azurebrd@its.caltech.edu>
2021-11-15 23:44:41 +0000

    load tests now check for explicit values from the github pubmed_xml/

--- src/xml_processing/tests/test_functional.py
+++ src/xml_processing/tests/test_functional.py
@@ -29,7 +29,9 @@ logger = logging.getLogger(__name__)
 #   docker run --rm --network=agr_literature_service_agr-literature -p 5432:5432 -v ${PWD}:/workdir -t -i 100225593120.dkr.ecr.us-east-1.amazonaws.com/agr_literature_dev:latest /bin/bash
 # python3 test_functional.py
 # 3 minutes on 34 references from 'inputs/sample_dqm_load.json'
-# based off of   pipenv run python functional_tests.py
+#
+# to run outside docker set PYTHONPATH to src/xml_processing/ directory and XML_PATH to src/xml_processing/tests/ directory
+# pipenv run python test_functional.py
 
 
 def test_load_references():
@@ -63,12 +65,16 @@ def test_load_references():
                         agr_found = True
                         agr = xref_ref[prefix][identifier]
         if not agr_found:
+            assert 'doi_conflict' in entry['load_check']
             continue
         if agr not in agr_wanted:
-            agr_wanted[agr] = set()
-        if 'check' in entry:
-            for check in entry['check']:
-                agr_wanted[agr].add(check)
+            agr_wanted[agr] = dict()
+        if 'load_check' in entry:
+            for check in entry['load_check']:
+                # for debugging
+                # json_data = json.dumps(entry['load_check'], indent=4, sort_keys=True)
+                # logger.info(json_data)
+                agr_wanted[agr][check] = entry['load_check'][check]
     api_port = environ.get('API_PORT')
     # counter = 0
     for agr in sorted(agr_wanted):
@@ -82,17 +88,15 @@ def test_load_references():
         #     break
         url = 'http://localhost:' + api_port + '/reference/' + agr
         logger.info("get AGR reference info from database %s", url)
-        # TODO
-        # put this back when database works
         get_return = requests.get(url)
         db_entry = json.loads(get_return.text)
         # logger.info(db_entry)
         for check in agr_wanted[agr]:
-            test_result = check_test(db_entry, check)
+            test_result = check_test(db_entry, check, agr_wanted[agr][check])
             logger.info("agr %s check %s result %s", agr, check, test_result)
 
 
-def erratum_check(agr_data):
+def erratum_check(agr_data, value):
     """
     future: check a database reference has comment_and_corrections connection to another reference
 
@@ -100,10 +104,11 @@ def erratum_check(agr_data):
     :return:
     """
 
+    # when comments and corrections loaded, check that an xref is made to value e.g. PMID:2 to PMID:8
     return 'Success: Errata references created, but connections not created yet, add to sample_reference_populate_load.sh later'
 
 
-def category_book_check(agr_data):
+def category_book_check(agr_data, value):
     """
     check a database reference has a category of 'book' from PubMed XML <BookDocument>
 
@@ -112,12 +117,13 @@ def category_book_check(agr_data):
     """
 
     if 'category' in agr_data:
-        if agr_data['category'] == 'book':
+        assert agr_data['category'] == value
+        if agr_data['category'] == value:
             return 'Success'
     return 'Failure'
 
 
-def title_check(agr_data):
+def title_check(agr_data, value):
     """
     check a database reference has a title
 
@@ -126,12 +132,13 @@ def title_check(agr_data):
     """
 
     if 'title' in agr_data:
-        if agr_data['title'] != '':
+        assert agr_data['title'] == value
+        if agr_data['title'] == value:
             return 'Success'
     return 'Failure'
 
 
-def author_name_check(agr_data):
+def author_name_check(agr_data, value):
     """
     check a database reference has all authors with names, because <CollectiveName> in PubMed XML is not standard author pattern
 
@@ -142,17 +149,23 @@ def author_name_check(agr_data):
     if 'authors' not in agr_data:
         return 'Failure: No authors found'
     result = 'Success'
+    has_specific_value = False
     for author in agr_data['authors']:
         has_name = False
         if 'name' in author:
             if author['name'] != '':
                 has_name = True
+            if author['name'] == value:
+                has_specific_value = True
         if not has_name:
             result = 'Failure'
+    assert has_specific_value is True
+    if not has_specific_value:
+        result = 'Failure'
     return result
 
 
-def author_affiliation_check(agr_data):
+def author_affiliation_check(agr_data, value):
     """
     check a database reference has an author with an affiliation
 
@@ -160,17 +173,19 @@ def author_affiliation_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'authors' not in agr_data:
         return 'Failure: No authors found'
     for author in agr_data['authors']:
-        if 'affiliation' in author:
+        if 'affiliation' in author and author['affiliation'] is not None:
             for affiliation in author['affiliation']:
-                if affiliation != '':
-                    return 'Success'
-    return 'Failure'
+                if affiliation == value:
+                    result = 'Success'
+    assert result == 'Success'
+    return result
 
 
-def author_orcid_check(agr_data):
+def author_orcid_check(agr_data, value):
     """
     check a database reference has an author with an orcid
 
@@ -178,17 +193,19 @@ def author_orcid_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'authors' not in agr_data:
-        return 'Failure: No authors found'
+        result = 'Failure: No authors found'
     for author in agr_data['authors']:
-        if 'orcid' in author:
+        if 'orcid' in author and author['orcid'] is not None:
             if 'curie' in author['orcid']:
-                if author['orcid']['curie'] != '':
-                    return 'Success'
-    return 'Failure'
+                if author['orcid']['curie'] == value:
+                    result = 'Success'
+    assert result == 'Success'
+    return result
 
 
-def xref_mods_check(agr_data):
+def xref_mods_check(agr_data, mods):
     """
     check a database reference has cross_references to six base mods
 
@@ -196,19 +213,27 @@ def xref_mods_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     prefixes = set()
     if 'cross_references' in agr_data:
         for xref in agr_data['cross_references']:
             if 'curie' in xref:
                 prefix, identifier, separator = split_identifier(xref['curie'])
                 prefixes.add(prefix)
-        if 'WB' in prefixes and 'MGI' in prefixes and 'FB' in prefixes and 'ZFIN' in prefixes and 'RGD' in prefixes and 'MGI' in prefixes:
-            return 'Success'
-    prefixes_found = ', '.join(sorted(prefixes))
-    return 'Failure: only found ' + prefixes_found
+    all_mods_found = True
+    for mod in mods:
+        if mod not in prefixes:
+            all_mods_found = False
+    if all_mods_found:
+        result = 'Success'
+    else:
+        prefixes_found = ', '.join(sorted(prefixes))
+        result = 'Failure: only found ' + prefixes_found
+    assert result == 'Success'
+    return result
 
 
-def html_abstract_check(agr_data):
+def html_abstract_check(agr_data, value):
     """
     future: check a database reference does not have html in the abstract
 
@@ -216,10 +241,14 @@ def html_abstract_check(agr_data):
     :return:
     """
 
+    if 'abstract' in agr_data:
+        assert agr_data['abstract'] == value
+        if agr_data['abstract'] == value:
+            return 'Success: Expected abstract value.  Abstracts do have html, have not decided whether they should not'
     return 'Success: Abstracts do have html, have not decided whether they should not'
 
 
-def html_doi_check(agr_data):
+def html_doi_check(agr_data, value):
     """
     check a database reference does not have html in the doi
 
@@ -227,17 +256,21 @@ def html_doi_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'cross_references' in agr_data:
         for xref in agr_data['cross_references']:
             if 'curie' in xref:
                 prefix, identifier, separator = split_identifier(xref['curie'])
+                if xref['curie'] == value:
+                    result = 'Success'
                 if prefix == 'DOI':
                     if re.search('&', xref['curie']):
-                        return 'Failure: ' + xref['curie'] + ' has &, could be html in doi'
-    return 'Success'
+                        result = 'Failure: ' + xref['curie'] + ' has &, could be html in doi'
+    assert result == 'Success'
+    return result
 
 
-def keywords_check(agr_data):
+def keywords_check(agr_data, value):
     """
     check a database reference does have a keyword
 
@@ -245,14 +278,16 @@ def keywords_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'keywords' in agr_data:
         for keyword in agr_data['keywords']:
-            if keyword != '':
-                return 'Success'
-    return 'Failure'
+            if keyword == value:
+                result = 'Success'
+    assert result == 'Success'
+    return result
 
 
-def has_doi_check(agr_data):
+def has_doi_check(agr_data, value):
     """
     check a database reference does have a doi
 
@@ -260,16 +295,17 @@ def has_doi_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'cross_references' in agr_data:
         for xref in agr_data['cross_references']:
             if 'curie' in xref:
-                prefix, identifier, separator = split_identifier(xref['curie'])
-                if prefix == 'DOI':
-                    return 'Success'
-    return 'Failure'
+                if xref['curie'] == value:
+                    result = 'Success'
+    assert result != 'Failure'
+    return result
 
 
-def has_pmid_check(agr_data):
+def has_pmid_check(agr_data, value):
     """
     check a database reference does have a pmid
 
@@ -277,16 +313,17 @@ def has_pmid_check(agr_data):
     :return:
     """
 
+    result = 'Failure'
     if 'cross_references' in agr_data:
         for xref in agr_data['cross_references']:
             if 'curie' in xref:
-                prefix, identifier, separator = split_identifier(xref['curie'])
-                if prefix == 'PMID':
-                    return 'Success: PMID found, PubMed XML might not have it'
-    return 'Failure'
+                if xref['curie'] == value:
+                    result = 'Success: PMID found, PubMed XML might not have it'
+    assert result != 'Failure'
+    return result
 
 
-def no_pmid_check(agr_data):
+def no_pmid_check(agr_data, value):
     """
     check a database reference does not have a pmid
 
@@ -294,16 +331,18 @@ def no_pmid_check(agr_data):
     :return:
     """
 
+    result = 'Success'
     if 'cross_references' in agr_data:
         for xref in agr_data['cross_references']:
             if 'curie' in xref:
                 prefix, identifier, separator = split_identifier(xref['curie'])
                 if prefix == 'PMID':
-                    return 'Failure: PMID found, this reference should not have one'
-    return 'Success'
+                    result = 'Failure: PMID found, this reference should not have one'
+    assert result == 'Success'
+    return result
 
 
-def check_test(agr_data, check):
+def check_test(agr_data, check, value):
     """
     case switch for different types of tests
 
@@ -331,7 +370,7 @@ def check_test(agr_data, check):
         'FAIL': title_check
     }
     if check in options:
-        test_result = options[check](agr_data)
+        test_result = options[check](agr_data, value)
         return test_result
     else:
         return 'test not found'
