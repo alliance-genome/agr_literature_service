98eca84342f21ec3d7e8114a260bdfad29e24f8b
ianlongden <ianlongden@morgan.harvard.edu>
2022-02-09 10:51:14 -0500

    docker version does not use pipenv and logs go to /logs now

--- src/xml_processing/sample_reference_populate_load.sh
+++ src/xml_processing/sample_reference_populate_load.sh
@@ -1,19 +1,15 @@
 # 22 seconds to call this shell script
 
-# outside docker, main run
-# export XML_PATH=/home/azurebrd/git/dqm_updates/agr_literature_service/src/xml_processing/
-# remove -s from process_many_pmids_to_json.py
-#
-# outside docker, tests run
-export XML_PATH=/home/azurebrd/git/dqm_updates/agr_literature_service/src/xml_processing/tests/
-
-# inside docker, tests run
-# export XML_PATH=/workdir/src/xml_processing/tests/
-
-pipenv run python3 parse_dqm_json_reference.py -p -d ./ -f dqm_load_sample/ > log_parse_dqm_json_reference_load_pmid_list
-pipenv run python3 process_many_pmids_to_json.py -s -f inputs/alliance_pmids > log_process_many_pmids_to_json_load
-pipenv run python3 parse_dqm_json_reference.py -d ./ -f dqm_load_sample/ -m all > log_parse_dqm_json_reference_load_sanitize
-pipenv run python3 parse_pubmed_json_reference.py -f inputs/pubmed_only_pmids > log_parse_pubmed_json_reference_load
-
-pipenv run python3 post_reference_to_api.py > log_post_reference_to_api_load         # 16 seconds
-pipenv run python3 post_comments_corrections_to_api.py -f inputs/all_pmids > log_post_comments_corrections_to_api_load
+echo $PWD
+echo "Running pqjr" > /logs/srpl.log
+python3 ./src/xml_processing/parse_dqm_json_reference.py -p -d ./ -f dqm_load_sample/ > /logs/log_parse_dqm_json_reference_load_pmid_list
+echo "Running pmptj"
+python3 ./src/xml_processing/process_many_pmids_to_json.py -s -f inputs/alliance_pmids > /logs/log_process_many_pmids_to_json_load
+echo "Running pdjr"
+python3 ./src/xml_processing/parse_dqm_json_reference.py -d ./ -f dqm_load_sample/ -m all > /logs/log_parse_dqm_json_reference_load_sanitize
+echo "Running ppjr"
+python3 ./src/xml_processing/parse_pubmed_json_reference.py -f inputs/pubmed_only_pmids > /logs/log_parse_pubmed_json_reference_load
+echo "Running prta"
+python3 ./src/xml_processing/post_reference_to_api.py > /logs/log_post_reference_to_api_load         # 16 seconds
+echo "Running pccta"
+python3 ./src/xml_processing/post_comments_corrections_to_api.py -f inputs/all_pmids > /logs/log_post_comments_corrections_to_api_load
