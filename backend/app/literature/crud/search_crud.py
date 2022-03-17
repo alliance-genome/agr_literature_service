7d9df8c6f88a7c608d371ae1b8b2fc2f82a7be72
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-03-16 20:02:04 -0700

    feat(docker-compose): updated search query on title

--- backend/app/literature/crud/search_crud.py
+++ backend/app/literature/crud/search_crud.py
@@ -4,8 +4,13 @@ from literature.config import config
 
 def show(query):
     es_host = config.ELASTICSEARCH_HOST
-    if not (es_host.startswith('http://') or es_host.startswith('https://')):
-        es_host = ('http://' if config.ELASTICSEARCH_PORT != "443" else 'https://') + es_host
     es = Elasticsearch(hosts=es_host + ':' + config.ELASTICSEARCH_PORT)
-    res = es.search(index="references_index", q=query)
+    res = es.search(index="references_index",
+                    body={
+                        "query": {
+                            "match": {
+                                "title": query
+                            }
+                        }
+                    })
     return [{'curie': ref['_source']['curie'], 'title': ref['_source']['title']} for ref in res['hits']['hits']]
