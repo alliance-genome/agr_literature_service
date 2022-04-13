0bb498006ecd235eb3ac267745d219e474a87c11
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-04-13 14:40:53 +0200

    fix(xml_processing): fixed function to retrieve associations between PMIDs and a specified MOD via mod-corpus associations
    - fixed bug with references with multiple mod-corpus associations: do not set mod value to None when mod association to specified mod is present but comes before associations to other mods in the list returned by the query

--- src/xml_processing/query_pubmed_mod_updates.py
+++ src/xml_processing/query_pubmed_mod_updates.py
@@ -162,8 +162,10 @@ def get_pmid_association_to_mod_via_reference(pmids: List[str], mod_abbreviation
         ModCorpusAssociationModel.mod
     )
     results = query.all()
-    pmid_curie_mod_dict = {result[0]: (result[1], result[2] if result[2] == mod_abbreviation else None)
-                           for result in results}
+    pmid_curie_mod_dict = {}
+    for result in results:
+        if result[0] not in pmid_curie_mod_dict or pmid_curie_mod_dict[result[0]][1] is None:
+            pmid_curie_mod_dict[result[0]] = (result[1], result[2] if result[2] == mod_abbreviation else None)
     for pmid in pmids:
         if pmid not in pmid_curie_mod_dict:
             pmid_curie_mod_dict[pmid] = (None, None)
