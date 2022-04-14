2046eb8df88a313870a13be25475ed39e0cdc89a
Juancarlos <azurebrd@its.caltech.edu>
2022-04-14 07:16:00 +0000

    Previous logger settings were overriding logger on query_pubmed_mod_updates.py, I don't really know why

--- src/xml_processing/helper_s3.py
+++ src/xml_processing/helper_s3.py
@@ -3,6 +3,7 @@
 
 
 from os import path
+import sys
 import logging
 import logging.config
 import boto3
@@ -12,12 +13,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-log_file_path = path.join(path.dirname(path.abspath(__file__)), '../logging.conf')
-logging.config.fileConfig(log_file_path)
-logger = logging.getLogger('literature logger')
-logging.getLogger("s3transfer.utils").setLevel(logging.WARNING)
-logging.getLogger("s3transfer.tasks").setLevel(logging.WARNING)
-logging.getLogger("s3transfer.futures").setLevel(logging.WARNING)
+# log_file_path = path.join(path.dirname(path.abspath(__file__)), '../logging.conf')
+# logging.config.fileConfig(log_file_path)
+# logger = logging.getLogger('literature logger')
+# logging.getLogger("s3transfer.utils").setLevel(logging.WARNING)
+# logging.getLogger("s3transfer.tasks").setLevel(logging.WARNING)
+# logging.getLogger("s3transfer.futures").setLevel(logging.WARNING)
+
+logging.basicConfig(level=logging.INFO,
+                    stream=sys.stdout,
+                    format= '%(asctime)s - %(levelname)s - {%(module)s %(funcName)s:%(lineno)d} - %(message)s',    # noqa E251
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 def upload_file_to_s3(filepath, bucketname, s3_file_location):
