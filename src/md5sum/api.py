36c071e65acd8a23da132f53cbeb88700ead0b6c
Paulo Nuin <nuin@genedrift.org>
2022-04-17 16:11:34 -0600

    api serving dqm changes

--- src/md5sum/api.py
+++ src/md5sum/api.py
@@ -3,6 +3,23 @@ api.py - API for tthe md5sum comparisons
 
 Paulo Nuin Apr 2022
 
+Redis DBs
+
+XML/JSON
+0 - XML/JSON md5sum "old" files
+1 - XML/JSON md5sum "new" files
+10 - XML/JSON md5sum new and changed files
+
+DQM
+11 - FB
+12 - MGD
+13 - RGD
+14 - SGD
+15 - WB
+16 - ZFIN
+17 - GO
+18 - XB
+
 """
 
 
@@ -70,6 +87,33 @@ def get_all_changes():
     :return:
     """
     r = redis.StrictRedis(host='localhost', port=6379, db=10, password="password", decode_responses=True)
+    keys = r.keys()
+    data = {}
+    for key in keys:
+        data[key] = r.get(key)
+
+    return json.dumps(data)
+
+
+@api.route("/dqm_changes/<string:mod>", methods=['GET'])
+def get_dqm_changes(mod):
+
+    if mod == "fb":
+        db = 11
+    elif mod == "mgd":
+        db = 12
+    elif mod == "rgd":
+        db = 13
+    elif mod == "sgd":
+        db = 14
+    elif mod == "wb":
+        db = 15
+    elif mod == "zfin":
+        db = 16
+    elif mod == "xb":
+        db = 18
+
+    r = redis.StrictRedis(host='localhost', port=6379, db=db, password="password", decode_responses=True)
     keys = r.keys()
     data = {}
     for key in keys:
