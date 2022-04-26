0f27bbc3696e326d4d8a117325bc8cac915ac790
ianlongden <ianlongden@morgan.harvard.edu>
2022-04-26 09:42:35 -0400

    routers removed

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,8 @@ from literature.database.config import SQLALCHEMY_DATABASE_URL
 from literature.database.main import is_database_online
 from literature.routers import (author_router, bulk_downloads_router,
                                 cross_reference_router, database_router,
-                                editor_router, file_router, mesh_detail_router,
-                                mod_reference_type_router, note_router,
+                                editor_router, mesh_detail_router,
+                                mod_reference_type_router,
                                 mod_router, mod_corpus_association_router,
                                 reference_automated_term_tag_router,
                                 reference_comment_and_correction_router,
@@ -83,11 +83,11 @@ app.include_router(author_router.router)
 app.include_router(editor_router.router)
 app.include_router(cross_reference_router.router)
 app.include_router(resource_descriptor_router.router)
-app.include_router(file_router.router)
+# app.include_router(file_router.router)
 app.include_router(mesh_detail_router.router)
 app.include_router(mod_reference_type_router.router)
 # app.include_router(person_router.router)
-app.include_router(note_router.router)
+# app.include_router(note_router.router)
 app.include_router(database_router.router)
 app.include_router(reference_comment_and_correction_router.router)
 app.include_router(reference_automated_term_tag_router.router)
