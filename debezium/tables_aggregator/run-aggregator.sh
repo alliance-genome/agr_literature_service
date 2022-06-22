3862b9eb540d433b5448222c69d1ad3b7fbcbcc0
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-06-22 10:14:57 -0700

    feat(debezium)!: added ksql queries and example on how to send them to ksqldb server
    Still need to connect elasticsearch to ksql generated kafka topic

--- debezium/tables_aggregator/run-aggregator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PATH="/opt/tables_aggregator/jdk/bin:${PATH}"
-export JAVA_APP_DIR=/opt/tables_aggregator/lib
-export JAVA_MAIN_CLASS=io.debezium.examples.aggregation.StreamingAggregatesDDDLit
-
-exec /opt/tables_aggregator/run-java.sh "$PARENT_TOPIC" "$CHILDREN_TOPIC" "$BOOTSTRAP_SERVERS"
