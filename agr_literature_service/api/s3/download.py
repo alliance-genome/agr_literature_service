eefb74022e22db998377537ea4454f833ca58b5b
sweng66 <sweng@stanford.edu>
2022-06-29 20:10:47 -0700

    fixed flake8 error

--- agr_literature_service/api/s3/download.py
+++ agr_literature_service/api/s3/download.py
@@ -1,4 +1,3 @@
-import logging
 from botocore.exceptions import ClientError
 from fastapi import HTTPException
 from fastapi.encoders import jsonable_encoder
@@ -6,7 +5,6 @@ from os import environ, getcwd
 from agr_literature_service.lit_processing.helper_s3 import download_file_from_s3
 from fastapi.responses import FileResponse
 
-logger = logging.getLogger(__name__)
 
 def download_file_from_bucket(s3_client, bucket, folder, object_name=None):
     """Upload a file to an S3 bucket
@@ -37,9 +35,14 @@ def get_json_file_from_s3(mod):
     s3_file_location = env_state + '/reference/dumps/latest/' + json_file
 
     try:
+        # not sure where to put the file downloaded from s3 in the docker container
+        # so just put it under current directory
+        # should we set a place outside docker container to store these files?
+        # so we can easily clean the directoty up (by cronjob or other method)
         download_file_from_s3(json_file, bucketname, s3_file_location)
     except Exception as e:
-        logger.info(f"Error occurred when retrieving the json file from s3 {s3_file_location}")
+        raise HTTPException(status_code=e.response['Error']['Code'],
+                            detail=f"Error occurred when retrieving the json file from s3 {s3_file_location}")
 
     file_path = getcwd() + "/" + json_file
     return FileResponse(path=file_path, filename=json_file, media_type='application/json')
