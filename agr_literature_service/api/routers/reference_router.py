0c3f0493d29ca58b5a62a94685b20d2c264e70c9
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-07-18 21:22:00 -0700

    feat(search)!: using multiprocessing to run dumps on demand - not tested!

--- agr_literature_service/api/routers/reference_router.py
+++ agr_literature_service/api/routers/reference_router.py
@@ -1,9 +1,10 @@
-import threading
+import ctypes
 from os import environ
 from fastapi import (APIRouter, Depends, HTTPException, Response,
                      Security, status)
 from fastapi_okta import OktaUser
 from sqlalchemy.orm import Session
+from multiprocessing import Process, Manager, Lock
 
 from agr_literature_service.api import database
 from agr_literature_service.api.crud import cross_reference_crud, reference_crud
@@ -19,6 +20,7 @@ import logging
 from agr_literature_service.lit_processing.process_single_pmid import process_pmid
 from agr_literature_service.lit_processing.dump_json_data import dump_data
 
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter(
@@ -31,6 +33,8 @@ db_session: Session = Depends(get_db)
 db_user = Security(auth.get_user)
 s3_session = Depends(s3_auth)
 
+running_processes_dumps_ondemand = Manager().dict()
+lock_dumps_ondemand = Lock()
 
 @router.post('/',
              status_code=status.HTTP_201_CREATED,
@@ -95,6 +99,18 @@ def download_data_by_filename(filename: str,
     return download.get_json_file(None, filename)
 
 
+def dump_data_process_wrapper(running_processes_dict, lock: Lock, mod: str, email: str, ondemand: int, api_url: str):
+    dump_data(mod=mod, email=email, ondemand=ondemand, api_url=api_url)
+    try:
+        lock.acquire()
+        if '@' not in email:
+            email = 'sweng@stanford.edu'
+        process_name = mod + "|" + email
+        del running_processes_dict[process_name]
+    finally:
+        lock.release()
+
+
 @router.get('/dumps/ondemand/{mod}',
             status_code=200)
 def generate_data_ondemand(mod: str,
@@ -102,26 +118,32 @@ def generate_data_ondemand(mod: str,
                            db: Session = db_session):
 
     set_global_user_id(db, user.id)
-
-    my_threads = []
-    for t in threading.enumerate():
-        my_threads.append(t.name)
-
     email = get_global_user_id()
 
     # for testing purpose
     if '@' not in email:
         email = 'sweng@stanford.edu'
 
-    thread_name = mod + "|" + email
-    if thread_name in my_threads:
-        return {"message": "You have already submitted a request for generating " + mod + " Reference json file so no need to submit again."}
-
-    t = threading.Thread(daemon=True, target=dump_data(mod, email, 1, environ.get('API_URL')))
-    threading.current_thread().name = thread_name
-    t.start()
-
-    return {"message": "Generating " + mod + " Reference json file now. An email will be sent to you shortly when the json file is ready."}
+    process_name = mod + "|" + email
+    try:
+        lock_dumps_ondemand.acquire()
+        if process_name in running_processes_dumps_ondemand:
+            return {
+                "message": "You have already submitted a request for generating " + mod +
+                           " Reference json file so no need to submit again."
+            }
+        else:
+            running_processes_dumps_ondemand[process_name] = 1
+            p = Process(target=dump_data_process_wrapper,
+                        args=(running_processes_dumps_ondemand, lock_dumps_ondemand, mod, email, 1,
+                              environ.get('API_URL')))
+            p.start()
+            return {
+                "message": "Generating " + mod +
+                           " Reference json file now. An email will be sent to you shortly when the json file is ready."
+            }
+    finally:
+        lock_dumps_ondemand.release()
 
 
 @router.get('/by_cross_reference/{curie:path}',
