af7d7090af850265b979b1b98c405429624bff66
sweng66 <sweng@stanford.edu>
2022-07-19 09:27:25 -0700

    changed limit from 1000 to 500

--- agr_literature_service/lit_processing/update_pubmed_papers.py
+++ agr_literature_service/lit_processing/update_pubmed_papers.py
@@ -13,7 +13,6 @@ from agr_literature_service.api.models import CrossReferenceModel, ReferenceMode
 from agr_literature_service.lit_processing.helper_sqlalchemy import create_postgres_session
 from agr_literature_service.lit_processing.update_resource_pubmed_nlm import update_resource_pubmed_nlm
 from agr_literature_service.lit_processing.get_pubmed_xml import download_pubmed_xml
-### fix here
 from agr_literature_service.lit_processing.xml_to_json import generate_json
 from agr_literature_service.lit_processing.filter_dqm_md5sum import load_s3_md5data
 from agr_literature_service.lit_processing.helper_s3 import upload_xml_file_to_s3
