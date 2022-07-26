6c88ee19ecc688abfee4a39c481d0bec5306eefc
ianlongden <ianlongden@morgan.harvard.edu>
2022-07-26 12:03:05 -0400

    fixes

--- agr_literature_service/api/routers/refererence_ontology_router.py
+++ agr_literature_service/api/routers/refererence_ontology_router.py
@@ -24,7 +24,7 @@ db_user = Security(auth.get_user)
 @router.post('/',
              status_code=status.HTTP_201_CREATED,
              response_model=str)
-def create(request: ReferenceOntologySchemaPost,
+def create(request: ReferenceOntologySchemaUpdate,
            user: OktaUser = db_user,
            db: Session = db_session):
     set_global_user_id(db, user.id)
