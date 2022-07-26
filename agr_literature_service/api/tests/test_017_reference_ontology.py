2428d2b647f76ddfab8f290faa6ceb336165fb55
ianlongden <ianlongden@morgan.harvard.edu>
2022-07-26 14:00:03 -0400

    changeset tests

--- agr_literature_service/api/tests/test_017_reference_ontology.py
+++ agr_literature_service/api/tests/test_017_reference_ontology.py
@@ -28,8 +28,10 @@ Base.metadata.create_all(engine)
 if "literature-test" not in SQLALCHEMY_DATABASE_URL:
     exit(-1)
 
+fb_mod = None
 
-def test_initilaise():
+def test_initialise():
+    global fb_mod
     # add User "017 Bob"
     user_create(db, "017_Bob")
 
@@ -39,7 +41,7 @@ def test_initilaise():
         "short_name": "017_FB",
         "full_name": "017_ont_1"
     }
-    mod_create(db, data)
+    fb_mod = mod_create(db, data)
 
     data = {
         "abbreviation": '017_RGD',
@@ -78,6 +80,7 @@ def test_bad_missing_args():
 
 
 def test_create_ref_ont():
+    global fb_mod
     xml = {'reference_curie': "AGR:AGR-Reference-0000000001",
            'mod_abbreviation': "017_FB",
            'ontology_id': "ont1",
@@ -92,7 +95,7 @@ def test_create_ref_ont():
         filter(ReferenceModel.curie == "AGR:AGR-Reference-0000000001").one()
     assert ref_ont_obj.ontology_id == "ont1"
     assert ref_ont_obj.created_by == "017_Bob"
-    assert ref_ont_obj.mod.abbreviation == "017_FB"
+    assert ref_ont_obj.mod_id == fb_mod
 
 
 def test_patch_ref_ont():
@@ -136,12 +139,12 @@ def test_changesets():
 
     for transaction in res:
         print("Test changesets 017: {}".format(transaction))
-        if not transaction['changeset']['reference_ontology_id'][0]:
+        if 'reference_ontology_id' in transaction['changeset']:
             assert transaction['changeset']['reference_ontology_id'][1] == 3
             assert transaction['changeset']['mod_id'][1] == 5
         else:
-            assert transaction['changeset']['reference_ontology_id'][1] == 3
-            assert transaction['changeset']['mod_id'][1] == 1
+            assert transaction['changeset']['ontology_id'][0] == 'ont1'
+            assert transaction['changeset']['ontology_id'][1] == 'ont test patch'
 
 
 def test_destroy_ref_ont():
