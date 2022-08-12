69f7b9f19dfb2c9272c4ec263bba2962224d4ed5
sweng66 <sweng@stanford.edu>
2022-08-12 14:18:02 -0700

    adding a new source

--- agr_literature_service/api/schemas/mod_corpus_sort_source_type.py
+++ agr_literature_service/api/schemas/mod_corpus_sort_source_type.py
@@ -5,4 +5,6 @@ class ModCorpusSortSourceType(str, Enum):
     Mod_pubmed_search = "mod_pubmed_search"
     Dqm_files = "dqm_files"
     Manual_creation = "manual_creation"
+    Automated_alliance = "automated_alliance"
     Assigned_for_review = "assigned_for_review"
+    
