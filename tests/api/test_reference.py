7c7e28ce2512d2e7c4a3f687d1e20446282e5b32
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-08-30 11:37:10 -0700

    feat(tests): finished rewriting reference unit tests with fastapi test framework

--- tests/api/test_reference.py
+++ tests/api/test_reference.py
@@ -8,7 +8,7 @@ from fastapi import status
 from agr_literature_service.api.database.base import Base
 from agr_literature_service.api.database.config import SQLALCHEMY_DATABASE_URL
 from agr_literature_service.api.main import app
-from agr_literature_service.api.models import ReferenceModel, initialize
+from agr_literature_service.api.models import ReferenceModel, initialize, AuthorModel, CrossReferenceModel
 from agr_literature_service.api.schemas import ReferenceSchemaPost
 from agr_literature_service.lit_processing.helper_post_to_api import get_authentication_token, generate_headers
 
@@ -94,198 +94,174 @@ class TestReference:
             assert added_ref["abstract"] == '3'
 
             # Lookup 1 that does not exist
-
             res = client.get(url="/reference/does_not_exist")
             assert res.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_update_reference(self, auth_headers, create_test_reference):
+    def test_update_reference(self, db, auth_headers, create_test_reference):
         with TestClient(app) as client:
 
             # patch docs says it needs a ReferenceSchemaUpdate
             # but does not work with this.
             # with pytest.raises(AttributeError):
+            created_ref_curie = create_test_reference.json()
             updated_fields = {"title": "new title", "category": "book", "language": "New"}
-            response = client.patch(url=f"/reference/{create_test_reference.json()}", json=updated_fields, headers=auth_headers)
+            response = client.patch(url=f"/reference/{created_ref_curie}", json=updated_fields, headers=auth_headers)
             assert response.status_code == status.HTTP_202_ACCEPTED
-            updated_ref = client.get(url=f"/reference/{create_test_reference.json()}").json()
+            response = client.post(url=f"/reference/citationupdate/{created_ref_curie}", headers=auth_headers)
+            assert response.status_code == status.HTTP_201_CREATED
+            updated_ref = client.get(url=f"/reference/{created_ref_curie}").json()
             assert updated_ref["title"] == "new title"
             assert updated_ref["category"] == "book"
             assert updated_ref["language"] == "New"
+            assert updated_ref["abstract"] == "3"
+            # Do we have a new citation
+            assert updated_ref["citation"] == ", () new title.   (): "
 
-            # Update the citation
+    def test_changesets(self, db, create_test_reference, auth_headers):
+        with TestClient(app) as client:
+            created_ref_curie = create_test_reference.json()
+            # title            : None -> bob -> 'new title'
+            # catergory        : None -> thesis -> book
+            updated_fields = {"title": "new title", "category": "book", "language": "New"}
+            client.patch(url=f"/reference/{created_ref_curie}", json=updated_fields, headers=auth_headers)
+            client.post(url=f"/reference/citationupdate/{created_ref_curie}", headers=auth_headers)
+            response = client.get(url=f"/reference/{created_ref_curie}/versions")
+            transactions = response.json()
+            assert transactions[0]['changeset']['curie'][1] == created_ref_curie
+            assert transactions[0]['changeset']['title'][1] == "Bob"
+            assert transactions[0]['changeset']['category'][1] == "thesis"
+            assert transactions[1]['changeset']['title'][1] == "new title"
+            assert transactions[1]['changeset']['category'][1] == "book"
+            assert transactions[2]['changeset']['citation'][0] == ", () Bob.   (): "
+            assert transactions[2]['changeset']['citation'][1] == ", () new title.   (): "
 
-            # update_citation(db, refs[0])
-            #
-            # # fetch the new record.
-            # res = show(db, refs[0])
-            #
-            # # do we have the new title?
-            # assert res['title'] == "new title"
-            #
-            # # do we have the new title?
-            # assert res['category'] == "book"
-            #
-            # # language changed
-            # assert res['language'] == "New"
-            #
-            # # NOTE: abstract set to None as it was not in the update and
-            # #       schemaupdate sets all items not listed to default values.
-            # #       In this case abstract is None
-            # assert res['abstract'] is None
-            #
-            # # Do we have a new citation
-            # assert res['citation'] == ", () new title.   (): "
+    def test_delete_reference(self, db, auth_headers, create_test_reference):
+        with TestClient(app) as client:
+            created_ref_curie = create_test_reference.json()
+            delete_response = client.delete(url=f"/reference/{created_ref_curie}", headers=auth_headers)
+            assert delete_response.status_code == status.HTTP_204_NO_CONTENT
+            get_response = client.get(url=f"/reference/{created_ref_curie}")
+            assert get_response.status_code == status.HTTP_404_NOT_FOUND
+            delete_response = client.delete(url=f"/reference/{created_ref_curie}", headers=auth_headers)
+            assert delete_response.status_code == status.HTTP_404_NOT_FOUND
 
-    # def test_changesets():
-    #     res = show_changesets(db, refs[0])
-    #
-    #     # title            : None -> bob -> 'new title'
-    #     # catergory        : None -> thesis -> book
-    #     for i, transaction in enumerate(res):
-    #         print(transaction)
-    #         if i == 0:
-    #             assert transaction['changeset']['reference_id'][1] == 1
-    #             assert transaction['changeset']['title'][1] == "Bob 001 1"
-    #             assert transaction['changeset']['category'][1] == "thesis"
-    #         elif i == 1:
-    #             assert transaction['changeset']['title'][1] == "new title"
-    #             assert transaction['changeset']['category'][1] == "book"
-    #         else:
-    #             assert transaction['changeset']['citation'][0] == ", () Bob 001 1.   (): "
-    #             assert transaction['changeset']['citation'][1] == ", () new title.   (): "
-    #
-    # def test_delete_Reference():
-    #     global refs
-    #
-    #     destroy(db, refs[1])
-    #
-    #     # It should now give an error on lookup.
-    #     with pytest.raises(HTTPException):
-    #         show(db, refs[1])
-    #
-    #     # Deleting it again should give an error as the lookup will fail.
-    #     with pytest.raises(HTTPException):
-    #         destroy(db, refs[1])
-    #
-    # def test_reference_large():
-    #     full_xml = {
-    #         "category": "research_article",
-    #         "abstract": "The Hippo (Hpo) pathway is a conserved tumor suppressor pathway",
-    #         "authors": [
-    #             {
-    #                 "order": 2,
-    #                 "first_name": "S.",
-    #                 "last_name": "Wu",
-    #                 "name": "S. Wu",
-    #                 # "reference_id": "PMID:23524264"
-    #             },
-    #             {
-    #                 "order": 1,
-    #                 "first_name": "D.",
-    #                 "last_name": "Wu",
-    #                 "name": "D. Wu",
-    #                 # "reference_id": "PMID:23524264"
-    #             }
-    #         ],
-    #         "mesh_terms": [
-    #             {
-    #                 "heading_term": "hterm",
-    #                 "qualifier_term": "qterm"
-    #             }
-    #         ],
-    #         "mod_reference_types": [
-    #             {
-    #                 "reference_type": "mrt_rt",
-    #                 "source": "mrt_s"
-    #             }
-    #         ],
-    #         "cross_references": [
-    #             {
-    #                 "curie": "FB:FBrf0221304",
-    #                 "pages": [
-    #                     "reference"
-    #                 ]
-    #             }
-    #         ],
-    #         "workflow_tags": [
-    #             {
-    #                 "workflow_tag_id": "workflow_tag1",
-    #                 "mod_abbreviation": "001_FB",
-    #                 "created_by": "001_Bob"
-    #             },
-    #             {
-    #                 "workflow_tag_id": "workflow_tag2",
-    #                 "mod_abbreviation": "001_RGD",
-    #                 "created_by": "001_Bob"
-    #             }
-    #         ],
-    #         "topic_entity_tags": [
-    #             {
-    #                 "topic": "string",
-    #                 "entity_type": "string",
-    #                 "alliance_entity": "string",
-    #                 "taxon": "string",
-    #                 "note": "string"
-    #             }
-    #         ],
-    #         "issue_name": "4",
-    #         "language": "English",
-    #         "page_range": "538--541",
-    #         "title": "Some test 001 title",
-    #         "volume": "433",
-    #         "open_access": True
-    #     }
-    #
-    #     # process the reference.
-    #     reference = ReferenceSchemaPost(**full_xml)
-    #     curie = create(db, reference)
-    #
-    #     # fetch the new record.
-    #     res = show(db, curie)
-    #     assert res['abstract'] == 'The Hippo (Hpo) pathway is a conserved tumor suppressor pathway'
-    #     assert res['category'] == 'research_article'
-    #
-    #     # Not sure of order in array of the authors so:-
-    #     assert len(res['authors']) == 2
-    #     for author in res['authors']:
-    #         if author['first_name'] == 'D.':
-    #             assert author['name'] == 'D. Wu'
-    #             assert author['order'] == 1
-    #         else:
-    #             assert author['name'] == 'S. Wu'
-    #             assert author['order'] == 2
-    #
-    #     # Were authors created in the db?
-    #     author = db.query(AuthorModel).filter(AuthorModel.name == "D. Wu").one()
-    #     assert author.first_name == 'D.'
-    #     author = db.query(AuthorModel).filter(AuthorModel.name == "S. Wu").one()
-    #     assert author.first_name == 'S.'
-    #
-    #     assert res['citation'] == "D. Wu; S. Wu, () Some test 001 title.  433 (): 538--541"
-    #
-    #     assert res['cross_references'][0]['curie'] == 'FB:FBrf0221304'
-    #
-    #     assert res['mod_reference_types'][0]['reference_type'] == "mrt_rt"
-    #
-    #     assert res['mesh_terms'][0]['heading_term'] == "hterm"
-    #
-    #     # cross references in the db?
-    #     xref = db.query(CrossReferenceModel).filter(CrossReferenceModel.curie == "FB:FBrf0221304").one()
-    #     assert xref.reference.curie == curie
-    #
-    #     assert res["issue_name"] == "4"
-    #     assert res["language"] == "English"
-    #     assert res["page_range"] == "538--541"
-    #     assert res["title"] == "Some test 001 title"
-    #     assert res["volume"] == "433"
-    #     assert res['open_access']
-    #
-    #     print("BOB................")
-    #     print(res)
-    #     for ont in res["workflow_tags"]:
-    #         if ont['mod_abbreviation'] == "001_RGD":
-    #             assert ont['workflow_tag_id'] == "workflow_tag2"
-    #         elif ont['mod_abbreviation'] == "001_FB":
-    #             assert ont['workflow_tag_id'] == "workflow_tag1"
-    #         else:
-    #             assert 1 == 0  # Not RGD or FB ?
+    def test_reference_large(self, db, auth_headers):
+        with TestClient(app) as client:
+            full_xml = {
+                "category": "research_article",
+                "abstract": "The Hippo (Hpo) pathway is a conserved tumor suppressor pathway",
+                "authors": [
+                    {
+                        "order": 2,
+                        "first_name": "S.",
+                        "last_name": "Wu",
+                        "name": "S. Wu",
+                        # "reference_id": "PMID:23524264"
+                    },
+                    {
+                        "order": 1,
+                        "first_name": "D.",
+                        "last_name": "Wu",
+                        "name": "D. Wu",
+                        # "reference_id": "PMID:23524264"
+                    }
+                ],
+                "mesh_terms": [
+                    {
+                        "heading_term": "hterm",
+                        "qualifier_term": "qterm"
+                    }
+                ],
+                "mod_reference_types": [
+                    {
+                        "reference_type": "mrt_rt",
+                        "source": "mrt_s"
+                    }
+                ],
+                "cross_references": [
+                    {
+                        "curie": "FB:FBrf0221304",
+                        "pages": [
+                            "reference"
+                        ]
+                    }
+                ],
+                "workflow_tags": [
+                    {
+                        "workflow_tag_id": "workflow_tag1",
+                        "mod_abbreviation": "001_FB",
+                        "created_by": "001_Bob"
+                    },
+                    {
+                        "workflow_tag_id": "workflow_tag2",
+                        "mod_abbreviation": "001_RGD",
+                        "created_by": "001_Bob"
+                    }
+                ],
+                "topic_entity_tags": [
+                    {
+                        "topic": "string",
+                        "entity_type": "string",
+                        "alliance_entity": "string",
+                        "taxon": "string",
+                        "note": "string"
+                    }
+                ],
+                "issue_name": "4",
+                "language": "English",
+                "page_range": "538--541",
+                "title": "Some test 001 title",
+                "volume": "433",
+                "open_access": True
+            }
+
+            new_curie = client.post(url="/reference/", json=full_xml, headers=auth_headers).json()
+            # fetch the new record.
+            res = client.get(url=f"/reference/{new_curie}").json()
+            assert res['abstract'] == 'The Hippo (Hpo) pathway is a conserved tumor suppressor pathway'
+            assert res['category'] == 'research_article'
+
+            # Not sure of order in array of the authors so:-
+            assert len(res['authors']) == 2
+            for author in res['authors']:
+                if author['first_name'] == 'D.':
+                    assert author['name'] == 'D. Wu'
+                    assert author['order'] == 1
+                else:
+                    assert author['name'] == 'S. Wu'
+                    assert author['order'] == 2
+
+            # Were authors created in the db?
+            author = db.query(AuthorModel).filter(AuthorModel.name == "D. Wu").one()
+            assert author.first_name == 'D.'
+            author = db.query(AuthorModel).filter(AuthorModel.name == "S. Wu").one()
+            assert author.first_name == 'S.'
+
+            assert res['citation'] == "D. Wu; S. Wu, () Some test 001 title.  433 (): 538--541"
+
+            assert res['cross_references'][0]['curie'] == 'FB:FBrf0221304'
+
+            assert res['mod_reference_types'][0]['reference_type'] == "mrt_rt"
+
+            assert res['mesh_terms'][0]['heading_term'] == "hterm"
+
+            # cross references in the db?
+            xref = db.query(CrossReferenceModel).filter(CrossReferenceModel.curie == "FB:FBrf0221304").one()
+            assert xref.reference.curie == new_curie
+
+            assert res["issue_name"] == "4"
+            assert res["language"] == "English"
+            assert res["page_range"] == "538--541"
+            assert res["title"] == "Some test 001 title"
+            assert res["volume"] == "433"
+            assert res['open_access']
+
+            print("BOB................")
+            print(res)
+            for ont in res["workflow_tags"]:
+                if ont['mod_abbreviation'] == "001_RGD":
+                    assert ont['workflow_tag_id'] == "workflow_tag2"
+                elif ont['mod_abbreviation'] == "001_FB":
+                    assert ont['workflow_tag_id'] == "workflow_tag1"
+                else:
+                    assert 1 == 0  # Not RGD or FB ?
