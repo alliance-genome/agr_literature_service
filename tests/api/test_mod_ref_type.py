d9da54c3752090b27bd11cfcd7a94e94e6cf468b
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-08-31 10:51:22 -0700

    fix(tests): fixed flake8

--- tests/api/test_mod_ref_type.py
+++ tests/api/test_mod_ref_type.py
@@ -3,7 +3,7 @@ from starlette.testclient import TestClient
 from fastapi import status
 
 from agr_literature_service.api.main import app
-from agr_literature_service.api.models import ReferenceModel, ReferenceCommentAndCorrectionModel, ModReferenceTypeModel
+from agr_literature_service.api.models import ModReferenceTypeModel
 from .fixtures import auth_headers, db # noqa
 from .test_reference import create_test_reference # noqa
 
@@ -30,7 +30,7 @@ class TestModReferenceType:
             response = client.get(url="/reference/mod_reference_type/-1")
             assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_create_mrt(self, db, create_test_mod_ref_type):
+    def test_create_mrt(self, db, create_test_mod_ref_type): # noqa
         assert create_test_mod_ref_type[0].status_code == status.HTTP_201_CREATED
         # check db for mrt
         mrt = db.query(ModReferenceTypeModel).filter(
@@ -69,12 +69,12 @@ class TestModReferenceType:
             assert transactions[1]['changeset']['source'][1] == "string4"
 
     # NOTE: BAD... recursion error. NEEDS fixing.
-    def test_show_mrt(self, create_test_mod_ref_type):
+    def test_show_mrt(self, create_test_mod_ref_type): # noqa
         with TestClient(app) as client:
             response = client.get(url=f"/reference/mod_reference_type/{create_test_mod_ref_type[0].json()}")
             assert response.status_code == status.HTTP_200_OK
 
-    def test_destroy_mrt(self, create_test_mod_ref_type, auth_headers):
+    def test_destroy_mrt(self, create_test_mod_ref_type, auth_headers): # noqa
         with TestClient(app) as client:
             response = client.delete(url=f"/reference/mod_reference_type/{create_test_mod_ref_type[0].json()}",
                                      headers=auth_headers)
