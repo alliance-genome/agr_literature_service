0fad06b41f6201368c353897bde1f61261bf2764
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-02 14:42:03 -0700

    feat(continuum): using decorator instead of global vars to run function only once

--- agr_literature_service/api/database/versioning.py
+++ agr_literature_service/api/database/versioning.py
@@ -1,13 +1,10 @@
 from sqlalchemy_continuum import make_versioned
 from sqlalchemy_continuum.plugins import PropertyModTrackerPlugin
 from agr_literature_service.api.continuum_plugins import UserPlugin
+from agr_literature_service.api.utils import execute_once
 
-already_called = False
 
-
+@execute_once
 def enable_versioning():
-    global already_called
-    if not already_called:
-        user_plugin = UserPlugin()
-        make_versioned(user_cls='UserModel', plugins=[user_plugin, PropertyModTrackerPlugin()])
-        already_called = True
+    user_plugin = UserPlugin()
+    make_versioned(user_cls='UserModel', plugins=[user_plugin, PropertyModTrackerPlugin()])
