161f87a7551deb3442e06eb31846ea13071e7a7a
azurebrd <azurebrd@its.caltech.edu>
2022-09-01 17:40:03 -0700

    feat(tests): flake8

--- tests/api/test_cross_ref.py
+++ tests/api/test_cross_ref.py
@@ -30,11 +30,11 @@ def test_cross_reference(db, auth_headers, test_reference): # noqa
 
 
 class TestCrossRef:
-    def test_modify_curie(self, db, test_cross_reference):
-        query_object = db.query(CrossReferenceModel).filter_by(curie = 'XREF:123456').one()
+    def test_modify_curie(self, db, test_cross_reference): # noqa
+        query_object = db.query(CrossReferenceModel).filter_by(curie='XREF:123456').one()
         query_object.curie = 'XREF:54321'
         db.commit()
-        query_object_updated = db.query(CrossReferenceModel).filter_by(curie = 'XREF:54321').one()
+        query_object_updated = db.query(CrossReferenceModel).filter_by(curie='XREF:54321').one()
         versions_updated = [version for version in query_object_updated.versions]
         versions = [version for version in query_object.versions]
         assert versions is not versions_updated
