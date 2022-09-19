d73e8e7ccd0e9779d9e4a3a2f7d7965dcfc1c24c
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-19 15:14:42 -0700

    fix(unit_tests): added FB count to the test

--- tests/lit_processing/data_export/test_export_all_mod_references_to_json.py
+++ tests/lit_processing/data_export/test_export_all_mod_references_to_json.py
@@ -13,7 +13,7 @@ class TestExportAllModReferencesToJson:
         base_path = environ.get('XML_PATH')
         json_path = path.join(base_path, "json_data/")
         # mod_to_count = {'WB': 3, 'XB': 2, 'ZFIN': 3, 'FB': 3, 'SGD': 2, 'RGD': 2, 'MGI': 2}
-        mod_to_count = {'WB': 3, 'ZFIN': 3}
+        mod_to_count = {'WB': 3, 'ZFIN': 3, 'FB': 3}
         # for mod in ['WB', 'XB', 'ZFIN', 'FB', 'SGD', 'RGD', 'MGI']:
         for mod in ['WB', 'ZFIN', 'FB']:
             json_file = path.join(json_path, "reference_" + mod + ".json")
