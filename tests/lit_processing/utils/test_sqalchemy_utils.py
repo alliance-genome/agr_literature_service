1ff86559f18d2ef1dd0c34918ae73229cd088488
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-19 20:04:40 -0700

    feat(unit_tests): added test for load_ref_xref function

--- tests/lit_processing/utils/test_sqalchemy_utils.py
+++ tests/lit_processing/utils/test_sqalchemy_utils.py
@@ -1,4 +1,3 @@
-import sqlalchemy.orm
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
 
@@ -6,6 +5,8 @@ from agr_literature_service.lit_processing.utils.sqlalchemy_utils import create_
     create_postgres_session, sqlalchemy_load_ref_xref
 from ...fixtures import db # noqa
 from ...api.test_cross_ref import test_cross_reference # noqa
+from ...api.fixtures import auth_headers # noqa
+from ...api.test_reference import test_reference # noqa
 
 
 class TestSqlalchemyUtils:
@@ -29,6 +30,8 @@ class TestSqlalchemyUtils:
         print_out = capfd.readouterr()[0]
         assert print_out != ""
 
-    # def test_sqlalchemy_load_ref_xref(self, db, test_cross_reference):
-    #     sqlalchemy_load_ref_xref(datatype="reference")
-    #     sqlalchemy_load_ref_xref(datatype="resource")
+    def test_sqlalchemy_load_ref_xref(self, db, test_cross_reference):
+        sqlalchemy_load_ref_xref(datatype="reference")
+        sqlalchemy_load_ref_xref(datatype="resource")
+        res = db.execute("SELECT * from cross_reference")
+        assert len([row for row in res]) > 0
