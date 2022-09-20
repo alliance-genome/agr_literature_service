bbb256661f391473c84bfcf502da3f8d11069962
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-20 09:15:22 -0700

    feat(cleanup): added log and tmp files to cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
 rm -rf agr_literature_service/lit_processing/data_ingest/tmp
 rm -rf agr_literature_service/lit_processing/tests/tmp
 rm -rf tests/lit_processing/tmp
+find ./ -name "*.log" | xargs rm -f
+find ./ -name "*.*~" | xargs rm -f
