415d7cfa7efcff9cd2be55ea3b3900c58f266c7d
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-19 19:28:57 -0700

    feat(unit_tests): added test for okta_utils and removed useless function in src file

--- agr_literature_service/lit_processing/utils/okta_utils.py
+++ agr_literature_service/lit_processing/utils/okta_utils.py
@@ -26,38 +26,6 @@ def generate_headers(token):
     return headers
 
 
-def update_auth0_token():
-    """
-
-    :return:
-    """
-
-    url = 'https://alliancegenome.us.auth0.com/oauth/token'
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-    data_dict = dict()
-    data_dict['audience'] = 'alliance'
-    data_dict['grant_type'] = 'client_credentials'
-    data_dict['client_id'] = environ.get('AUTH0_CLIENT_ID')
-    data_dict['client_secret'] = environ.get('AUTH0_CLIENT_SECRET')
-    # data for this api must be a string instead of a dict
-    header_entry = dumps(data_dict)
-    # logger.info("data %s data end", header_entry)
-    post_return = requests.post(url, headers=headers, data=header_entry)
-    # logger.info("post return %s status end", post_return.status_code)
-    # logger.info("post return %s text end", post_return.text)
-    response_dict = loads(post_return.text)
-    token = response_dict['access_token']
-    # logger.info("token %s", token)
-    auth0_file = base_path + 'auth0_token'
-    with open(auth0_file, 'w') as auth0_fh:
-        auth0_fh.write("%s" % (token))
-        auth0_fh.close
-    return token
-
-
 def update_okta_token():
     """
 
@@ -86,16 +54,6 @@ def update_okta_token():
     return token
 
 
-def update_token():
-    """
-
-    :return:
-    """
-
-    token = update_okta_token()
-    return token
-
-
 def get_authentication_token():
     okta_file = base_path + 'okta_token'
     token = ''
@@ -104,5 +62,5 @@ def get_authentication_token():
             token = okta_fh.read().replace("\n", "")
             okta_fh.close
     else:
-        token = update_token()
+        token = update_okta_token()
     return token
