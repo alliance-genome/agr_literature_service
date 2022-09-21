66766a4179466cdd7f2f0721a5774ee558ef9b8e
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-09-20 18:36:43 -0700

    fix(unit_tests): create tmp dir at the beginning of test since it is deleted by previous one
    - the module that initializes the tmp dir is imported on ly once and the initialization happens at the beginning of the module, outside functions. This is causing issues with tests, where each test case deletes the tmp dir when done

--- tests/lit_processing/data_ingest/test_post_resource_to_db.py
+++ tests/lit_processing/data_ingest/test_post_resource_to_db.py
@@ -1,29 +1,30 @@
-# from os import path
-#
-# from agr_literature_service.api.models import ResourceModel, CrossReferenceModel
-# from agr_literature_service.lit_processing.data_ingest.post_resource_to_db import \
-#     post_resources
-# from agr_literature_service.lit_processing.tests.mod_populate_load import populate_test_mods
-#from ...fixtures import db, cleanup_tmp_files_when_done # noqa
+from os import path
+
+from agr_literature_service.api.models import ResourceModel, CrossReferenceModel
+from agr_literature_service.lit_processing.data_ingest.post_resource_to_db import \
+    post_resources
+from agr_literature_service.lit_processing.tests.mod_populate_load import populate_test_mods
+from agr_literature_service.lit_processing.utils.tmp_files_utils import init_tmp_dir
+from ...fixtures import db, cleanup_tmp_files_when_done # noqa
 
 
-# class TestPostResourceToDb:
-#
-#     def test_post_resources(self, db): # noqa
-#
-#         populate_test_mods()
-#
-#         base_input_dir = path.join(path.dirname(__file__), "../sample_data/")
-#         json_path = "sanitized_resources/"
-#         post_resources(json_path, 'NLM', base_input_dir)
-#
-#         res_rows = db.query(ResourceModel).all()
-#         assert len(res_rows) == 5
-#         assert res_rows[0].title == 'Biochemical and biophysical research communications'
-#         assert res_rows[1].iso_abbreviation == 'J Physiol Sci'
-#         assert res_rows[2].online_issn == '2399-3642'
-#         assert res_rows[3].print_issn == '1552-5260'
-#
-#         resource_id = res_rows[4].resource_id
-#         crossRef = db.query(CrossReferenceModel).filter_by(resource_id=resource_id).first()
-#         assert crossRef.curie == 'NLM:101759238'
+class TestPostResourceToDb:
+
+    def test_post_resources(self, db): # noqa
+        init_tmp_dir()
+        populate_test_mods()
+
+        base_input_dir = path.join(path.dirname(__file__), "../sample_data/")
+        json_path = "sanitized_resources/"
+        post_resources(json_path, 'NLM', base_input_dir)
+
+        res_rows = db.query(ResourceModel).all()
+        assert len(res_rows) == 5
+        assert res_rows[0].title == 'Biochemical and biophysical research communications'
+        assert res_rows[1].iso_abbreviation == 'J Physiol Sci'
+        assert res_rows[2].online_issn == '2399-3642'
+        assert res_rows[3].print_issn == '1552-5260'
+
+        resource_id = res_rows[4].resource_id
+        crossRef = db.query(CrossReferenceModel).filter_by(resource_id=resource_id).first()
+        assert crossRef.curie == 'NLM:101759238'
