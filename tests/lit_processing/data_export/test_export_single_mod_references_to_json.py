7d0bb9743cb2f178ce721015f12f51a95f33c3ac
sweng66 <sweng@stanford.edu>
2022-10-03 13:58:55 -0700

    removed two tests since they will be covered in unittest for db_read_utils

--- tests/lit_processing/data_export/test_export_single_mod_references_to_json.py
+++ tests/lit_processing/data_export/test_export_single_mod_references_to_json.py
@@ -6,9 +6,9 @@ from agr_literature_service.lit_processing.data_export.export_single_mod_referen
     dump_data, concatenate_json_files, get_meta_data, generate_json_file, get_reference_col_names,\
     get_reference_data_and_generate_json
 from agr_literature_service.lit_processing.utils.db_read_utils import \
-    get_comment_correction_data, get_journal_data, get_cross_reference_data_for_ref_ids,\
-    get_author_data_for_ref_ids, get_mesh_term_data_for_ref_ids, \
-    get_mod_corpus_association_data_for_ref_ids, get_mod_reference_type_data_for_ref_ids
+    get_cross_reference_data_for_ref_ids, get_author_data_for_ref_ids, \
+    get_mesh_term_data_for_ref_ids, get_mod_corpus_association_data_for_ref_ids, \
+    get_mod_reference_type_data_for_ref_ids
 
 from ...fixtures import cleanup_tmp_files_when_done, load_sanitized_references, db # noqa
 
@@ -107,14 +107,9 @@ class TestExportSingleModReferencesToJson:
         assert json.dumps(json.load(open(new_json_file)), sort_keys=True) == json.dumps(json.load(open(json_file)),
                                                                                         sort_keys=True)
 
-        ## test get_comment_correction_data() and get_journal_data()
-        reference_id_to_comment_correction_data = get_comment_correction_data(db)
-        assert type(reference_id_to_comment_correction_data) == dict
-
-        resource_id_to_journal = get_journal_data(db)
-        assert type(resource_id_to_journal) == dict
-
         ## test get_reference_data_and_generate_json()
+        reference_id_to_comment_correction_data = {}
+        resource_id_to_journal = {}
         json_file = json_path + "reference_WB.json"
         datestamp = str(date.today()).replace("-", "")
         get_reference_data_and_generate_json('WB', reference_id_to_comment_correction_data,
