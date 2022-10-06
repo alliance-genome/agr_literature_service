d47fa8f6383b8a173fc616642525e10c4bb7705d
Juancarlos <azurebrd@its.caltech.edu>
2022-10-06 00:19:14 +0000

    remove redundant parser.  add examples to each general parser

--- agr_literature_service/lit_processing/data_ingest/utils/date_utils.py
+++ agr_literature_service/lit_processing/data_ingest/utils/date_utils.py
@@ -39,8 +39,6 @@ def parse_numeric_date(date_string):
     if not result:
         result = parse_year_dash_year(date_string)
     if not result:
-        result = parse_year_dot_month(date_string)
-    if not result:
         result = parse_year_dot_or_dash_month(date_string)
     if not result:
         result = parse_year_space_month_dash_day(date_string)
@@ -91,6 +89,7 @@ def parse_hardcoded_pubmed_exceptions(date_string):
 
 def parse_year_space_mon_space_mon(date_string):
     # year space three letter month abbreviation space three letter month abbreviation
+    # 1985 Aug 1-7   ('1985-08-1', '1985-08-7')      1985-08-01      1985-08-07      AGR:AGR-Reference-0000134630
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3}) ([A-Za-z]{3})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -103,7 +102,7 @@ def parse_year_space_mon_space_mon(date_string):
 
 def parse_year_space_mon_dash_mon_space_year(date_string):
     # year space three letter month abbreviation dash three letter month abbreviation space year
-    # e.g. 2017 Jan-Feb 2017
+    # 2017 Jan-Feb 2017      ('2017-01-01', '2017-02-28')    2017-01-01      2017-02-28      AGR:AGR-Reference-0000498718
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3})-([A-Za-z]{3}) (\d{4})$", date_string)
     if re_output is not None:
         year1 = re_output.group(1)
@@ -118,6 +117,10 @@ def parse_year_space_mon_dash_mon_space_year(date_string):
 def parse_year_space_season(date_string):
     # year space season range
     # e.g. 1996 Autumn-Winter  1997 Fall-Winter  2000 Spring-Summer
+    # 1988 Summer-Autumn     ('1988-06-01', '1988-10-31')    1988-06-01      1988-10-31      AGR:AGR-Reference-0000077652
+    # 1996 Autumn-Winter     ('1996-09-01', '1996-12-31')    1996-09-01      1996-12-31      AGR:AGR-Reference-0000118116
+    # 1997 Fall-Winter       ('1997-09-01', '1997-12-31')    1997-09-01      1997-12-31      AGR:AGR-Reference-0000125880
+    # 1998 Spring-Summer     ('1998-03-01', '1998-08-31')    1998-03-01      1998-08-31      AGR:AGR-Reference-0000127636
     re_output = re.search(r"^(\d{4}) ([A-Za-z\-]*)$", date_string)
     if re_output is not None:
         season_range_dict = {
@@ -137,7 +140,7 @@ def parse_year_space_season(date_string):
 
 def parse_year_space_mon_dash_year_space_mon(date_string):
     # year space three letter month abbreviation dash year space three letter month abbreviation
-    # e.g. 1988 Dec-1989 Feb
+    # 1988 Dec-1989 Feb      ('1988-12-01', '1989-02-28')    1988-12-01      1989-02-28      AGR:AGR-Reference-0000885960
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3})-(\d{4}) ([A-Za-z]{3})$", date_string)
     if re_output is not None:
         year1 = re_output.group(1)
@@ -151,7 +154,7 @@ def parse_year_space_mon_dash_year_space_mon(date_string):
 
 def parse_year_space_mon_space_day_dash_year_space_mon_space_day(date_string):
     # year space three letter month abbreviation space day dash year space three letter month abbreviation space day
-    # e.g. 1985 Dec 19-1986 Jan 1
+    # 1985 Dec 19-1986 Jan 1 ('1985-12-19', '1986-01-1')     1985-12-19      1986-01-01      AGR:AGR-Reference-0000885273
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3}) (\d{1,2})-(\d{4}) ([A-Za-z]{3}) (\d{1,2})$", date_string)
     if re_output is not None:
         year1 = re_output.group(1)
@@ -166,6 +169,7 @@ def parse_year_space_mon_space_day_dash_year_space_mon_space_day(date_string):
 
 def parse_year_space_mon_dash_mon_space_day(date_string):
     # year space three letter month abbreviation dash three letter month abbreviation space day
+    # 2002 Feb-Mar 1 ('2002-02-1', '2002-03-1')      2002-02-01      2002-03-01      AGR:AGR-Reference-0000650454
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3})-([A-Za-z]{3}) (\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -178,6 +182,7 @@ def parse_year_space_mon_dash_mon_space_day(date_string):
 
 def parse_year_space_mon_space_day(date_string):
     # year space three letter month abbreviation space day dash three letter month abbreviation space day
+    # 1991 Feb 22    ('1991-02-22', '1991-02-22')    1991-02-22      1991-02-22      AGR:AGR-Reference-0000823017
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3}) (\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -189,6 +194,7 @@ def parse_year_space_mon_space_day(date_string):
 
 def parse_year_space_mon(date_string):
     # year space three letter month abbreviation
+    # 1965 Jun       ('1965-06-01', '1965-06-30')    1965-06-01      1965-06-30      AGR:AGR-Reference-0000821246
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -200,6 +206,7 @@ def parse_year_space_mon(date_string):
 
 def parse_year_space_mon_space_day_dash_mon_space_day(date_string):
     # year space three letter month abbreviation space day dash three letter month abbreviation space day
+    # 1999 Jan 15-Feb 1      ('1999-01-15', '1999-02-1')     1999-01-15      1999-02-01      AGR:AGR-Reference-0000005775
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3}) (\d{1,2})-([A-Za-z]{3}) (\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -213,6 +220,7 @@ def parse_year_space_mon_space_day_dash_mon_space_day(date_string):
 
 def parse_year_space_mon_space_day_dash_day(date_string):
     # year space three letter month abbreviation space day dash day
+    # 1999 Dec 16-30 ('1999-12-16', '1999-12-30')    1999-12-16      1999-12-30      AGR:AGR-Reference-0000019281
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3}) (\d{1,2})-(\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -225,6 +233,7 @@ def parse_year_space_mon_space_day_dash_day(date_string):
 
 def parse_year_space_mon_dash_mon(date_string):
     # year space three letter month abbreviation dash letter month abbreviation
+    # 2021 Jan-Dec   ('2021-01-01', '2021-12-31')    2021-01-01      2021-12-31      AGR:AGR-Reference-0000862730
     re_output = re.search(r"^(\d{4}) ([A-Za-z]{3})-([A-Za-z]{3})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -236,6 +245,7 @@ def parse_year_space_mon_dash_mon(date_string):
 
 
 def parse_date_timezone(date_string):
+    # 2006-02-01T00:00:00.000-06:00  ('2006-02-01', '2006-02-01')    2006-02-01      2006-02-01      AGR:AGR-Reference-0000829213
     just_year_re_output = re.search(r"^(\d{4})-(\d{2})-(\d{2})T00", date_string)
     if just_year_re_output is not None:
         year = just_year_re_output.group(1)
@@ -247,6 +257,7 @@ def parse_date_timezone(date_string):
 
 def parse_year_space_month_dash_day(date_string):
     # PMID:27566080 has this in the MedlineDate
+    # 2016 09-10     ('2016-09-10', '2016-09-10')    2016-09-10      2016-09-10      AGR:AGR-Reference-0000498245
     re_output = re.search(r"^(\d{4}) (\d{1,2})-(\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -257,6 +268,8 @@ def parse_year_space_month_dash_day(date_string):
 
 
 def parse_year_dot_or_dash_month(date_string):
+    # 2003.12        ('2003-12-01', '2003-12-31')    2003-12-01      2003-12-31      AGR:AGR-Reference-0000681265
+    # 2003-10        ('2003-10-01', '2003-10-31')    2003-10-01      2003-10-31      AGR:AGR-Reference-0000826710
     re_output = re.search(r"^(\d{4})[-\.](\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -268,17 +281,8 @@ def parse_year_dot_or_dash_month(date_string):
     return False
 
 
-def parse_year_dot_month(date_string):
-    re_output = re.search(r"^(\d{4})\.(\d{1,2})$", date_string)
-    if re_output is not None:
-        year = re_output.group(1)
-        month = re_output.group(2)
-        days = monthrange(int(year), int(month))[1]
-        return f"{year}-{month}-01", f"{year}-{month}-{days}"
-    return False
-
-
 def parse_year_dash_year(date_string):
+    # 1932-1933      ('1932-01-01', '1933-12-31')    1932-01-01      1933-12-31      AGR:AGR-Reference-0000683960
     re_output = re.search(r"^(\d{4})-(\d{4})$", date_string)
     if re_output is not None:
         year1 = re_output.group(1)
@@ -288,6 +292,7 @@ def parse_year_dash_year(date_string):
 
 
 def parse_dot_date(date_string):
+    # 2000.11.30     ('2000-11-30', '2000-11-30')    2000-11-30      2000-11-30      AGR:AGR-Reference-0000680909
     re_output = re.search(r"^(\d{4})\.(\d{1,2})\.(\d{1,2})$", date_string)
     if re_output is not None:
         year = re_output.group(1)
@@ -298,6 +303,7 @@ def parse_dot_date(date_string):
 
 
 def parse_just_year(date_string):
+    # 2016   ('2016-01-01', '2016-12-31')    2016-01-01      2016-12-31      AGR:AGR-Reference-0000001171
     just_year_re_output = re.search(r"^(\d{4})$", date_string)
     if just_year_re_output is not None:
         year = just_year_re_output.group(1)
@@ -306,6 +312,7 @@ def parse_just_year(date_string):
 
 
 def parse_just_date(date_string):
+    # 1998-11-01     ('1998-11-01', '1998-11-01')    1998-11-01      1998-11-01      AGR:AGR-Reference-0000001170
     just_year_re_output = re.search(r"^(\d{4})-(\d{2})-(\d{2})$", date_string)
     if just_year_re_output is not None:
         year = just_year_re_output.group(1)
