25b1352b48c3d711f806eb627be42cb216b81df3
sweng66 <sweng@stanford.edu>
2022-10-06 21:04:51 -0700

    updated to get new resource curie from A-team MaTi webservice

--- agr_literature_service/api/crud/resource_crud.py
+++ agr_literature_service/api/crud/resource_crud.py
@@ -5,8 +5,9 @@ resource_crud.py
 
 from datetime import datetime
 from typing import Dict, Union
+from os import environ
+import requests
 
-import sqlalchemy
 from fastapi import HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy import ARRAY, Boolean, String, func
@@ -18,19 +19,27 @@ from agr_literature_service.api.crud.reference_resource import create_obj
 from agr_literature_service.api.models import (CrossReferenceModel, EditorModel,
                                                MeshDetailModel, ResourceModel)
 from agr_literature_service.api.schemas import ResourceSchemaPost, ResourceSchemaUpdate
+from agr_literature_service.lit_processing.utils.okta_utils import (
+    get_authentication_token,
+    generate_headers
+)
 
 
-def create_next_curie(curie):
+def create_next_curie():
     """
     Creates the next curie in the sequence.
-    :param curie:
     :return:
     """
 
-    curie_parts = curie.rsplit('-', 1)
-    number_part = curie_parts[1]
-    number = int(number_part) + 1
-    return "-".join([curie_parts[0], str(number).rjust(10, '0')])
+    token = get_authentication_token()
+    headers = generate_headers(token)
+    headers['subdomain'] = 'resource'
+    url = environ['ID_MATI_URL']
+    headers['value'] = '1'
+    res = requests.post(url, headers=headers)
+    res_json = res.json()
+    new_curie = res_json['first']['curie']
+    return new_curie
 
 
 def create(db: Session, resource: ResourceSchemaPost):
@@ -52,14 +61,7 @@ def create(db: Session, resource: ResourceSchemaPost):
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                     detail=f"CrossReference with curie {cross_reference.curie} already exists")
 
-    last_curie = db.query(ResourceModel.curie).order_by(sqlalchemy.desc(ResourceModel.curie)).first()
-
-    if not last_curie:
-        last_curie = 'AGR:AGR-Resource-0000000000'
-    else:
-        last_curie = last_curie[0]
-
-    curie = create_next_curie(last_curie)
+    curie = create_next_curie()
     resource_data['curie'] = curie
 
     for field, value in vars(resource).items():
