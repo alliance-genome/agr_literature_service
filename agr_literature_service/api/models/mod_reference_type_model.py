8962ba74540d86a9e176363d49596f3b64a0cc1f
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-10-10 16:05:32 -0700

    feat(mod_ref_type): restored old tables for mod ref type and removed alembic revision to avoid deleting old data

--- agr_literature_service/api/models/mod_reference_type_model.py
+++ agr_literature_service/api/models/mod_reference_type_model.py
@@ -6,7 +6,7 @@ mod_reference_type_model.py
 
 from typing import Dict
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 from agr_literature_service.api.database.base import Base
@@ -14,6 +14,42 @@ from agr_literature_service.api.database.versioning import enable_versioning
 from agr_literature_service.api.models.audited_model import AuditedModel
 
 enable_versioning()
+
+
+class ModReferenceTypeModel(Base):
+    __tablename__ = "mod_reference_type"
+    __versioned__: Dict = {}
+    __table_args__ = (UniqueConstraint('reference_id', 'reference_type', 'source', name='uniq_mrt'),)
+
+    mod_reference_type_id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    reference_id = Column(
+        Integer,
+        ForeignKey("reference.reference_id",
+                   ondelete="CASCADE"),
+        index=True
+    )
+
+    reference = relationship(
+        "ReferenceModel",
+        back_populates="mod_reference_type"
+    )
+
+    reference_type = Column(
+        String(),
+        unique=False,
+        nullable=False
+    )
+
+    source = Column(
+        String(),
+        unique=False,
+        nullable=True
+    )
 
 
 class ReferenceTypeModel(Base):
