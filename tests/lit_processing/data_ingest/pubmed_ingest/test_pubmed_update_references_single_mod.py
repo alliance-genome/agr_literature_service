941de327ca21588f7e2a2ead56985e5019195a23
sweng66 <sweng@stanford.edu>
2022-10-14 15:59:55 -0700

    adding populate_test_mod_reference_types to the import list

--- tests/lit_processing/data_ingest/pubmed_ingest/test_pubmed_update_references_single_mod.py
+++ tests/lit_processing/data_ingest/pubmed_ingest/test_pubmed_update_references_single_mod.py
@@ -6,7 +6,8 @@ from agr_literature_service.lit_processing.data_ingest.pubmed_ingest.pubmed_upda
     update_database, create_new_citation, update_reference_table, generate_pmids_with_info
 from agr_literature_service.lit_processing.utils.db_read_utils import \
     get_pmid_to_reference_id, get_author_data
-from ....fixtures import cleanup_tmp_files_when_done, load_sanitized_references, db # noqa
+from ....fixtures import cleanup_tmp_files_when_done, load_sanitized_references, \
+    populate_test_mod_reference_types, db # noqa
 
 
 class TestPubmedUpdateReferenceSingleMod:
