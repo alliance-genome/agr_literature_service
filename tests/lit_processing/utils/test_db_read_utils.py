4583f6e78fad0d47ffa9c86dfda2e79e93880063
sweng66 <sweng@stanford.edu>
2022-11-20 16:41:42 -0800

    adding unit tests for PMC DB reading/writing functions

--- tests/lit_processing/utils/test_db_read_utils.py
+++ tests/lit_processing/utils/test_db_read_utils.py
@@ -1,7 +1,12 @@
 import logging
-from agr_literature_service.api.models import ReferenceModel
+from agr_literature_service.api.models import ReferenceModel, CrossReferenceModel, \
+    ReferencefileModel
 from agr_literature_service.lit_processing.utils.db_read_utils import \
-    get_references_by_curies, get_curie_to_title_mapping
+    get_references_by_curies, get_curie_to_title_mapping, \
+    get_pmid_list_without_pmc_package, get_referencefile_rows, \
+    get_referencefile_mod_rows, get_pmid_to_reference_id_mapping
+from agr_literature_service.lit_processing.data_ingest.utils.db_write_utils import \
+    insert_referencefile, insert_referencefile_mod_for_pmc
 from ...fixtures import db, load_sanitized_references, populate_test_mod_reference_types # noqa
 
 logging.basicConfig(format='%(message)s')
@@ -11,7 +16,7 @@ logger.setLevel(logging.INFO)
 
 class TestDbReadUtils:
 
-    def test_dqm_read_functions(self, db, load_sanitized_references, populate_test_mod_reference_types): # noqa
+    def test_dqm_db_read_functions(self, db, load_sanitized_references, populate_test_mod_reference_types): # noqa
 
         refs = db.query(ReferenceModel).order_by(ReferenceModel.curie).all()
 
@@ -28,3 +33,40 @@ class TestDbReadUtils:
         agr_to_title = get_curie_to_title_mapping(missing_agr_in_mod['ZFIN'])
         assert agr_to_title[refs[0].curie] == refs[0].title
         assert agr_to_title[refs[1].curie] == refs[1].title
+
+
+    def test_pmc_read_and_write_functions(self, db, load_sanitized_references, populate_test_mod_reference_types): # noqa
+
+        pmid = "33622238"
+        crossRef = db.query(CrossReferenceModel).filter_by(curie='PMID:' + pmid).one_or_none()
+        reference_id = crossRef.reference_id
+        file_class = 'supplement'
+        file_publication_status = 'final'
+        file_name_with_suffix = "test_suppl.txt"
+        md5sum = "d5073c77841aa7ae1066dbd2323dcd56"
+        referencefile_id = insert_referencefile(db, pmid, file_class,
+                                                file_publication_status,
+                                                file_name_with_suffix,
+                                                reference_id, md5sum,
+                                                logger)
+        insert_referencefile_mod_for_pmc(db, pmid, file_name_with_suffix,
+                                         referencefile_id, logger)
+        db.commit()
+
+        refFile = db.query(ReferencefileModel).filter_by(reference_id=reference_id).one_or_none()
+        assert refFile.display_name == 'test_suppl'
+        assert refFile.file_extension == 'txt'
+        assert refFile.md5sum == md5sum
+
+        pmids = get_pmid_list_without_pmc_package(['ZFIN'], db)
+        assert pmids == ['34354223', '35151207']
+
+        (referencefile_loaded, reference_id_file_name) = get_referencefile_rows(db)
+        assert referencefile_loaded[(reference_id, md5sum)] == referencefile_id
+        assert reference_id_file_name[(reference_id, file_name_with_suffix)] == 1
+
+        referencefile_mod_loaded = get_referencefile_mod_rows(db)
+        assert referencefile_mod_loaded[referencefile_id] == 1
+
+        pmid_to_reference_id = get_pmid_to_reference_id_mapping(db)
+        assert pmid_to_reference_id.get(pmid) == reference_id
