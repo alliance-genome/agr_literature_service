a6ca2e6c692e75a55fae468ba8e45da9ecd769e8
Paul Hale <paul.hale@jax.org>
2022-11-28 20:21:38 -0700

    Formatting

--- agr_literature_service/api/crud/search_crud.py
+++ agr_literature_service/api/crud/search_crud.py
@@ -14,8 +14,7 @@ from fastapi import HTTPException, status
 def search_references(query: str = None, facets_values: Dict[str, List[str]] = None,
                       size_result_count: Optional[int] = 10, page: Optional[int] = 0,
                       facets_limits: Dict[str, int] = None, return_facets_only: bool = False,
-                      author_filter: Optional[str] = None, date_pubmed_modified: Optional[List[str]] = None,
-                      date_pubmed_arrive: Optional[List[str]]):
+                      author_filter: Optional[str] = None, date_pubmed_modified: Optional[List[str]] = None, date_pubmed_arrive: Optional[List[str]] = None):
     if query is None and facets_values is None and not return_facets_only:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="requested a search but no query and no facets provided")
