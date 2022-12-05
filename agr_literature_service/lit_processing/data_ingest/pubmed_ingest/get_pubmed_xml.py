f23527385d6bc05102c972be96c17dd8b6ca46c7
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2022-12-05 15:25:48 -0800

    feat(xml_download): using retry decorator to add delay between attempts

--- agr_literature_service/lit_processing/data_ingest/pubmed_ingest/get_pubmed_xml.py
+++ agr_literature_service/lit_processing/data_ingest/pubmed_ingest/get_pubmed_xml.py
@@ -13,6 +13,7 @@ from typing import List
 import requests
 from dotenv import load_dotenv
 from agr_literature_service.lit_processing.utils.tmp_files_utils import init_tmp_dir
+from retry import retry
 
 logging.basicConfig(level=logging.INFO,
                     stream=sys.stdout,
@@ -62,6 +63,43 @@ init_tmp_dir()
 # log_file_path = path.join(path.dirname(path.abspath(__file__)), '../logging.conf')
 # logging.config.fileConfig(log_file_path)
 # logger = logging.getLogger('literature logger')
+
+
+@retry(requests.exceptions.RequestException, delay=30, backoff=2, tries=10)
+def download_pubmed_xml_slice(url, parameters, pmids_found, storage_path, md5dict, pmids_joined):
+    # PubMed randomly has ("Connection broken: InvalidChunkLength(got length b'', 0 bytes read)"
+    # that crashes this script.
+    with requests.post(url, data=parameters) as r:
+        xml_all = r.text
+        # xml_all = r.text.encode('utf-8').strip()		  # python2
+        # xml_split = xml_all.split("\n<Pubmed")		  # before 2021 08 11  xml output had linebreaks between pmids, making that easier
+        xml_split = re.split('(<Pubmed[^>]*Article>)',
+                             xml_all)  # some types are not PubmedArticle, like PubmedBookArticle, e.g. 32644453
+
+        header = xml_split.pop(0)
+        # header = header + "\n<Pubmed" + xml_split.pop(0)	  # before when splitting on linebreak without capturing was manually adding the split
+        # footer = "\n\n</PubmedArticleSet>"
+        footer = "</PubmedArticleSet>"
+
+        while xml_split:
+            this_xml = header + xml_split.pop(0) + xml_split.pop(0)
+            if len(xml_split) > 0:
+                this_xml = this_xml + footer
+            clean_xml = os.linesep.join([s for s in this_xml.splitlines() if s])
+            clean_xml = clean_xml.replace('\n', ' ')
+            # logger.info(clean_xml)
+            if re.search(r"<PMID[^>]*?>(\d+)</PMID>", clean_xml):
+                pmid_group = re.search(r"<PMID[^>]*?>(\d+)</PMID>", clean_xml)
+                assert pmid_group is not None
+                pmid = pmid_group.group(1)
+                pmids_found.add(pmid)
+                filename = storage_path + pmid + '.xml'
+                f = open(filename, "w")
+                f.write(clean_xml)
+                f.close()
+                md5sum = hashlib.md5(clean_xml.encode('utf-8')).hexdigest()
+                md5dict[pmid] = md5sum
+        time.sleep(5)
 
 
 def download_pubmed_xml(pmids_wanted: List[str]):  # pragma: no cover
@@ -132,45 +170,7 @@ def download_pubmed_xml(pmids_wanted: List[str]):  # pragma: no cover
         url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
         parameters = {'db': 'pubmed', 'retmode': 'xml', 'id': pmids_joined}
 
-        try:
-            # PubMed randomly has ("Connection broken: InvalidChunkLength(got length b'', 0 bytes read)"
-            # that crashes this script.
-            with requests.post(url, data=parameters) as r:
-                xml_all = r.text
-                # xml_all = r.text.encode('utf-8').strip()		  # python2
-                # xml_split = xml_all.split("\n<Pubmed")		  # before 2021 08 11  xml output had linebreaks between pmids, making that easier
-                xml_split = re.split('(<Pubmed[^>]*Article>)', xml_all)	  # some types are not PubmedArticle, like PubmedBookArticle, e.g. 32644453
-
-                header = xml_split.pop(0)
-                # header = header + "\n<Pubmed" + xml_split.pop(0)	  # before when splitting on linebreak without capturing was manually adding the split
-                # footer = "\n\n</PubmedArticleSet>"
-                footer = "</PubmedArticleSet>"
-
-                while xml_split:
-                    this_xml = header + xml_split.pop(0) + xml_split.pop(0)
-                    if len(xml_split) > 0:
-                        this_xml = this_xml + footer
-                    clean_xml = os.linesep.join([s for s in this_xml.splitlines() if s])
-                    clean_xml = clean_xml.replace('\n', ' ')
-                    # logger.info(clean_xml)
-                    if re.search(r"<PMID[^>]*?>(\d+)</PMID>", clean_xml):
-                        pmid_group = re.search(r"<PMID[^>]*?>(\d+)</PMID>", clean_xml)
-                        assert pmid_group is not None
-                        pmid = pmid_group.group(1)
-                        pmids_found.add(pmid)
-                        filename = storage_path + pmid + '.xml'
-                        f = open(filename, "w")
-                        f.write(clean_xml)
-                        f.close()
-                        md5sum = hashlib.md5(clean_xml.encode('utf-8')).hexdigest()
-                        md5dict[pmid] = md5sum
-
-                if len(pmids_slice) == pmids_slice_size:
-                    logger.info("waiting to process more pmids")
-                    time.sleep(5)
-        except requests.exceptions.RequestException as e:
-            logger.info("requests failure with input %s %s", pmids_joined, e)
-            raise SystemExit(e)
+        download_pubmed_xml_slice(url, parameters, pmids_found, storage_path, md5dict, pmids_joined)
 
     # md5file = storage_path + 'md5sum'
     logger.info("Writing md5sum mappings to %s", md5file)
