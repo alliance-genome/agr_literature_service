13041c91d390c65d93cbe225ec410632ae02ae97
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-01-27 09:15:47 -0800

    chore(search): fixed indentation

--- agr_literature_service/api/crud/search_crud.py
+++ agr_literature_service/api/crud/search_crud.py
@@ -66,26 +66,26 @@ def search_date_range(es_body,
         logger.debug(f"Search date_published: start={start}, end={end}")
         es_body["query"]["bool"]["filter"]["bool"]["must"].append(
             {
-               "bool": {
-                   "should": [
-                       {
-                           "range": {
-                               "date_published_end": {
-                                   "gte": start,
-                                   "lte": end
-                               }
-                           }
-                       },
-                       {
-                           "range": {
-                               "date_published_start": {
-                                   "gte": start,
-                                   "lte": end
-                               }
-                           }
-                       }
-                   ]
-               }
+                "bool": {
+                    "should": [
+                        {
+                            "range": {
+                                "date_published_end": {
+                                    "gte": start,
+                                    "lte": end
+                                }
+                            }
+                        },
+                        {
+                            "range": {
+                                "date_published_start": {
+                                    "gte": start,
+                                    "lte": end
+                                }
+                            }
+                        }
+                    ]
+                }
             })
 
 
