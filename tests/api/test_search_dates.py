d2d4c0bd936b08189f8b0d22627ed9c8ca395987
ianlongden <ianlongden@morgan.harvard.edu>
2023-02-06 20:41:49 -0500

    Get date zone and look at res.

--- tests/api/test_search_dates.py
+++ tests/api/test_search_dates.py
@@ -216,6 +216,14 @@ class TestSearch:
         # would return.
         # So this checks the whole dates are used and ignores the time bit.
         with TestClient(app) as client:
+            date1_str = '2022-01-01'
+            date2_str = '2022-03-28'
+
+            date1 = int(datetime.datetime.strptime(date1_str, '%Y-%m-%d').timestamp()) * 1000000
+            date2 = int(datetime.datetime.strptime(date2_str, '%Y-%m-%d').timestamp()) * 1000000
+            print(f"date1: {date1}")
+            print(f"date2: {date2}")
+            print(f"date1 conv {date_str_to_micro_seconds('2022-01-01T', True)}")
             search_data = {
                 "query": "",
                 "facets_limits": {"pubmed_types.keyword": 10,
@@ -237,14 +245,6 @@ class TestSearch:
     def test_search_references_case2(self, initialize_elasticsearch, auth_headers): # noqa
         # Should just find 3 record where the end is overlapped.
         with TestClient(app) as client:
-            date1_str = '2022-01-01'
-            date2_str = '2022-03-28'
-
-            date1 = int(datetime.datetime.strptime(date1_str, '%Y-%m-%d').timestamp()) * 1000000
-            date2 = int(datetime.datetime.strptime(date2_str, '%Y-%m-%d').timestamp()) * 1000000
-            print(f"date1: {date1}")
-            print(f"date2: {date2}")
-            print(f"date1 conv {date_str_to_micro_seconds('2022-01-01T', True)}")
             search_data = {
                 "query": "",
                 "facets_limits": {"pubmed_types.keyword": 10,
