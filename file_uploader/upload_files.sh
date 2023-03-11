b0d912ac66fa0da07ef47414f7fe11add2491b36
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-03-10 17:26:07 -0800

    feat(file_uploader): finalized function to extract metadata

--- file_uploader/upload_files.sh
+++ file_uploader/upload_files.sh
@@ -12,13 +12,31 @@ OKTA_ACCESS_TOKEN=$(curl -s --request POST --url https://${OKTA_DOMAIN}/v1/token
 
 extract_metadata() {
   filepath=$1
-  echo $filepath
   filename=$(basename ${filepath})
+  display_name="${filename%.*}"
+  file_extension="${filename##*.}"
+  file_class="main"
+  file_publication_status="final"
+  pdf_type="null"
+
   regex="^([0-9]+)[-_]([^_]+)[_](.*)?\..*$"
   [[ $filename =~ $regex ]]
   reference_id=${BASH_REMATCH[1]}
   author_and_year=${BASH_REMATCH[2]}
   additional_options=${BASH_REMATCH[3]}
+  if [[ "${additional_options}" == *"temp"* ]]; then
+    file_publication_status="temp"
+  fi
+  if [[ "${additional_options,,}" == *"supp"* ]]; then
+    file_class="supplement"
+  fi
+  if [[ "${additional_options,,}" == *"aut"* ]]; then
+    pdf_type="aut"
+  elif [[ "${additional_options,,}" == *"ocr"* ]]; then
+    pdf_type="ocr"
+  elif [[ "${additional_options,,}" == *"html"* ]]; then
+    pdf_type="html"
+  fi
 }
 
 for refdir in /usr/files_to_upload/*; do
@@ -28,8 +46,12 @@ for refdir in /usr/files_to_upload/*; do
       if [[ ! -d ${reffile} && $(basename ${reffile}) != "*" ]]; then
         echo "Processing file ${reffile}"
         extract_metadata $reffile
-        echo $reference_id
-        echo $additional_options
+        echo "reference ID: ${reference_id}"
+        echo "display_name: ${display_name}"
+        echo "file_extension: ${file_extension}"
+        echo "file_class: ${file_class}"
+        echo "file_publication_status: ${file_publication_status}"
+        echo "pdf_type: ${pdf_type}"
       else
         echo "Cannot process reference"
       fi
