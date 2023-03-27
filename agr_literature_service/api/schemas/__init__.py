19a9c9ea20656403b94085c4677184ce324a1705
sweng66 <sweng@stanford.edu>
2023-03-27 14:49:07 -0700

    testing

--- agr_literature_service/api/schemas/__init__.py
+++ agr_literature_service/api/schemas/__init__.py
@@ -98,8 +98,5 @@ from agr_literature_service.api.schemas.reference_schemas import (
     ReferenceSchemaShow,
     ReferenceSchemaNeedReviewShow)
 
-from agr_literature_service.api.schemas.copyright_license_schemas import (
-    CopyrightLicenseSchemaPost,
-    CopyrightLicenseSchemaShow)
-
+from agr_literature_service.api.schemas.copyright_license_schemas import CopyrightLicenseSchemaPost
 from agr_literature_service.api.schemas.search_schemas import FacetsOptionsSchema
