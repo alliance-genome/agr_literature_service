b78af5f5bc061543c60a3bb5c386185eee3d45f3
sweng66 <sweng@stanford.edu>
2023-03-27 12:15:32 -0700

    adding tests for adding license to a paper

--- tests/api/test_reference.py
+++ tests/api/test_reference.py
@@ -97,6 +97,9 @@ class TestReference:
             response = client.post(url=f"/reference/citationupdate/{test_reference.new_ref_curie}",
                                    headers=auth_headers)
             assert response.status_code == status.HTTP_201_CREATED
+            response = client.post(url=f"/reference/all_license/{test_reference.new_ref_curie}/CC+BY",
+                                   headers=auth_headers)
+            assert response.status_code == status.HTTP_201_CREATED
             updated_ref = client.get(url=f"/reference/{test_reference.new_ref_curie}").json()
             assert updated_ref["title"] == "new title"
             assert updated_ref["category"] == "book"
@@ -105,7 +108,7 @@ class TestReference:
             assert updated_ref["date_published_start"] == "2022-10-01"
             # Do we have a new citation
             assert updated_ref["citation"] == ", () new title.   (): "
-
+            assert updated_ref["copyright_license_id"] is not None
     def test_changesets(self, test_reference, auth_headers): # noqa
         with TestClient(app) as client:
             # title            : None -> bob -> 'new title'
