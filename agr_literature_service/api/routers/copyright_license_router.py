8b62142134c2a4652682c8f1a02e81c60763f504
sweng66 <sweng@stanford.edu>
2023-03-27 15:58:33 -0700

    adding one more endpoint for adding a new license row into database

--- agr_literature_service/api/routers/copyright_license_router.py
+++ agr_literature_service/api/routers/copyright_license_router.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security, status
+from fastapi_okta import OktaUser
 from sqlalchemy.orm import Session
 
 from agr_literature_service.api import database
 from agr_literature_service.api.crud import copyright_license_crud
-
+from agr_literature_service.api.routers.authentication import auth
+from agr_literature_service.api.schemas import CopyrightLicenseSchemaPost
+from agr_literature_service.api.user import set_global_user_from_okta
 
 router = APIRouter(
     prefix="/copyright_license",
@@ -13,6 +16,17 @@ router = APIRouter(
 
 get_db = database.get_db
 db_session: Session = Depends(get_db)
+db_user = Security(auth.get_user)
+
+
+@router.post('/',
+             status_code=status.HTTP_201_CREATED,
+             response_model=int)
+def create(request: CopyrightLicenseSchemaPost,
+           user: OktaUser = db_user,
+           db: Session = db_session):
+    set_global_user_from_okta(db, user)
+    return copyright_license_crud.create(db, request)
 
 
 @router.get('/all',
