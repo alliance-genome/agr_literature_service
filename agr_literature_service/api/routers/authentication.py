39d8b9635bbdc0de6bd0e2193f1ad12aa19498e8
ianlongden <ianlongden@morgan.harvard.edu>
2023-04-06 17:54:15 -0400

    try around authentication to trap problems

--- agr_literature_service/api/routers/authentication.py
+++ agr_literature_service/api/routers/authentication.py
@@ -2,9 +2,16 @@ from fastapi import APIRouter
 from fastapi_okta import Okta
 
 from agr_literature_service.api.config import config
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(tags=['Authentication'])
 
-auth = Okta(domain=config.OKTA_DOMAIN,
-            api_audience=config.OKTA_API_AUDIENCE
-            )
+try:
+    auth = {}
+    auth = Okta(domain=config.OKTA_DOMAIN,
+                api_audience=config.OKTA_API_AUDIENCE
+                )
+except Exception as e:
+    logger.error(f"Authentication failed: Exception {e}")
