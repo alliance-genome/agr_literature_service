22fce96a9e887de89bb94b468f47e44b71f86fc3
ianlongden <ianlongden@morgan.harvard.edu>
2023-04-06 17:56:14 -0400

    Still need to add almebic but getting there

--- agr_literature_service/lit_processing/oneoff_scripts/make_subset_db/make_subset.py
+++ agr_literature_service/lit_processing/oneoff_scripts/make_subset_db/make_subset.py
@@ -1,103 +1,202 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, subqueryload
 from os import environ
+
+import argparse
 
 from agr_literature_service.api.models import (
     ModModel,
     UserModel,
     ReferenceModel,
     ResourceModel,
+    ModReferencetypeAssociationModel,
+    ReferencetypeModel,
     CopyrightLicenseModel
 )
+ALL_OUTPUT = 2  # For verbosity
+helptext = r"""
+    env PSQL_XXX used for Target subset db details.
+    env ORIG_XXX used for source reference db details.
+    If ORIG_XXX is defined use that for source db details.
+    If ORIG_XXX NOT defined then use PSQL_XXX details.
+    In most cases ORIG_XXX will not be needed but this is incase
+    the target and source databases are not on the same server.
 
-num_of_ref = 2000
-orig_db = 'literature-test'
-subset_db = 'literature_subset'
+"""
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=helptext)
+parser.add_argument('-l', '--limit', help='Limit to x references', type=int, default=2000, required=False)
+parser.add_argument('-v', '--verbose', help="verbosity, minumum=0, headlines=1, full=2,", type=int, default=1, required=False)
+args = parser.parse_args()
+
+num_of_refs = args.limit
+verbose = args.verbose
+# BOB: Check other table data that maybe needed.
 
 
-def create_postgres_engine(verbose, db):
+def create_postgres_engine(verbose, source=False):
 
-    """Connect to database."""
-    USER = environ.get('PSQL_USERNAME', 'postgres')
-    PASSWORD = environ.get('PSQL_PASSWORD', 'postgres')
-    SERVER = environ.get('PSQL_HOST', 'localhost')
-    PORT = environ.get('PSQL_PORT', '5433')
-
-    DB = db
+    """Connect to database.
+    If ORIG_XXX is defined use that for source db details.
+    If ORIG_XXX not defined then use PSQL_XXX.
+    In most cases ORIG_XXX will not be needed but this is incase
+    the target and source databases are not on the same server.
+    """
+    db_type = "Source"
+    if source:
+        USER = environ.get('ORIG_USERNAME', None)
+        if not USER:
+            USER = environ.get('PSQL_USERNAME', 'postgres')
+        PASSWORD = environ.get('ORIG_PASSWORD', None)
+        if not PASSWORD:
+            PASSWORD = environ.get('PSQL_PASSWORD', 'postgres')
+        SERVER = environ.get('ORIG_HOST', None)
+        if not SERVER:
+            SERVER = environ.get('PSQL_HOST', 'localhost')
+        PORT = environ.get('ORIG_PORT', None)
+        if not PORT:
+            PORT = environ.get('PSQL_PORT', '5432')
+        DB = environ.get('ORIG_DATABASE', 'literature_test')
+    else:
+        USER = environ.get('PSQL_USERNAME', 'postgres')
+        PASSWORD = environ.get('PSQL_PASSWORD', 'postgres')
+        SERVER = environ.get('PSQL_HOST', 'localhost')
+        PORT = environ.get('PSQL_PORT', '5432')
+        DB = environ.get('PSQL_DATABASE', 'literature_subset')
+        db_type = "Target"
 
     # Create our SQL Alchemy engine from our environmental variables.
     engine_var = 'postgresql://' + USER + ":" + PASSWORD + '@' + SERVER + ':' + PORT + '/' + DB
+
+    if verbose:
+        print(f"SQL {db_type}: {engine_var}")
     engine = create_engine(engine_var)
-    if True:
-        print('Using server: {}'.format(SERVER))
-        print('Using database: {}'.format(DB))
-        print(engine_var)
 
     return engine
 
 
-def create_postgres_session(verbose, db):
+def create_postgres_session(verbose, source=False):
 
-    engine = create_postgres_engine(verbose, db)
+    engine = create_postgres_engine(verbose, source)
 
-    # Session = sessionmaker(bind=engine)
     Session = sessionmaker(bind=engine, autoflush=False)
     session = Session()
 
     return session
 
 
+def add_references(db_orig_session, db_subset_session):
+    if verbose:
+        print("Adding references")
+    refs = db_orig_session.query(ReferenceModel).options(
+        subqueryload(ReferenceModel.cross_reference),
+        subqueryload(ReferenceModel.obsolete_reference),
+        subqueryload(ReferenceModel.mod_referencetypes),
+        subqueryload(ReferenceModel.mod_corpus_association),
+        subqueryload(ReferenceModel.mesh_term),
+        subqueryload(ReferenceModel.author),
+        subqueryload(ReferenceModel.referencefiles),
+        subqueryload(ReferenceModel.topic_entity_tags),
+        subqueryload(ReferenceModel.workflow_tag)
+    ).filter(ReferenceModel.reference_id <= num_of_refs)
+    for ref in refs:
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {ref}")
+        db_subset_session.merge(ref)
+    print("Be patient the commit can take a wee while.")
+    db_subset_session.commit()
+
+
+def add_specific_test_references(db_orig_session, db_subset_session):
+    # curies in curie order. Please reatin this order.
+    curies = ['AGRKB:101000000656561',
+              'AGRKB:101000000650977',
+              'AGRKB:101000000661443',
+              'AGRKB:101000000679676',
+              'AGRKB:101000000654341',
+              'AGRKB:101000000589922',
+              'AGRKB:101000000863609',
+              'AGRKB:101000000946299',
+              'AGRKB:101000000594062',
+              'AGRKB:101000000872479',
+              'AGRKB:101000000865324']
+    if verbose:
+        print("Adding SET test references")
+    refs = db_orig_session.query(ReferenceModel).options(
+        subqueryload(ReferenceModel.cross_reference),
+        subqueryload(ReferenceModel.obsolete_reference),
+        subqueryload(ReferenceModel.mod_referencetypes),
+        subqueryload(ReferenceModel.mod_corpus_association),
+        subqueryload(ReferenceModel.mesh_term),
+        subqueryload(ReferenceModel.author),
+        subqueryload(ReferenceModel.referencefiles),
+        subqueryload(ReferenceModel.topic_entity_tags),
+        subqueryload(ReferenceModel.workflow_tag)
+    ).filter(ReferenceModel.curie.in_(curies))
+    # ).filter(ReferenceModel.curie.any(ReferenceModel.curie.in_(curies)))
+    for ref in refs:
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {ref}")
+        db_subset_session.merge(ref)
+    db_subset_session.commit()
+
+
 def start():
-    db_orig_session = create_postgres_session(False, orig_db)
+    db_orig_session = create_postgres_session(True, source=True)
 
-    db_subset_session = create_postgres_session(False, subset_db)
-
-    # Uncomment if not starting from a fresh db
-    # db_subset_session.execute("DELETE FROM cross_reference")
-    # db_subset_session.execute("DELETE FROM resource")
-    # db_subset_session.execute("DELETE FROM reference")
-    # db_subset_session.execute("DELETE FROM users")
-    # db_subset_session.execute("DELETE FROM mod")
+    db_subset_session = create_postgres_session(True)
 
     users = db_orig_session.query(UserModel)
     for user in users:
-        # db_subset_session.add(user)
-        print(f"Adding user {user.id} {user.email}")
+        if verbose == ALL_OUTPUT:
+            print(f"Adding user {user.id} {user.email}")
         db_subset_session.merge(user)
     db_subset_session.commit()
     db_subset_session.close()
 
     mods = db_orig_session.query(ModModel)
     for mod in mods:
-        print(f"Adding mod {mod}")
+        if verbose == ALL_OUTPUT:
+            print(f"Adding mod {mod}")
         db_subset_session.merge(mod)
     db_subset_session.commit()
     db_subset_session.close()
 
-    db_subset_session = create_postgres_session(False, subset_db)
-
-    resources = db_orig_session.query(ResourceModel).join(ReferenceModel).filter(ReferenceModel.reference_id <= num_of_ref)
+    resources = db_orig_session.query(ResourceModel).join(ReferenceModel).filter(ReferenceModel.reference_id <= num_of_refs)
     for res in resources:
-        print(f"Adding {res}")
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {res}")
         db_subset_session.merge(res)
     db_subset_session.commit()
-
     db_subset_session.close()
 
     copyrightlicenses = db_orig_session.query(CopyrightLicenseModel).join(ReferenceModel).filter(
-        ReferenceModel.reference_id <= num_of_ref)
+        ReferenceModel.reference_id <= num_of_refs)
     for copy in copyrightlicenses:
-        print(f"Adding {copy}")
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {copy}")
         db_subset_session.merge(copy)
     db_subset_session.commit()
     db_subset_session.close()
 
-    refs = db_orig_session.query(ReferenceModel).filter(ReferenceModel.reference_id <= num_of_ref)
-    for ref in refs:
-        print(f"Adding {ref}")
-        db_subset_session.merge(ref)
-    print("Be patient the commit can take a wee while.")
+    referencetypes = db_orig_session.query(ReferencetypeModel).all()
+    for referencetype in referencetypes:
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {referencetype}")
+        db_subset_session.merge(referencetype)
     db_subset_session.commit()
+    db_subset_session.close()
+
+    mod_referencetypes = db_orig_session.query(ModReferencetypeAssociationModel).all()
+    for mod_referencetype in mod_referencetypes:
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {mod_referencetype}")
+        db_subset_session.merge(mod_referencetype)
+    db_subset_session.commit()
+    db_subset_session.close()
+
+    add_references(db_orig_session, db_subset_session)
+
+    # BOB: Add alembic_version
 
     # Need to set the initial seq values else we cannot add any more entries,
     # which might be needed for testing the api.
@@ -128,9 +227,22 @@ def start():
         "copyright_license_copyright_license_id_seq"
     ]
     for seq in seq_list:
-        com = f"ALTER SEQUENCE {seq} RESTART WITH {num_of_ref+1000}"
+        com = f"ALTER SEQUENCE {seq} RESTART WITH {1000000}"
         db_subset_session.execute(com)
     db_subset_session.commit()
+
+    # Sanity checks
+    okay = True
+    tables = ['reference', 'cross_reference']  # add other tables
+    for table_name in tables:
+        count = db_subset_session.execute(f"SELECT count(1) from {table_name}")
+        if not count:
+            print(f"ERROR: No records found for table  {table_name}")
+            okay = False
+        if verbose:
+            print(f"COUNT: {table_name} -> {count}")
+    if not okay:
+        exit(-1)
 
 
 start()
