b0871e302f26d3b31995f3898490bde76da47300
Paul Hale <paul.hale@jax.org>
2023-04-11 16:39:24 -0600

    Variables

--- agr_literature_service/api/crud/reference_crud.py
+++ agr_literature_service/api/crud/reference_crud.py
@@ -572,30 +572,28 @@ def add_license(db: Session, curie: str, license: str):  # noqa
                             detail=f"Error adding license '{license}'")
     return {"message": "Update Success!"}
 
-def missing_files (db: Session, mod_id: int):
+def missing_files (db: Session, mod_abbreviation: string):
     try:
-        rs = db.execute(
-            """
-            SELECT reference.curie, short_citation, reference.date_created, MAINCOUNT, SUPCOUNT, ref_pmid.curie as PMID, ref_mod.curie AS mod_curie
-                FROM reference, citation,
-                	(SELECT b.reference_id, COUNT(1) FILTER (WHERE c.file_class = 'main') AS MAINCOUNT,
-                	COUNT(1) FILTER (WHERE c.file_class = 'supplement') AS SUPCOUNT
-                	FROM mod_corpus_association AS b
-                	LEFT JOIN referencefile AS c ON b.reference_id = c.reference_id
-                	WHERE b.mod_id = 2
-                	GROUP BY b.reference_id
-                	HAVING COUNT(1) FILTER (WHERE c.file_class = 'main') < 1
-                	OR COUNT(1) FILTER (WHERE c.file_class = 'supplement') < 1
-                	LIMIT 25)
-                	AS sub_select,
-                	(SELECT cross_reference.curie, reference_id FROM cross_reference where curie_prefix='PMID') as ref_pmid,
-                	(SELECT cross_reference.curie, reference_id FROM cross_reference where curie_prefix='FB') as ref_mod
-                WHERE sub_select.reference_id=reference.reference_id
-                AND sub_select.reference_id=ref_pmid.reference_id
-                AND sub_select.reference_id=ref_mod.reference_id
-                AND reference.citation_id=citation.citation_id
-            """
-        )
+        query = """SELECT reference.curie, short_citation, reference.date_created, MAINCOUNT, SUPCOUNT, ref_pmid.curie as PMID, ref_mod.curie AS mod_curie
+                    FROM reference, citation,
+                        (SELECT b.reference_id, COUNT(1) FILTER (WHERE c.file_class = 'main') AS MAINCOUNT,
+                        COUNT(1) FILTER (WHERE c.file_class = 'supplement') AS SUPCOUNT
+                        FROM mod_corpus_association AS b
+                        LEFT JOIN referencefile AS c ON b.reference_id = c.reference_id
+                        WHERE b.mod_id = {mod_id}
+                        GROUP BY b.reference_id
+                        HAVING COUNT(1) FILTER (WHERE c.file_class = 'main') < 1
+                        OR COUNT(1) FILTER (WHERE c.file_class = 'supplement') < 1
+                        LIMIT 25)
+                        AS sub_select,
+                        (SELECT cross_reference.curie, reference_id FROM cross_reference where curie_prefix='PMID') as ref_pmid,
+                        (SELECT cross_reference.curie, reference_id FROM cross_reference where curie_prefix='FB') as ref_mod
+                    WHERE sub_select.reference_id=reference.reference_id
+                    AND sub_select.reference_id=ref_pmid.reference_id
+                    AND sub_select.reference_id=ref_mod.reference_id
+                    AND reference.citation_id=citation.citation_id
+                """.format(mod_id=2)
+        rs = db.execute(query)
         rows = rs.fetchall()
         data = jsonable_encoder(rows)
     except Exception:
