e4fa27d41165a3bb38a7ba5bc9cbe35da5547009
Paul Hale <paul.hale@jax.org>
2023-04-11 17:17:08 -0600

    Will f strings work?

--- agr_literature_service/api/crud/reference_crud.py
+++ agr_literature_service/api/crud/reference_crud.py
@@ -574,13 +574,14 @@ def add_license(db: Session, curie: str, license: str):  # noqa
 
 def missing_files (db: Session, mod_abbreviation: str):
     try:
-        query = """SELECT reference.curie, short_citation, reference.date_created, MAINCOUNT, SUPCOUNT, ref_pmid.curie as PMID, ref_mod.curie AS mod_curie
+        mod_id=2
+        query = f"""SELECT reference.curie, short_citation, reference.date_created, MAINCOUNT, SUPCOUNT, ref_pmid.curie as PMID, ref_mod.curie AS mod_curie
                     FROM reference, citation,
                         (SELECT b.reference_id, COUNT(1) FILTER (WHERE c.file_class = 'main') AS MAINCOUNT,
                         COUNT(1) FILTER (WHERE c.file_class = 'supplement') AS SUPCOUNT
                         FROM mod_corpus_association AS b
                         LEFT JOIN referencefile AS c ON b.reference_id = c.reference_id
-                        WHERE b.mod_id = 2
+                        WHERE b.mod_id = {mod_id}
                         GROUP BY b.reference_id
                         HAVING COUNT(1) FILTER (WHERE c.file_class = 'main') < 1
                         OR COUNT(1) FILTER (WHERE c.file_class = 'supplement') < 1
