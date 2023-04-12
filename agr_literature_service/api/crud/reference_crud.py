9318c39c2a9deb642e6ad52cf21e3f9d36dcd5da
Paul Hale <paul.hale@jax.org>
2023-04-12 08:42:27 -0600

    Hardcode for now

--- agr_literature_service/api/crud/reference_crud.py
+++ agr_literature_service/api/crud/reference_crud.py
@@ -574,6 +574,7 @@ def add_license(db: Session, curie: str, license: str):  # noqa
 
 def missing_files (db: Session, mod_abbreviation: str):
     try:
+        mod_abbreviation='MGI'
         query = f"""SELECT reference.curie, short_citation, reference.date_created, MAINCOUNT, SUPCOUNT, ref_pmid.curie as PMID, ref_mod.curie AS mod_curie
                     FROM reference, citation,
                     	(SELECT b.reference_id, COUNT(1) FILTER (WHERE c.file_class = 'main') AS MAINCOUNT,
