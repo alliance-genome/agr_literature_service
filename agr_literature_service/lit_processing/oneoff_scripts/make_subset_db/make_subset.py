eb6f0c599e6b09b1ed1e153523f79cb002699d01
ianlongden <ianlongden@morgan.harvard.edu>
2023-04-18 11:42:47 -0400

    Lots of code repition removed. Added resources for the test set references. Made more modular.

--- agr_literature_service/lit_processing/oneoff_scripts/make_subset_db/make_subset.py
+++ agr_literature_service/lit_processing/oneoff_scripts/make_subset_db/make_subset.py
@@ -5,15 +5,29 @@ from os import environ, system
 import argparse
 
 from agr_literature_service.api.models import (
-    ModModel,
     UserModel,
+    ModModel,
     ReferenceModel,
     ResourceModel,
-    ModReferencetypeAssociationModel,
     ReferencetypeModel,
+    ModReferencetypeAssociationModel,
     CopyrightLicenseModel
 
 )
+
+# NOTE: Order is important here.
+# i.e. ModModel have to be loaded before ModReferencetypeAssociationModel
+# as it references it.
+data_needed_before_reference = [
+    UserModel,
+    ModModel,
+    ReferencetypeModel,
+    ModReferencetypeAssociationModel,
+    CopyrightLicenseModel
+]
+
+# curies in curie order. Please retain this order to
+# make them easier to find.
 test_reference_curies = [
     'AGRKB:101000000656561',
     'AGRKB:101000000650977',
@@ -27,7 +41,38 @@ test_reference_curies = [
     'AGRKB:101000000872479',
     'AGRKB:101000000865324']
 
+trigger_list = ['reference', 'resource', 'author', 'cross_reference']
+
+seq_list = [
+    "author_author_id_seq",
+    "cross_reference_id_seq",
+    "editor_editor_id_seq",
+    "mesh_detail_mesh_detail_id_seq",
+    "mod_corpus_association_mod_corpus_association_id_seq",
+    "mod_mod_id_seq",
+    "mod_referencetype_mod_referencetype_id_seq",
+    "mod_taxon_mod_taxon_id_seq",
+    "obsolete_reference_curie_obsolete_id_seq",
+    "reference_comments_and_correc_reference_comment_and_correct_seq",
+    "reference_mod_md5sum_reference_mod_md5sum_id_seq",
+    "reference_mod_referencetype_reference_mod_referencetype_id_seq",
+    "reference_reference_id_seq",
+    "referencefile_mod_referencefile_mod_id_seq",
+    "referencefile_referencefile_id_seq",
+    "referencetype_referencetype_id_seq",
+    "resource_descriptor_pages_resource_descriptor_pages_id_seq",
+    "resource_descriptors_resource_descriptor_id_seq",
+    "resource_resource_id_seq",
+    "topic_entity_tag_prop_topic_entity_tag_prop_id_seq",
+    "topic_entity_tag_topic_entity_tag_id_seq",
+    "transaction_id_seq",
+    "workflow_tag_reference_workflow_tag_id_seq",
+    "copyright_license_copyright_license_id_seq",
+    "citation_citation_id_seq"
+]
+
 ALL_OUTPUT = 2  # For verbosity
+
 helptext = r"""
     env PSQL_XXX used for Target subset db details.
     env ORIG_XXX used for source reference db details.
@@ -42,7 +87,7 @@ parser.add_argument('-l', '--limit', help='Limit to x references', type=int, def
 parser.add_argument('-v', '--verbose', help="verbosity, minumum=0, headlines=1, full=2,", type=int, default=1, required=False)
 parser.add_argument('-d', '--dump_dir', help='Directory to dump schemas and dbs to', type=str, required=True)
 parser.add_argument('-f', '--full_orig_dump', help='Dump the original database in full as well', type=bool, default=False, required=False)
-parser.add_argument('-s', '--subset_dump', help='Dump the new subset database.', type=bool, default=True, required=False)
+parser.add_argument('-s', '--subset_dump', help='Dump the new subset database.', type=bool, default=False, required=False)
 args = parser.parse_args()
 
 num_of_refs = args.limit
@@ -181,18 +226,18 @@ def add_references(db_orig_session, db_subset_session):
         subqueryload(ReferenceModel.workflow_tag),
         subqueryload(ReferenceModel.citation)
     ).filter(ReferenceModel.reference_id <= num_of_refs)
+    count = 0
     for ref in refs:
         if verbose == ALL_OUTPUT:
-            print(f"Adding {ref}")
-        elif verbose:
             print(f"Adding {ref.curie} {ref.reference_id}")
         db_subset_session.merge(ref)
+        count += 1
+    if verbose:
+        print(f"Added {count} records for References.")
 
 
 def add_specific_test_references(db_orig_session, db_subset_session):
     global verbose, test_reference_curies
-
-    # curies in curie order. Please retain this order.
 
     if verbose:
         print("Adding SET test references")
@@ -208,81 +253,110 @@ def add_specific_test_references(db_orig_session, db_subset_session):
         subqueryload(ReferenceModel.workflow_tag),
         subqueryload(ReferenceModel.citation)
     ).filter(ReferenceModel.curie.in_(test_reference_curies))
+    count = 0
     for ref in refs:
         if verbose == ALL_OUTPUT:
-            print(f"Adding {ref}")
-        elif verbose:
             print(f"Adding {ref.curie} {ref.reference_id}")
         db_subset_session.merge(ref)
+        count += 1
+    if verbose:
+        print(f"Added {count} records for Test Set references.")
 
 
-def start():  # noqa
+def add_sequence_data(db_subset_session):
+    # Need to set the initial seq values else we cannot add any more entries,
+    # which might be needed for testing the api.
+    global seq_list
+    for seq in seq_list:
+        com = f"ALTER SEQUENCE {seq} RESTART WITH {1000000}"
+        db_subset_session.execute(com)
+    db_subset_session.commit()
+
+
+def add_alembic(db_orig_session, db_subset_session):
+    # Add alembic_version
+    alembic_rows = db_orig_session.execute("SELECT version_num from alembic_version")
+    version = ''
+    for alembic_row in alembic_rows:
+        version = alembic_row[0]
+    if not version:
+        print("ERROR: Could not find version_num for alembic_version table")
+    else:
+        db_subset_session.execute(f"INSERT into alembic_version (version_num) VALUES ('{version}');")
+    db_subset_session.commit()
+    db_subset_session.close()
+
+
+def trigger_settings(db_session, state="DISABLE"):
+    global trigger_list
+    for table in trigger_list:
+        db_session.execute(f'ALTER TABLE {table} {state} TRIGGER all;')
+    db_session.commit()
+
+
+def add_full_table_data(db_orig_session, db_subset_session, model):
+    records = db_orig_session.query(model)
+    count = 0
+    for record in records:
+        count += 1
+        if verbose == ALL_OUTPUT:
+            print(f"Adding record {record}")
+        db_subset_session.merge(record)
+    if verbose:
+        print(f"Added {count} records for {model.__name__}.")
+    db_subset_session.commit()
+    db_subset_session.close()
+
+
+def load_data_needed_before_reference(db_orig_session, db_subset_session):
+    global data_needed_before_reference
+    for modclass in data_needed_before_reference:
+        add_full_table_data(db_orig_session, db_subset_session, modclass)
+
+
+def load_resources(db_orig_session, db_subset_session):
+    """
+    Too many resources to load so only load those needed by
+    filtering on the reference_id.
+    Note: no commit here, we load the references first as
+          resources may take a little while too.
+    """
+    global test_reference_curies
+    resources = db_orig_session.query(ResourceModel).join(ReferenceModel).filter(ReferenceModel.reference_id <= num_of_refs)
+    count = 0
+    loaded = []
+    for res in resources:
+        count += 1
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {res}")
+        db_subset_session.merge(res)
+        loaded.append(res.resource_id)
+
+    # We also need to load the resources for the test set
+    ress = db_orig_session.query(ResourceModel).join(ReferenceModel)\
+        .filter(ReferenceModel.curie.in_(test_reference_curies))
+    for res in ress:
+        if verbose == ALL_OUTPUT:
+            print(f"Adding {res}")
+        if res.resource_id not in loaded:
+            db_subset_session.merge(res)
+            count += 1
+    if verbose:
+        print(f"Added {count} Resources")
+
+
+def start():
     global test_reference_curies
     db_orig_session = create_postgres_session(True, source=True)
 
     db_subset_session = create_postgres_session(True)
 
     # remove the triggers while loading.
-    db_subset_session.execute('ALTER TABLE reference DISABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE author DISABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE resource DISABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE cross_reference DISABLE TRIGGER all;')
+    trigger_settings(db_subset_session, state="DISABLE")
 
-    users = db_orig_session.query(UserModel)
-    for user in users:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding user {user.id} {user.email}")
-        db_subset_session.merge(user)
-    db_subset_session.commit()
-    db_subset_session.close()
+    load_data_needed_before_reference(db_orig_session, db_subset_session)
 
-    mods = db_orig_session.query(ModModel)
-    for mod in mods:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding mod {mod}")
-        db_subset_session.merge(mod)
-    db_subset_session.commit()
-    db_subset_session.close()
-
-    resources = db_orig_session.query(ResourceModel).join(ReferenceModel).filter(ReferenceModel.reference_id <= num_of_refs)
-    for res in resources:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding {res}")
-        db_subset_session.merge(res)
-    db_subset_session.commit()
-    db_subset_session.close()
-
-    copyrightlicenses = db_orig_session.query(CopyrightLicenseModel).join(ReferenceModel).filter(
-        ReferenceModel.reference_id <= num_of_refs)
-    for copy in copyrightlicenses:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding {copy}")
-        db_subset_session.merge(copy)
-    db_subset_session.commit()
-    db_subset_session.close()
-
-    referencetypes = db_orig_session.query(ReferencetypeModel).all()
-    for referencetype in referencetypes:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding {referencetype}")
-        db_subset_session.merge(referencetype)
-    db_subset_session.commit()
-    db_subset_session.close()
-
-    mod_referencetypes = db_orig_session.query(ModReferencetypeAssociationModel).all()
-    for mod_referencetype in mod_referencetypes:
-        print(f"Adding {mod_referencetype}")
-        db_subset_session.merge(mod_referencetype)
-    db_subset_session.commit()
-    db_subset_session.close()
-
-    mod_referencetypes = db_orig_session.query(ModReferencetypeAssociationModel).all()
-    for mod_referencetype in mod_referencetypes:
-        if verbose == ALL_OUTPUT:
-            print(f"Adding {mod_referencetype}")
-        db_subset_session.merge(mod_referencetype)
-    db_subset_session.commit()
-    db_subset_session.close()
+    load_resources(db_orig_session, db_subset_session)
 
     # add the references, one set by a count and other from preset list.
     add_references(db_orig_session, db_subset_session)
@@ -291,51 +365,10 @@ def start():  # noqa
     db_subset_session.commit()
 
     # Add alembic_version
-    alembic_rows = db_orig_session.execute("SELECT version_num from alembic_version")
-    version = ''
-    for alembic_row in alembic_rows:
-        version = alembic_row[0]
-    if not version:
-        print("ERROR: Could not find version_num for alembic_version table")
-        okay = False
-    else:
-        db_subset_session.execute(f"INSERT into alembic_version (version_num) VALUES ('{version}');")
-    db_subset_session.commit()
-    db_subset_session.close()
+    add_alembic(db_orig_session, db_subset_session)
 
-    # Need to set the initial seq values else we cannot add any more entries,
-    # which might be needed for testing the api.
-    seq_list = [
-        "author_author_id_seq",
-        "cross_reference_id_seq",
-        "editor_editor_id_seq",
-        "mesh_detail_mesh_detail_id_seq",
-        "mod_corpus_association_mod_corpus_association_id_seq",
-        "mod_mod_id_seq",
-        "mod_referencetype_mod_referencetype_id_seq",
-        "mod_taxon_mod_taxon_id_seq",
-        "obsolete_reference_curie_obsolete_id_seq",
-        "reference_comments_and_correc_reference_comment_and_correct_seq",
-        "reference_mod_md5sum_reference_mod_md5sum_id_seq",
-        "reference_mod_referencetype_reference_mod_referencetype_id_seq",
-        "reference_reference_id_seq",
-        "referencefile_mod_referencefile_mod_id_seq",
-        "referencefile_referencefile_id_seq",
-        "referencetype_referencetype_id_seq",
-        "resource_descriptor_pages_resource_descriptor_pages_id_seq",
-        "resource_descriptors_resource_descriptor_id_seq",
-        "resource_resource_id_seq",
-        "topic_entity_tag_prop_topic_entity_tag_prop_id_seq",
-        "topic_entity_tag_topic_entity_tag_id_seq",
-        "transaction_id_seq",
-        "workflow_tag_reference_workflow_tag_id_seq",
-        "copyright_license_copyright_license_id_seq",
-        "citation_citation_id_seq"
-    ]
-    for seq in seq_list:
-        com = f"ALTER SEQUENCE {seq} RESTART WITH {1000000}"
-        db_subset_session.execute(com)
-    db_subset_session.commit()
+    # add sequence data. What value to use next in a sequence
+    add_sequence_data(db_subset_session)
 
     # Sanity checks
     okay = True
@@ -354,11 +387,7 @@ def start():  # noqa
             okay = False
 
     # Add the triggers back.
-    db_subset_session.execute('ALTER TABLE reference ENABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE author ENABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE resource ENABLE TRIGGER all;')
-    db_subset_session.execute('ALTER TABLE cross_reference ENABLE TRIGGER all;')
-    db_subset_session.commit()
+    trigger_settings(db_subset_session, state="ENABLE")
 
     if subset_dump:
         dump_subset()
