bc03c6c74d0cb766311bdbc68842dde548fe32af
sweng66 <sweng@stanford.edu>
2023-05-15 16:40:18 -0700

    fixed the author/reference triggers so they will be firsed only certain columns got changed

--- agr_literature_service/api/triggers/reference_sql_func_triggers.py
+++ agr_literature_service/api/triggers/reference_sql_func_triggers.py
@@ -11,15 +11,42 @@ BEGIN
   END;
 $$;
 """
-reference_update_trigger = """
-DROP TRIGGER IF EXISTS reference_citation_trigger on public.reference;
-CREATE TRIGGER reference_citation_trigger
-AFTER UPDATE OR INSERT ON reference
-        FOR EACH ROW
-        EXECUTE FUNCTION public.reference_update_citation();
+
+# reference_update_trigger = """
+# DROP TRIGGER IF EXISTS reference_citation_trigger on public.reference;
+# CREATE TRIGGER reference_citation_trigger
+# AFTER UPDATE OR INSERT ON reference
+#        FOR EACH ROW
+#        EXECUTE FUNCTION public.reference_update_citation();
+# """
+
+reference_citation_insert_trigger = """
+DROP TRIGGER IF EXISTS reference_citation_insert_trigger on public.reference;
+CREATE TRIGGER reference_citation_insert_trigger
+AFTER INSERT ON reference
+    FOR EACH ROW
+    EXECUTE FUNCTION public.reference_update_citation();
+"""
+
+reference_citation_update_trigger = """
+DROP TRIGGER IF EXISTS reference_citation_update_trigger on public.reference;
+CREATE TRIGGER reference_citation_update_trigger
+AFTER UPDATE ON reference
+    FOR EACH ROW
+    WHEN (
+        COALESCE(OLD.title, '') IS DISTINCT FROM COALESCE(NEW.title, '') OR
+        COALESCE(OLD.volume, '') IS DISTINCT FROM COALESCE(NEW.volume, '') OR
+        COALESCE(OLD.issue_name, '') IS DISTINCT FROM COALESCE(NEW.issue_name, '') OR
+        COALESCE(OLD.page_range, '') IS DISTINCT FROM COALESCE(NEW.page_range, '') OR
+        COALESCE(OLD.date_published, '') IS DISTINCT FROM COALESCE(NEW.date_published, '') OR
+        COALESCE(OLD.resource_id, 0) IS DISTINCT FROM COALESCE(NEW.resource_id, 0)
+    )
+    EXECUTE FUNCTION public.reference_update_citation();
 """
 
 
 def add_reference_triggers(db_session):
     db_session.execute(reference_update_function)
-    db_session.execute(reference_update_trigger)
+    # db_session.execute(reference_update_trigger)
+    db_session.execute(reference_citation_insert_trigger)
+    db_session.execute(reference_citation_update_trigger)
