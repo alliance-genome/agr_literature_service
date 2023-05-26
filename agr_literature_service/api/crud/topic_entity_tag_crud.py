37851bcdb3ae0f8638dba45e45567fef00cef2bc
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-05-26 09:55:57 -0700

    fix(topic_entity_tag): fixed indentation

--- agr_literature_service/api/crud/topic_entity_tag_crud.py
+++ agr_literature_service/api/crud/topic_entity_tag_crud.py
@@ -159,7 +159,7 @@ def get_map_entity_curie_to_name(db: Session, curie_or_reference_id: str, token:
             all_topics_and_entities.append(tag.entity_type)
             all_entities[tag.entity_type].append(tag.entity)
     entity_curie_to_name = get_map_ateam_curies_to_names(curies_category="atpterm", curies=all_topics_and_entities,
-                                                     token=token)
+                                                         token=token)
     for atpterm_curie in all_entities.keys():
         entity_curie_to_name.update(get_map_ateam_curies_to_names(curies_category=entity_curie_to_name[atpterm_curie],
                                                                   curies=all_entities[atpterm_curie],
