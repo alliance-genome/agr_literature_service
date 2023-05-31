4e7c965a3c1e5c70188caac55f00fd5f4637b57b
sweng66 <sweng@stanford.edu>
2023-05-31 11:52:07 -0700

    adding filter argument to missing_files

--- agr_literature_service/api/routers/reference_router.py
+++ agr_literature_service/api/routers/reference_router.py
@@ -198,8 +198,9 @@ def add_license(curie: str,
 def missing_files(mod_abbreviation: str,
                   order_by: str,
                   page: int,
+                  filter: str,
                   db: Session = db_session):
-    return reference_crud.missing_files(db, mod_abbreviation, order_by, page)
+    return reference_crud.missing_files(db, mod_abbreviation, order_by, page, filter)
 
 
 @router.get('/download_tracker_table/{mod_abbreviation}',
