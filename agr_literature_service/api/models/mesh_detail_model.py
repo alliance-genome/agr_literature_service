2d48e4c9b56b4aeb35aa03cbf3e11dcd153c8097
ianlongden <ianlongden@morgan.harvard.edu>
2023-07-17 10:15:37 -0400

    :remove mesh_detail_version

--- agr_literature_service/api/models/mesh_detail_model.py
+++ agr_literature_service/api/models/mesh_detail_model.py
@@ -9,15 +9,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from agr_literature_service.api.database.base import Base
-from agr_literature_service.api.database.versioning import enable_versioning
-
-
-enable_versioning()
 
 
 class MeshDetailModel(Base):
     __tablename__ = "mesh_detail"
-    __versioned__: Dict = {}
 
     mesh_detail_id = Column(
         Integer,
