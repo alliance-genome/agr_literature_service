bcebd522a3e4d8aec8d0509a98097c1d988f7f6d
Pinglei Zhou <zhou@morgan.harvard.edu>
2023-08-10 10:56:37 -0400

    remove file delete part

--- agr_literature_service/lit_processing/data_export/dump_database.py
+++ agr_literature_service/lit_processing/data_export/dump_database.py
@@ -2,8 +2,8 @@ import argparse
 import logging
 from os import environ
 from dotenv import load_dotenv
-from agr_literature_service.lit_processing.utils.s3_utils import upload_file_to_s3, delete_file_from_s3, file_exist_from_s3
-from datetime import timedelta, date
+from agr_literature_service.lit_processing.utils.s3_utils import upload_file_to_s3
+from datetime import date
 import os
 
 logging.basicConfig(format='%(message)s')
@@ -28,7 +28,6 @@ def dump_database(dump_type="ondemand"):  # noqa: C901
     username = environ.get('PSQL_USERNAME', "")
     password = environ.get('PSQL_PASSWORD', "")
     port = environ.get('PSQL_PORT', "")
-    file_name_date_7 = database + "_" + str(date.today() - timedelta(days=7)) + ".sql"
     file_name = database + "_" + str(date.today()) + ".sql"
     cmd = "PGPASSWORD=" + password + " pg_dump -Fc --clean -h " + host + " -p " + port + " -U " + username + " " + database + "   > " + file_name
     log.info(cmd)
@@ -38,7 +37,7 @@ def dump_database(dump_type="ondemand"):  # noqa: C901
         upload_file_to_s3(file_name, s3_bucket, s3_filename)
         ## upload file to monthly bucket if it is first day of the month
         todayDate = date.today()
-        if todayDate.day == 1:
+        if todayDate.day == 10:
             s3_filename_monthly = monthly_bucket + file_name
             upload_file_to_s3(file_name, s3_bucket, s3_filename_monthly, 'GLACIER_IR')
         # delete local file after upload to s3
@@ -47,13 +46,6 @@ def dump_database(dump_type="ondemand"):  # noqa: C901
         except OSError:
             log.info("fail to delete local file")
             return False
-
-        ## delete day7 file from lastweek folder
-        s3_filename_day7 = lastweek_bucket + file_name_date_7
-        file_date_7_exist = file_exist_from_s3(s3_bucket, s3_filename_day7)
-        if file_date_7_exist:
-            delete_file_from_s3(s3_bucket, s3_filename_day7)
-
     return True
 
 
