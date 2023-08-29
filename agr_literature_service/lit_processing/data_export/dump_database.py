198c03a7bf34c59c804443fd5b8c04b901a3af6e
Pinglei Zhou <zhou@morgan.harvard.edu>
2023-08-29 14:52:40 -0400

    modify dump cron

--- agr_literature_service/lit_processing/data_export/dump_database.py
+++ agr_literature_service/lit_processing/data_export/dump_database.py
@@ -3,7 +3,7 @@ import logging
 from os import environ
 from dotenv import load_dotenv
 from agr_literature_service.lit_processing.utils.s3_utils import upload_file_to_s3
-from datetime import date
+from datetime import datetime
 import os
 
 logging.basicConfig(format='%(message)s')
@@ -28,10 +28,11 @@ def dump_database(dump_type="ondemand"):  # noqa: C901
     username = environ.get('PSQL_USERNAME', "")
     password = environ.get('PSQL_PASSWORD', "")
     port = environ.get('PSQL_PORT', "")
-    file_name = database + "_" + str(date.today()) + ".sql"
+    now = datetime.now()  # current date and time
+    file_name = database + "_" + now.strftime("%Y-%m-%d-%H-%M-%S")+ ".sql"
     cmd = "PGPASSWORD=" + password + " pg_dump -Fc --clean -h " + host + " -p " + port + " -U " + username + " " + database + "   > " + file_name
     os.system(cmd)
-    if dump_type == 'cron':
+    if dump_type == 'cron' and env_state == 'prod' and 'prod' in database:
         s3_filename = lastweek_bucket + file_name
         upload_file_to_s3(file_name, s3_bucket, s3_filename)
         ## upload file to monthly bucket if it is first day of the month
