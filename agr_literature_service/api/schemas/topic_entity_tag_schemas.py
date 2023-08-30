e35282525c8eafbb51483d0ce0a379fb1f8522dd
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-08-30 16:17:15 -0700

    fix(topic_entity_tag): Add field constraints to topic_entity_tag_schemas.py
    This change introduces the use of `pydantic` constraints, specifically `Field` and `constr`, to the schema definitions within `topic_entity_tag_schemas.py`. Previously, fields of the schema classes could accept empty strings or exclusively white spaces, which led to improper utilization of the database and reduced data integrity. By placing a minimum length requirement on each string-based field, we ensure all data being pushed to the database is non-trivial and supports our application's use cases. The updated unittest reflects this enhancement by asserting that creating entries with empty strings now results in HTTP 422 status.

--- agr_literature_service/api/schemas/topic_entity_tag_schemas.py
+++ agr_literature_service/api/schemas/topic_entity_tag_schemas.py
@@ -1,13 +1,13 @@
 from typing import Optional, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, constr
 from agr_literature_service.api.schemas import AuditedObjectModelSchema
 
 
 class TopicEntityTagSourceSchemaCreate(AuditedObjectModelSchema):
-    source_type: str
-    source_method: str
-    validation_type: Optional[str] = None
-    evidence: str
+    source_type: str = Field(..., min_length=1)
+    source_method: str = Field(..., min_length=1)
+    validation_type: Optional[constr(min_length=1)] = None
+    evidence: str = Field(..., min_length=1)
     description: str
     mod_abbreviation: str
 
@@ -17,16 +17,16 @@ class TopicEntityTagSourceSchemaShow(TopicEntityTagSourceSchemaCreate):
 
 
 class TopicEntityTagSourceSchemaUpdate(BaseModel):
-    source_type: Optional[str]
-    source_method: Optional[str]
-    validation_type: Optional[str]
-    evidence: Optional[str]
-    description: Optional[str]
-    mod_abbreviation: Optional[str]
-    date_created: Optional[str]
-    date_updated: Optional[str]
-    created_by: Optional[str]
-    updated_by: Optional[str]
+    source_type: Optional[constr(min_length=1)]
+    source_method: Optional[constr(min_length=1)]
+    validation_type: Optional[constr(min_length=1)]
+    evidence: Optional[constr(min_length=1)]
+    description: Optional[constr(min_length=1)]
+    mod_abbreviation: Optional[constr(min_length=1)]
+    date_created: Optional[constr(min_length=1)]
+    date_updated: Optional[constr(min_length=1)]
+    created_by: Optional[constr(min_length=1)]
+    updated_by: Optional[constr(min_length=1)]
 
     class Config:
         orm_mode = True
@@ -34,17 +34,17 @@ class TopicEntityTagSourceSchemaUpdate(BaseModel):
 
 
 class TopicEntityTagSchemaCreate(AuditedObjectModelSchema):
-    topic: str
-    entity_type: Optional[str] = None
-    entity: Optional[str] = None
-    entity_source: Optional[str] = None
-    entity_published_as: Optional[str] = None
-    species: Optional[str] = None
-    display_tag: Optional[str] = None
+    topic: str = Field(..., min_length=1)
+    entity_type: Optional[constr(min_length=1)] = None
+    entity: Optional[constr(min_length=1)] = None
+    entity_source: Optional[constr(min_length=1)] = None
+    entity_published_as: Optional[constr(min_length=1)] = None
+    species: Optional[constr(min_length=1)] = None
+    display_tag: Optional[constr(min_length=1)] = None
     topic_entity_tag_source_id: int
     negated: Optional[bool] = False
-    confidence_level: Optional[str] = None
-    note: Optional[str] = None
+    confidence_level: Optional[constr(min_length=1)] = None
+    note: Optional[constr(min_length=1)] = None
 
 
 class TopicEntityTagSchemaPost(TopicEntityTagSchemaCreate):
@@ -74,13 +74,13 @@ class TopicEntityTagSchemaShow(TopicEntityTagSchemaRelated):
 
 
 class TopicEntityTagSchemaUpdate(AuditedObjectModelSchema):
-    topic: Optional[str] = None
-    entity_type: Optional[str] = None
-    entity: Optional[str] = None
-    entity_source: Optional[str] = None
-    entity_published_as: Optional[str] = None
-    species: Optional[str] = None
-    display_tag: Optional[str] = None
+    topic: Optional[constr(min_length=1)] = None
+    entity_type: Optional[constr(min_length=1)] = None
+    entity: Optional[constr(min_length=1)] = None
+    entity_source: Optional[constr(min_length=1)] = None
+    entity_published_as: Optional[constr(min_length=1)] = None
+    species: Optional[constr(min_length=1)] = None
+    display_tag: Optional[constr(min_length=1)] = None
     negated: Optional[bool] = False
-    confidence_level: Optional[str] = None
-    note: Optional[str] = None
+    confidence_level: Optional[constr(min_length=1)] = None
+    note: Optional[constr(min_length=1)] = None
