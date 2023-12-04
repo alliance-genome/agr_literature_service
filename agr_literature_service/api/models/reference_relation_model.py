e4eff4dae0b22f756d1c263a35ef396f8a637ab8
Pinglei Zhou <zhou@morgan.harvard.edu>
2023-12-04 15:22:48 -0500

    fix reference_relation_model

--- agr_literature_service/api/models/reference_relation_model.py
+++ agr_literature_service/api/models/reference_relation_model.py
@@ -54,7 +54,7 @@ class ReferenceRelationModel(Base):
         back_populates="reference_relation_in"
     )
 
-    reference_relations_type = Column(
+    reference_relation_type = Column(
         Enum(ReferenceRelationType),
         unique=False,
         nullable=False
