568b2e996339754cae07f0fda6884bc181aa2115
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-01-25 17:33:44 +0100

    Fixed return values of mock functions

--- tests/api/test_topic_entity_tag.py
+++ tests/api/test_topic_entity_tag.py
@@ -418,11 +418,9 @@ class TestTopicEntityTag:
 
     def test_validate_positive_with_pos_and_neg(self, test_topic_entity_tag, test_reference, test_mod,  # noqa
                                                 auth_headers, db, test_topic_entity_tag_source):  # noqa
-        with TestClient(app) as client:
-            class Mock(object):
-                pass
-            mock_get_ancestors = Mock()
-            mock_get_descendants = Mock()
+        with TestClient(app) as client, \
+                patch("agr_literature_service.api.crud.topic_entity_tag_crud.get_ancestors") as mock_get_ancestors, \
+                patch("agr_literature_service.api.crud.topic_entity_tag_crud.get_descendants") as mock_get_descendants:
             author_source = {
                 "source_type": "manual",
                 "source_method": "ACKnowledge",
@@ -494,13 +492,15 @@ class TestTopicEntityTag:
                                                  'ATP:0000084', 'ATP:0000085', 'ATP:0000086', 'ATP:0000087',
                                                  'ATP:0000100'}
             client.post(url="/topic_entity_tag/", json=more_generic_positive_tag, headers=auth_headers).json()
-            mock_get_ancestors.return_value = {'ATP:0000001', 'ATP:0000002', 'ATP:0000009', 'ATP:0000079',
-                                               'ATP:0000082'}
-            mock_get_descendants.return_value = {'ATP:0000082'}
+            mock_get_ancestors.return_value = {'ATP:0000001', 'ATP:0000002', 'ATP:0000009'}
+            mock_get_descendants.return_value = {'ATP:0000009', 'ATP:0000033', 'ATP:0000034', 'ATP:0000079',
+                                                 'ATP:0000080', 'ATP:0000081', 'ATP:0000082', 'ATP:0000083',
+                                                 'ATP:0000084', 'ATP:0000085', 'ATP:0000086', 'ATP:0000087',
+                                                 'ATP:0000100'}
             more_generic_negative_tag_id = client.post(url="/topic_entity_tag/", json=more_generic_negative_tag,
                                                        headers=auth_headers).json()["topic_entity_tag_id"]
-            mock_get_ancestors.return_value = {'ATP:0000001', 'ATP:0000002', 'ATP:0000009', 'ATP:0000079'}
-            mock_get_descendants.return_value = {'ATP:0000079', 'ATP:0000082', 'ATP:0000083', 'ATP:0000084'}
+            mock_get_ancestors.return_value = {'ATP:0000001', 'ATP:0000002', 'ATP:0000009', 'ATP:0000079', 'ATP:0000082'}
+            mock_get_descendants.return_value = {'ATP:0000082'}
             more_specific_positive_id = client.post(url="/topic_entity_tag/", json=more_specific_positive_tag,
                                                     headers=auth_headers).json()["topic_entity_tag_id"]
             mock_get_ancestors.return_value = {'ATP:0000001', 'ATP:0000002', 'ATP:0000009', 'ATP:0000079'}
