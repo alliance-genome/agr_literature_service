cb17ee0b5ee4ab9d7f9f4c16870a3b341666b922
ianlongden <ianlongden@morgan.harvard.edu>
2024-06-14 12:11:19 -0400

    change atp root node and flake8 niceness

--- agr_literature_service/lit_processing/oneoff_scripts/6755b395f397-SCRUM-4112.py
+++ agr_literature_service/lit_processing/oneoff_scripts/6755b395f397-SCRUM-4112.py
@@ -30,7 +30,7 @@ def populate_data():
             cmd = "INSERT INTO workflow_transition (mod_id, transition_from, transition_to, date_created) agr_literature_service/lit_processing/oneoff_scripts/6755b395f397-SCRUM-4112.py"
             cmd += f"VALUES ({mod.mod_id}, '{name_to_atp[transition[0]]}', '{name_to_atp[transition[1]]}', '{datetime.now(tz=pytz.timezone('UTC'))}')"
             db_session.execute(cmd)
-            log.info("%s(%s): Inserting transition %s to %s",mod.abbreviation, mod.mod_id, name_to_atp[transition[0]], name_to_atp[transition[1]])
+            log.info("%s(%s): Inserting transition %s to %s", mod.abbreviation, mod.mod_id, name_to_atp[transition[0]], name_to_atp[transition[1]])
     db_session.rollback()
     # db_session.commit()
     db_session.close()
