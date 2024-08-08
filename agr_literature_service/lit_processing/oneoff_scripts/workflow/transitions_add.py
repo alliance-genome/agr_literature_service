3a5f9485addd2b86e497fca9514cffcb7fea831a
ianlongden <ianlongden@morgan.harvard.edu>
2024-08-08 08:32:16 -0400

    Allow turning off of the print statements and a bit more docs

--- agr_literature_service/lit_processing/oneoff_scripts/workflow/transitions_add.py
+++ agr_literature_service/lit_processing/oneoff_scripts/workflow/transitions_add.py
@@ -1,5 +1,16 @@
 """
-Add subtask transition.
+Add transitions.
+
+i.e.
+   python3 -d transitions_add.py -f file_upload -v 1
+
+   data files (methods that return the data to insert into the table) are in the
+   directory 'data'.
+   If new transitions are added then update existing files or create a new file and
+   import it here (See # Data files) and add an elif (see # Add new data files here with appropriate elif)
+
+    get_name_to_atp_and_children method maybe useful outside the workflow module,
+    so we may want to move this into the api at some point.
 """
 import json
 from fastapi_okta.okta_utils import get_authentication_token
@@ -15,15 +26,17 @@ from agr_literature_service.lit_processing.utils.sqlalchemy_utils import create_
 
 from agr_literature_service.api.models import WorkflowTransitionModel
 
+# Data files
 from agr_literature_service.lit_processing.oneoff_scripts.workflow.data.file_upload import get_data as file_upload
 logger = logging.getLogger(__name__)
 name_to_atp = {}
 atp_to_name = {}
 mod_ids = {}
 
-helptext = "--filename ."
+helptext = "--filename file1 -debug"
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=helptext)
-parser.add_argument('-f', '--filename', help='Filename to be processed', type=str, required=True)
+parser.add_argument('-f', '--filename', help='Filename to be processed.', type=str, required=True)
+parser.add_argument('-v', '--verbose', help='Print a lot of info during run.', default=False, type=bool, required=False)
 args = parser.parse_args()
 
 
@@ -39,7 +52,7 @@ def load_mod_abbr(db):
         print('Error: ' + str(type(e)))
 
 
-def get_name_to_atp_and_children(token, curie='ATP:0000177'):
+def get_name_to_atp_and_children(token, debug, curie='ATP:0000177'):
     """
     Add data to atp_to_name and name_to_atp dictionaries.
     From the top curie given go down all children and store the data.
@@ -62,19 +75,21 @@ def get_name_to_atp_and_children(token, curie='ATP:0000177'):
             resp_obj = json.loads(resp)
             for bob in resp_obj:
                 for jane in resp_obj[bob]:
-                    print(f"\t {jane['curie']} - {jane['name']}")
+                    if debug:
+                        print(f"\t {jane['curie']} - {jane['name']}")
                     name_to_atp[jane['name']] = jane['curie']
                     atp_to_name[jane['curie']] = jane['name']
-                    get_name_to_atp_and_children(token=token, curie=jane['curie'])
+                    get_name_to_atp_and_children(token=token, curie=jane['curie'], debug=debug)
     except HTTPError:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="Error from A-team API")
 
 
-def add_transitions(db: Session, filename: str):
+def add_transitions(db: Session, filename: str, debug: bool = False):
     global name_to_atp
     global mod_ids
 
+    # Add new data files here with appropriate elif
     if filename == "file_upload":
         data_to_add = file_upload(name_to_atp)
     else:
@@ -84,10 +99,8 @@ def add_transitions(db: Session, filename: str):
         if 'mod' in transition:
             if transition['mod'] == 'ALL':
                 mod_list = list(mod_ids.keys())
-                print(mod_list)
             elif transition['mod'].startswith("NOT_"):
                 for mod in mod_ids.keys():
-                    print(f" {type(mod)} {mod}")
                     if mod != transition['mod'][4:]:
                         mod_list.append(mod)
             else:
@@ -104,12 +117,16 @@ def add_transitions(db: Session, filename: str):
             except KeyError:
                 print(f"ERROR: {transition['to']} is not found in name_to_atp")
                 exit(-1)
-            print(f"mod_abbreviation: {mod_abbr} -> mod_id {mod_ids[mod_abbr]}")
             wft = db.query(WorkflowTransitionModel).\
                 filter(WorkflowTransitionModel.mod_id == mod_ids[mod_abbr],
                        WorkflowTransitionModel.transition_from == trans_from,
                        WorkflowTransitionModel.transition_to == trans_to).one_or_none()
+            if wft:
+                if debug:
+                    print(f"Transition found, will update actions and condition only for {wft}")
             if not wft:
+                if debug:
+                    print(f"Adding new wft {wft}")
                 wft = WorkflowTransitionModel(mod_id=mod_ids[mod_abbr],
                                               transition_from=trans_from,
                                               transition_to=trans_to)
@@ -118,21 +135,21 @@ def add_transitions(db: Session, filename: str):
                 wft.actions = transition['action']
             if 'condition' in transition:
                 wft.condition = transition['condition']
-            print(f"pre commit: {wft}")
     db.commit()
-    wfts = db.query(WorkflowTransitionModel).all()
-    for wft in wfts:
-        print(f"Final: {wft}")
+    if debug:
+        wfts = db.query(WorkflowTransitionModel).all()
+        for wft in wfts:
+            print(f"Final: {wft}")
 
 
 if __name__ == "__main__":
     auth_token = get_authentication_token()
     if not auth_token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    get_name_to_atp_and_children(token=auth_token)
+    get_name_to_atp_and_children(token=auth_token, debug=args.verbose)
     engine = create_postgres_engine(False)
     db_connection = engine.connect()
     db_session: Session = create_postgres_session(False)
 
     load_mod_abbr(db_session)
-    add_transitions(db_session, args.filename)
+    add_transitions(db_session, args.filename, args.verbose)
