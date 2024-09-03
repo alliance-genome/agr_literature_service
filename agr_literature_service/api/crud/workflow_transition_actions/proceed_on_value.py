6346df521cce7b632a1cf43f93bee9f5ea0c66f1
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2024-09-03 16:05:09 -0700

    Checking if checktype is 'reference_type' and not 'reference'

--- agr_literature_service/api/crud/workflow_transition_actions/proceed_on_value.py
+++ agr_literature_service/api/crud/workflow_transition_actions/proceed_on_value.py
@@ -27,7 +27,7 @@ def proceed_on_value(db: Session, current_workflow_tag_db_obj: WorkflowTagModel,
     if checktype == "category":  # Check reference category is "Research article"
         if current_workflow_tag_db_obj.reference.category == check_value:
             call_process = True
-    elif checktype == "reference":
+    elif checktype == "reference_type":
         # reference types are not loaded by default so we have to grab them manually
         select = f"""
         select rt.label
