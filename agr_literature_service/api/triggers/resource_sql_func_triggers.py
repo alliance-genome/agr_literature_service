a8f3b0e4787a58c4ba8c4f7f0cbd8fc3e660bdd1
sweng66 <sweng@stanford.edu>
2024-10-08 17:07:15 -0700

    adding commit()

--- agr_literature_service/api/triggers/resource_sql_func_triggers.py
+++ agr_literature_service/api/triggers/resource_sql_func_triggers.py
@@ -32,3 +32,4 @@ AFTER UPDATE ON resource
 def add_resource_triggers(db_session):
     db_session.execute(text(resource_update_function))
     db_session.execute(text(resource_update_trigger))
+    db_session.commit()
