90d574f7ed4a635c0c1a0ae3edcd007ae4e8df94
sweng66 <sweng@stanford.edu>
2024-10-16 20:13:17 -0700

    adding 'entity extraction' WF to workflow_transition table

---
+++ agr_literature_service/lit_processing/oneoff_scripts/workflow/data/entity_extraction.py
@@ -0,0 +1,82 @@
+"""
+
+entity extraction (ATP:0000172)
+
+    entity extraction complete (ATP:0000174)
+        allele extraction complete (ATP:0000215)
+        antibody extraction complete (ATP:0000196)
+        gene extraction complete (ATP:0000214)
+        species extraction complete (ATP:0000203)
+        strain extraction complete (ATP:0000250)
+        transgenic allele extraction complete (ATP:0000251)
+
+    entity extraction failed (ATP:0000187)
+        allele extraction failed (ATP:0000217)
+        antibody extraction failed (ATP:0000188)
+        gene extraction failed (ATP:0000216)
+        species extraction failed (ATP:0000204)
+        transgenic allele extraction failed (ATP:0000267)
+
+    entity extraction in progress (ATP:0000190)
+        allele extraction in progress (ATP:0000219)
+        antibody extraction in progress (ATP:0000195)
+        gene extraction in progress (ATP:0000218)
+        species extraction in progress (ATP:0000205)
+        transgenic allele extraction in progress (ATP:0000268)
+
+    entity extraction needed (ATP:0000173)
+        allele extraction needed (ATP:0000221)
+        antibody extraction needed (ATP:0000175)
+        gene extraction needed (ATP:0000220)
+        species extraction needed (ATP:0000206)
+        transgenic allele extraction needed (ATP:0000269)
+
+"""
+
+
+def get_data(name_to_atp):
+    """
+    mod can only be 'ALL', the actual mod abbreviation or 'NOT_' + mod abbreviation
+    i.e. ALL, WB, NOT_FB are three examples.
+    """
+    test_data = []
+    # for each XXX extraction needed for job control
+    for entry in ('allele', 'antibody', 'gene', 'species', 'transgenic allele', 'strain'):
+        if entry == "strain":
+            item = {
+                'mod': 'ALL',
+                'from': 'entity extraction',
+                'to': f'{entry} extraction complete',
+                'condition': 'on_success'
+            }
+            test_data.append(item)
+            continue
+        item = {
+            'mod': 'ALL',
+            'from': 'entity extraction needed',
+            'to': f'{entry} extraction needed',
+            'condition': f'{entry}_extraction_job'
+        }
+        test_data.append(item)
+        item = {
+            'mod': 'ALL',
+            'from': f'{entry} extraction needed',
+            'to': f'{entry} extraction in progress',
+            'condition': 'on_start'
+        }
+        test_data.append(item)
+        item = {
+            'mod': 'ALL',
+            'from': f'{entry} extraction in progress',
+            'to': f'{entry} extraction failed',
+            'condition': 'on_failed'
+        }
+        test_data.append(item)
+        item = {
+            'mod': 'ALL',
+            'from': f'{entry} extraction in progress',
+            'to': f'{entry} extraction complete',
+            'condition': 'on_success'
+        }
+        test_data.append(item)
+    return test_data
