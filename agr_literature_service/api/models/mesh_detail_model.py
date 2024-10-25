ffd95fb424ac5805f11badeddd327b3545227b3c
Pinglei Zhou <zhou@morgan.harvard.edu>
2024-10-24 20:58:38 -0400

    no versioned for mesh_detail

--- agr_literature_service/api/models/mesh_detail_model.py
+++ agr_literature_service/api/models/mesh_detail_model.py
@@ -5,19 +5,13 @@ mesh_detail_model.py
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
 from agr_literature_service.api.database.base import Base
-from agr_literature_service.api.database.versioning import enable_versioning
-from typing import Dict
-
-enable_versioning()
 
 
 class MeshDetailModel(Base):
     __tablename__ = "mesh_detail"
     __bind_key__ = 'lit'
     __table_args__ = {"schema": "lit"}
-    __versioned__: Dict = {'schema': 'lit', 'inherit': True}
 
     mesh_detail_id = Column(
         Integer,
