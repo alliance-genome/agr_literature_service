6db1ead307fef996ec951f39aab1670d5aef65f2
sweng66 <sweng@stanford.edu>
2024-11-01 15:16:46 -0700

    adding a new utils file containing functions for entity validation

---
+++ agr_literature_service/api/crud/topic_entity_id_mapping_utils.py
@@ -0,0 +1,156 @@
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from fastapi import HTTPException, status
+
+# TODO 1: pass in mod_abbreviation or to get this list from database
+curie_prefix_list = [
+    "FB",
+    "MGI",
+    "RGD",
+    "SGD",
+    "WB",
+    "XenBase",
+    "ZFIN"
+]
+# TODO 2: optimize sqls/add indexes if needed
+# TODO 3: adding endpoint for searching topic (for TET page autocomplete)
+# TODO 4: adding endpoint for searching species (for TET page autocomplete)
+# TODO 5: adding function for name to id mapping (for TET table display)
+# TODO 6: adding caching for name to id mapping
+
+
+def map_entity_to_curie(db: Session, entity_type, entity_list):
+
+    entity_type = entity_type.lower()
+    (entity_name_list, entity_curie_list) = classify_entity_list(entity_list)
+
+    entity_curie_rows = search_for_entity_curies(db, entity_type, entity_curie_list)
+    entity_name_rows = search_for_entity_names(db, entity_type, entity_name_list)
+
+    if entity_curie_rows is None or entity_name_rows is None:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Unknown entity_type '{entity_type}'"
+        )
+
+    data = [
+        {
+            "entity_curie": row[0],
+            "is_obsolete": row[1],
+            "entity": row[2]
+        }
+        for row in (entity_curie_rows or []) + (entity_name_rows or [])
+    ]
+    json_data = jsonable_encoder(data)
+    return JSONResponse(content=json_data)
+
+
+def classify_entity_list(entity_list):
+
+    entity_name_list = []
+    entity_curie_list = []
+    for entity in entity_list.replace("+", " ").split("|"):
+        is_mod_curie = False
+        for curie_prefix in curie_prefix_list:
+            if entity.startswith(curie_prefix + ":"):
+                is_mod_curie = True
+                break
+        if is_mod_curie:
+            entity_curie_list.append(entity.upper())
+        else:
+            entity_name_list.append(entity.upper())
+    return (entity_name_list, entity_curie_list)
+
+
+def search_for_entity_names(db: Session, entity_type, entity_name_list):
+    if len(entity_name_list) == 0:
+        return []
+    sql_query = None
+    if entity_type == 'gene':
+        # gene symbol: ACT1
+        # systematic name: YFL039C
+        # genomic feature: CEN1
+        sql_query = text("""
+        SELECT distinct be.modentityid, sa.obsolete, sa.displaytext
+        FROM biologicalentity be
+        JOIN slotannotation sa ON be.id = sa.singlegene_id
+        WHERE sa.slotannotationtype in (
+            'GeneSymbolSlotAnnotation',
+            'GeneSystematicNameSlotAnnotation',
+            'GeneFullNameSlotAnnotation'
+        )
+        AND UPPER(sa.displaytext) IN :entity_name_list
+        """)
+    elif entity_type == 'allele':
+        # for 'allele' and 'transgenic allele'
+        sql_query = text("""
+        SELECT DISTINCT be.modentityid, sa.obsolete, sa.displaytext
+        FROM biologicalentity be
+        JOIN slotannotation sa ON be.id = sa.singleallele_id
+        WHERE sa.slotannotationtype = 'AlleleSymbolSlotAnnotation'
+        AND UPPER(sa.displaytext) IN :entity_name_list
+        """)
+    elif entity_type in ['agms', 'strain', 'genotype', 'fish']:
+        sql_query = text("""
+        SELECT DISTINCT be.modentityid, be.obsolete, agm.name
+        FROM biologicalentity be
+        JOIN affectedgenomicmodel agm ON be.id = agm.id
+        WHERE UPPER(agm.name) IN :entity_name_list
+        """)
+    elif entity_type == 'construct':
+        sql_query = text("""
+        SELECT DISTINCT r.modentityid, sa.obsolete, sa.displaytext
+        FROM reagent r
+        JOIN slotannotation sa ON r.id = sa.singleconstruct_id
+        WHERE sa.slotannotationtype in (
+            'ConstructFullNameSlotAnnotation',
+            'ConstructSymbolSlotAnnotation'
+        )
+        AND UPPER(sa.displaytext) IN :entity_name_list
+        """)
+    elif entity_type == 'species':
+        sql_query = text("""
+        SELECT DISTINCT curie, obsolete, name
+        FROM ontologyterm
+        WHERE name IN :entity_name_list
+        OR curie in :entity_name_list
+        """)
+    else:
+        return None
+    rows = db.execute(sql_query, {'entity_name_list': tuple(entity_name_list)}).fetchall()
+
+    return rows
+
+
+def search_for_entity_curies(db: Session, entity_type, entity_curie_list):
+    if len(entity_curie_list) == 0:
+        return []
+    sql_query = None
+    if entity_type in ['gene', 'allele']:
+        entity_table_name = entity_type
+        sql_query = text(f"""
+        SELECT DISTINCT be.modentityid, be.obsolete, be.modentityid
+        FROM biologicalentity be, {entity_table_name} ent_tbl
+        WHERE be.id = ent_tbl.id
+        AND UPPER(be.modentityid) IN :entity_curie_list
+        """)
+    elif entity_type == 'construct':
+        sql_query = text("""
+        SELECT DISTINCT r.modentityid, r.obsolete, r.modentityid
+        FROM reagent r, construct c
+        WHERE r.id = c.id
+        AND UPPER(r.modentityid) IN :entity_curie_list
+        """)
+    elif entity_type in ['agms', 'strain', 'genotype', 'fish']:
+        sql_query = text("""
+        SELECT DISTINCT be.modentityid, be.obsolete, be.modentityid
+        FROM biologicalentity be, affectedgenomicmodel agm
+        WHERE be.id = agm.id
+        AND UPPER(be.modentityid) IN :entity_curie_list
+        """)
+    else:
+        return None
+    rows = db.execute(sql_query, {'entity_curie_list': tuple(entity_curie_list)}).fetchall()
+    return rows
