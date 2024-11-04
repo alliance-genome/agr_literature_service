8404d38fe907fe715c89bf69b6f1c40ed7a962ff
sweng66 <sweng@stanford.edu>
2024-11-03 19:35:24 -0800

    adding 'search_topic', 'search_species', 'map_curies_to_names', etc functions

--- agr_literature_service/api/crud/topic_entity_id_mapping_utils.py
+++ agr_literature_service/api/crud/topic_entity_id_mapping_utils.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text
 from fastapi import HTTPException, status
 
-# TODO 1: pass in mod_abbreviation or to get this list from database
+# TODO 1: get this list from database
 curie_prefix_list = [
     "FB",
     "MGI",
@@ -19,13 +19,13 @@ curie_prefix_list = [
 # TODO 4: adding caching for name to id mapping
 
 
-def map_entity_to_curie(db: Session, entity_type, entity_list):
+def map_entity_to_curie(db: Session, entity_type, entity_list, taxon):
 
     entity_type = entity_type.lower()
     (entity_name_list, entity_curie_list) = classify_entity_list(entity_list)
 
     entity_curie_rows = search_for_entity_curies(db, entity_type, entity_curie_list)
-    entity_name_rows = search_for_entity_names(db, entity_type, entity_name_list)
+    entity_name_rows = search_for_entity_names(db, entity_type, entity_name_list, taxon)
 
     if entity_curie_rows is None or entity_name_rows is None:
         raise HTTPException(
@@ -62,7 +62,7 @@ def classify_entity_list(entity_list):
     return (entity_name_list, entity_curie_list)
 
 
-def search_for_entity_names(db: Session, entity_type, entity_name_list):
+def search_for_entity_names(db: Session, entity_type, entity_name_list, taxon):
     if len(entity_name_list) == 0:
         return []
     sql_query = None
@@ -74,12 +74,14 @@ def search_for_entity_names(db: Session, entity_type, entity_name_list):
         SELECT distinct be.modentityid, sa.obsolete, sa.displaytext
         FROM biologicalentity be
         JOIN slotannotation sa ON be.id = sa.singlegene_id
+        JOIN ontologyterm ot ON be.taxon_id = ot.id
         WHERE sa.slotannotationtype in (
             'GeneSymbolSlotAnnotation',
             'GeneSystematicNameSlotAnnotation',
             'GeneFullNameSlotAnnotation'
         )
         AND UPPER(sa.displaytext) IN :entity_name_list
+        AND ot.curie = :taxon
         """)
     elif entity_type == 'allele':
         # for 'allele' and 'transgenic allele'
@@ -87,8 +89,10 @@ def search_for_entity_names(db: Session, entity_type, entity_name_list):
         SELECT DISTINCT be.modentityid, sa.obsolete, sa.displaytext
         FROM biologicalentity be
         JOIN slotannotation sa ON be.id = sa.singleallele_id
+        JOIN ontologyterm ot ON be.taxon_id = ot.id
         WHERE sa.slotannotationtype = 'AlleleSymbolSlotAnnotation'
         AND UPPER(sa.displaytext) IN :entity_name_list
+        AND ot.curie = :taxon
         """)
         ## using subquery doesn't help in this case
         ## exection time is similar
@@ -99,7 +103,7 @@ def search_for_entity_names(db: Session, entity_type, entity_name_list):
         #    SELECT singleallele_id, obsolete, displaytext
         #    FROM slotannotation
         #    WHERE slotannotationtype = 'AlleleSymbolSlotAnnotation'
-        #    AND UPPER(displaytext) IN :entity_name_list
+        #    AND UPPER(displaytext) IN :exntity_name_list
         # ) AS sa ON be.id = sa.singleallele_id;
         # """)
     elif entity_type in ['agms', 'strain', 'genotype', 'fish']:
@@ -107,7 +111,9 @@ def search_for_entity_names(db: Session, entity_type, entity_name_list):
         SELECT DISTINCT be.modentityid, be.obsolete, agm.name
         FROM biologicalentity be
         JOIN affectedgenomicmodel agm ON be.id = agm.id
+        JOIN ontologyterm ot ON be.taxon_id = ot.id
         WHERE UPPER(agm.name) IN :entity_name_list
+        AND ot.curie = :taxon
         """)
     elif entity_type == 'construct':
         sql_query = text("""
@@ -129,7 +135,7 @@ def search_for_entity_names(db: Session, entity_type, entity_name_list):
         """)
     else:
         return None
-    rows = db.execute(sql_query, {'entity_name_list': tuple(entity_name_list)}).fetchall()
+    rows = db.execute(sql_query, {'entity_name_list': tuple(entity_name_list), 'taxon': taxon}).fetchall()
     return rows
 
 
@@ -226,3 +232,73 @@ def search_species(db: Session, species):
     ]
     json_data = jsonable_encoder(data)
     return JSONResponse(content=json_data)
+
+
+def map_atp_id_to_name(db: Session, atp_id):
+
+    sql_query = text("""
+    SELECT name
+    FROM ontologyterm
+    WHERE ontologytermtype = 'ATPTerm'
+    AND curie = :atp_id
+    """)
+    row = db.execute(sql_query, {'atp_id', atp_id}).fetchone()
+    if row:
+        return row[0]
+    return None
+
+
+def map_curies_to_names(db: Session, category, curies):
+
+    if not curies:
+        return {}
+
+    if category.startswith('ATP:'):
+        category = map_atp_id_to_name(db, category)
+        if category is None:
+            return {curie: curie for curie in curies}
+    category = category.lower()
+    sql_query = None
+    if category == 'gene':
+        sql_query = text("""
+        SELECT be.modentityid, sa.displaytext
+        FROM biologicalentity be
+        JOIN slotannotation sa ON be.id = sa.singlegene_id
+        WHERE be.modentityid IN :curies
+        AND sa.slotannotationtype = 'GeneSymbolSlotAnnotation'
+        """)
+    elif 'allele' in category:
+        sql_query = text("""
+        SELECT be.modentityid, sa.displaytext
+        FROM biologicalentity be
+        JOIN slotannotation sa ON be.id = sa.singleallele_id
+        WHERE be.modentityid IN :curies
+        AND sa.slotannotationtype = 'AlleleSymbolSlotAnnotation'
+        """)
+    elif category in ['affected genome model', 'strain', 'genotype', 'fish']:
+        sql_query = text("""
+        SELECT DISTINCT be.modentityid, agm.name
+        FROM biologicalentity be
+        JOIN affectedgenomicmodel agm ON be.id = agm.id
+        WHERE be.modentityid IN :curies
+        """)
+    elif 'construct' in category:
+        sql_query = text("""
+        SELECT r.modentityid, sa.displaytext
+        FROM reagent r
+        JOIN slotannotation sa ON r.id = sa.singleconstruct_id
+        WHERE r.modentityid IN :curies
+        AND sa.slotannotationtype = 'ConstructSymbolSlotAnnotation'
+        """)
+    elif category in ['species', 'atpterm', 'ecoterm']:
+        curies = [curie.upper() for curie in curies]
+        sql_query = text("""
+        SELECT curie, name
+        FROM ontologyterm
+        WHERE UPPER(curie) IN :curies
+        """)
+    else:
+        return {curie: curie for curie in curies}
+    rows = db.execute(sql_query, {'curies': tuple(curies)}).fetchall()
+    curie_to_name_map = {row[0]: row[1] for row in rows}
+    return curie_to_name_map
