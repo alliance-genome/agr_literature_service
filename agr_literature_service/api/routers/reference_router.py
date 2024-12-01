3b1a896ed8def0a2798f03facb1bba19e374c712
sweng66 <sweng@stanford.edu>
2024-12-01 14:56:16 -0800

    adding a new endpoint to retrieve new references from ABC

--- agr_literature_service/api/routers/reference_router.py
+++ agr_literature_service/api/routers/reference_router.py
@@ -269,3 +269,13 @@ def add_to_corpus(mod_abbreviation: str,
 
     set_global_user_from_okta(db, user)
     return reference_crud.add_to_corpus(db, mod_abbreviation, reference_curie)
+
+
+@router.get('/new_references/{mod_abbreviation}',
+            status_code=status.HTTP_200_OK)
+def new_references(mod_abbreviation: str,
+                   days: int = 7,
+                   db: Session = db_session):
+    references = reference_crud.new_references(db, mod_abbreviation, days)
+
+    return references
