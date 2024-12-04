74720902394d2762d6bee2c34f62ed8fde36d67d
sweng66 <sweng@stanford.edu>
2024-12-04 15:22:17 -0800

    adding code to avoid mod curie generation conflict when multiple people trying to add new paper/generate mod ID at the same time

--- agr_literature_service/api/crud/cross_reference_crud.py
+++ agr_literature_service/api/crud/cross_reference_crud.py
@@ -46,9 +46,15 @@ def create(db: Session, cross_reference) -> int:
             error_details = f"Error details: {str(orig_args[0])}"
         else:
             error_details = f"Error details: {str(e)}"
+        if "duplicate key value violates unique constraint" in error_details and "idx_curie" in error_details:
+            return error_details
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail=f"Cannot add cross reference with curie {cross_reference_data['curie']}. {error_details}"
+            detail=(
+                f"Cannot add cross-reference with CURIE {cross_reference_data['curie']}. "
+                f"{error_details}. This may be due to another curator working on the same paper. "
+                "Please reload the page and try again. Thank you."
+            )
         )
     return int(db_obj.cross_reference_id)
 
@@ -147,46 +153,69 @@ def check_xref_and_generate_mod_id(db: Session, reference_obj: ReferenceModel, m
              CrossReferenceModel.is_obsolete.is_(False),
              CrossReferenceModel.curie_prefix == mod_abbreviation)).order_by(
         CrossReferenceModel.is_obsolete).first()
-    if not cross_reference:
-        env_state = os.environ.get("ENV_STATE", "")
-        if env_state == "prod":
-            ## do not create MOD IDs for prod at the momemt
-            return
-        if mod_abbreviation == 'WB':
-            new_wbpaper_number = 1
-            cross_reference = db.query(CrossReferenceModel.curie).filter(
-                and_(CrossReferenceModel.curie.startswith("WB:WBPaper0"),
-                     CrossReferenceModel.curie_prefix == mod_abbreviation)).order_by(
-                CrossReferenceModel.curie.desc()).first()
-            if cross_reference:
-                new_wbpaper_number = int(cross_reference.curie[11:]) + 1
-            new_wbpaper_string = str(new_wbpaper_number).zfill(8)
-            new_wbpaper_curie = f"WB:WBPaper{new_wbpaper_string}"
-            new_wbpaper_xref = {
-                "curie": new_wbpaper_curie,
-                "pages": [
-                    "reference"
-                ],
-                "reference_curie": reference_obj.curie
-            }
-            create(db, new_wbpaper_xref)
-        elif mod_abbreviation == 'SGD':
-            new_sgdid_number = 100000001
-            cross_reference = db.query(CrossReferenceModel.curie).filter(
-                and_(CrossReferenceModel.curie.startswith("SGD:S100"),
-                     CrossReferenceModel.curie_prefix == mod_abbreviation)).order_by(
-                CrossReferenceModel.curie.desc()).first()
-            if cross_reference:
-                new_sgdid_number = int(cross_reference.curie[5:]) + 1
-            new_sgdid = f"SGD:S{new_sgdid_number}"
-            new_xref = {
-                "curie": new_sgdid,
-                "pages": [
-                    "reference"
-                ],
-                "reference_curie": reference_obj.curie
-            }
-            create(db, new_xref)
+    if cross_reference:
+        return
+    env_state = os.environ.get("ENV_STATE", "")
+    if env_state == "prod":
+        ## do not create MOD IDs for prod at the momemt
+        return
+    if mod_abbreviation not in ['WB', 'SGD']:
+        return
+    create_status = None
+    """
+    To make sure the function works correctly when multiple users are simultaneously
+    adding papers and generating MOD IDs.
+    """
+    for i in range(5):
+        new_mod_curie = generate_new_mod_curie(db, mod_abbreviation, reference_obj.curie)
+        create_status = create(db, new_mod_curie)
+        if str(create_status).isdigit():  # valid status found
+            return create_status
+    # If no valid status is returned after 5 attempts
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"Cannot add cross-reference after 5 attempts. {create_status}"
+    )
+
+
+def generate_new_mod_curie(db: Session, mod_abbreviation, ref_curie):
+
+    if mod_abbreviation == 'WB':
+        new_wbpaper_number = 1
+        cross_reference = db.query(CrossReferenceModel.curie).filter(
+            and_(CrossReferenceModel.curie.startswith("WB:WBPaper0"),
+                 CrossReferenceModel.curie_prefix == mod_abbreviation)).order_by(
+            CrossReferenceModel.curie.desc()).first()
+        if cross_reference:
+            new_wbpaper_number = int(cross_reference.curie[11:]) + 1
+        new_wbpaper_string = str(new_wbpaper_number).zfill(8)
+        new_wbpaper_curie = f"WB:WBPaper{new_wbpaper_string}"
+        new_wbpaper_xref = {
+            "curie": new_wbpaper_curie,
+            "pages": [
+                "reference"
+            ],
+            "reference_curie": ref_curie
+        }
+        return new_wbpaper_xref
+
+    if mod_abbreviation == 'SGD':
+        new_sgdid_number = 100000001
+        cross_reference = db.query(CrossReferenceModel.curie).filter(
+            and_(CrossReferenceModel.curie.startswith("SGD:S100"),
+                 CrossReferenceModel.curie_prefix == mod_abbreviation)).order_by(
+            CrossReferenceModel.curie.desc()).first()
+        if cross_reference:
+            new_sgdid_number = int(cross_reference.curie[5:]) + 1
+        new_sgdid = f"SGD:S{new_sgdid_number}"
+        new_xref = {
+            "curie": new_sgdid,
+            "pages": [
+                "reference"
+            ],
+            "reference_curie": ref_curie
+        }
+        return new_xref
 
 
 def set_mod_curie_to_invalid(db, reference_id, mod_abbreviation):
