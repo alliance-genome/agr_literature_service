732ebcf743fd7a904204522dc7e915c91e2e6de7
sweng66 <sweng@stanford.edu>
2024-12-09 09:53:14 -0800

    removed prepublication_pipeline paper creation test for sgd

--- tests/api/test_sort.py
+++ tests/api/test_sort.py
@@ -68,31 +68,6 @@ class TestSort:
             if new_curie.startswith('"') and new_curie.endswith('"'):
                 curie_pp_nopmid_wb = new_curie[1:-1]
             curie_pp_nopmid_wb_bool = False
-
-            reference_create_json = {
-                "cross_references": [
-                    {
-                        "curie": "PMID:1111",
-                        "is_obsolete": "false"
-                    }
-                ],
-                "mod_corpus_associations": [
-                    {
-                        "mod_abbreviation": "SGD",
-                        "mod_corpus_sort_source": "prepublication_pipeline",
-                        "corpus": "true"
-                    }
-                ],
-                "title": "pp pmid sgd",
-                "prepublication_pipeline": "true"
-            }
-            response = client.post(url="/reference/", json=reference_create_json, headers=auth_headers)
-            assert response.status_code == status.HTTP_201_CREATED
-            new_curie = response.text
-            if new_curie.startswith('"') and new_curie.endswith('"'):
-                curie_pp_pmid_sgd = new_curie[1:-1]
-            curie_pp_pmid_sgd_bool = False
-
             reference_create_json = {
                 "cross_references": [
                     {
