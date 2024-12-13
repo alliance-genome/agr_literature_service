3fadb9b34f0385077904b14173141c6c5e1c0183
Juancarlos <azurebrd@its.caltech.edu>
2024-12-13 04:30:21 +0000

    mypy didn't like the variable changing type

--- agr_literature_service/api/crud/workflow_tag_crud.py
+++ agr_literature_service/api/crud/workflow_tag_crud.py
@@ -632,8 +632,8 @@ def counters(db: Session, mod_abbreviation: str = None, workflow_process_atp_id:
 
     if date_range_start is not None and date_range_end is not None and date_range_start != "" and date_range_end != "":
         if isinstance(date_range_end, str):
-            date_range_end = datetime.strptime(date_range_end, "%Y-%m-%d")
-            new_timestamp = date_range_end + timedelta(days=1)
+            date_range_end_date = datetime.strptime(date_range_end, "%Y-%m-%d")
+            new_timestamp = date_range_end_date + timedelta(days=1)
             date_range_end = new_timestamp.strftime("%Y-%m-%d %H:%M:%S")
         if date_option == 'default' or date_option is None:
             where_clauses.append("wt.date_updated BETWEEN :start_date AND :end_date")
