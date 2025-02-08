cf65e585b4f6d7b33c0b188e978e515ea44ba431
pjhale <Paul.Hale@jax.org>
2025-02-08 14:57:09 -0700

    Bugs
    Fixing Router

--- agr_literature_service/api/routers/workflow_tag_router.py
+++ agr_literature_service/api/routers/workflow_tag_router.py
@@ -159,4 +159,4 @@ def get_report_workflow_tags(mod_abbreviation: str,
 @router.get('/workflow_diagram/{mod}',
             status_code=200)
 def get_report_workflow_diagram( mod: str, db: Session = db_session):
-    return workflow_tag_crud.get_workflow_tag_diagram(db)
+    return workflow_tag_crud.get_workflow_tag_diagram(mod,db)
