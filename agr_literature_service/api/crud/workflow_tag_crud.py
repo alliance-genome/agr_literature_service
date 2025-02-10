83821dc9a0e379c527bc7d694ee5bc8915975605
pjhale <Paul.Hale@jax.org>
2025-02-10 11:00:21 -0700

    Test
    Testing return again again x2

--- agr_literature_service/api/crud/workflow_tag_crud.py
+++ agr_literature_service/api/crud/workflow_tag_crud.py
@@ -53,11 +53,12 @@ def load_workflow_parent_children(root_node='ATP:0000177'):
 
 def get_workflow_tag_diagram(mod : str, db:Session):
     try:
-        query = f"""SELECT transition_to, ARRAY_AGG(transition_from)  FROM workflow_transition GROUP BY transition_to"""
+        #query = f"""SELECT transition_to, ARRAY_AGG(transition_from)  FROM workflow_transition GROUP BY transition_to"""
+        query = f"""SELECT * FROM workflow_transition"""
         rs = db.execute(text(query))
         rows = rs.fetchall()
-        ##data = jsonable_encoder(rows)
-        data = rows
+        data = jsonable_encoder(rows)
+        ##data = rows
     except Exception:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Cant search WF transition tag diagram.")
