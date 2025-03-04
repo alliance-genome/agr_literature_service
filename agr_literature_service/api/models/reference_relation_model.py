f2d7d442039a7600314afde700e8c27abbb800c2
sweng66 <sweng@stanford.edu>
2025-03-04 09:39:23 -0800

    adding uniq constraint back

--- agr_literature_service/api/models/reference_relation_model.py
+++ agr_literature_service/api/models/reference_relation_model.py
@@ -6,7 +6,7 @@ reference_relation_model.py
 
 from typing import Dict
 
-from sqlalchemy import Column, Enum, ForeignKey, Integer
+from sqlalchemy import Column, Enum, ForeignKey, Integer, Index, text
 from sqlalchemy.orm import relationship
 
 from agr_literature_service.api.database.base import Base
@@ -18,6 +18,14 @@ enable_versioning()
 
 class ReferenceRelationModel(Base):
     __tablename__ = "reference_relation"
+    __table_args__ = (
+        Index(
+            "idx_unique_reference_relation_pair",
+            text("LEAST(reference_id_from, reference_id_to)"),
+            text("GREATEST(reference_id_from, reference_id_to)"),
+            unique=True,
+        ),
+    )
     __versioned__: Dict = {}
 
     reference_relation_id = Column(
