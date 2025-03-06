82c20a92382a285b5ff9bec87ec077e8d549ba68
ianlongden <ianlongden@morgan.harvard.edu>
2025-03-06 13:46:01 -0500

    Allow passing of mod_abbr to search topic and deal with it acordingly

--- agr_literature_service/api/routers/topic_entity_tag_router.py
+++ agr_literature_service/api/routers/topic_entity_tag_router.py
@@ -214,8 +214,9 @@ def entity_validation(taxon: str,
 
 @router.get('/search_topic/{topic}',
             status_code=200)
-def search_topic(topic: str):
-    return ateam_db_helpers.search_topic(topic)
+def search_topic(topic: str,
+                 mod_abbr: str = None):
+    return ateam_db_helpers.search_topic(topic, mod_abbr)
 
 
 @router.get('/search_descendants/{ancestor_curie}',
