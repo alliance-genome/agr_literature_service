e57518287067070f06f44a597e98c7f5a57793e3
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2025-03-14 11:37:45 -0700

    feat(api): add support for downloading models without topic
    Enhanced the `download_model_file` endpoint to allow downloading models without specifying a topic. Updated function parameters and routing to make the `topic` argument optional for increased flexibility.

--- agr_literature_service/api/crud/ml_model_crud.py
+++ agr_literature_service/api/crud/ml_model_crud.py
@@ -144,7 +144,7 @@ def get_model_metadata(db: Session, task_type: str, mod_abbreviation: str, topic
     return get_model_schema_from_orm(model)
 
 
-def download_model_file(db: Session, task_type: str, mod_abbreviation: str, topic: str, version_num: int = None):
+def download_model_file(db: Session, task_type: str, mod_abbreviation: str, topic: str = None, version_num: int = None):
     mod = get_mod(db, mod_abbreviation)
     model = get_model(db, task_type, mod.mod_id, topic, version_num)
     folder = get_ml_model_s3_folder(task_type, mod_abbreviation, topic)
