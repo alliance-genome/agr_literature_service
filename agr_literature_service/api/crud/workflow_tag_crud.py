0b36eb6269944c5ba222a8e3cf540e968250e578
pjhale <Paul.Hale@jax.org>
2025-03-21 15:06:29 -0600

    Adding Names
    Adding the transition to names.  Testing again

--- agr_literature_service/api/crud/workflow_tag_crud.py
+++ agr_literature_service/api/crud/workflow_tag_crud.py
@@ -57,9 +57,9 @@ def get_workflow_tag_diagram(mod: str, db: Session):
         data = []
         tag_ids_to = db.query(WorkflowTransitionModel.transition_to).all()
         ##This needs a full list before mapping
-        tag_ids_from = (o.transition_from for o in tags)
+        tag_ids = (o.transition_from for o in tag_ids_to)
 
-        atp_curie_to_name = get_map_ateam_curies_to_names(category="atpterm", curies=tag_ids_from)
+        atp_curie_to_name = get_map_ateam_curies_to_names(category="atpterm", curies=tag_ids)
         for tag in tags:
             result = {}
             result['tag'] = tag.transition_from
