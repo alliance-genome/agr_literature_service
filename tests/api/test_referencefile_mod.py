6778a815a63e794b8ddd58a3bd04ef90d0c1e951
Pinglei Zhou <zhou@morgan.harvard.edu>
2025-04-07 15:39:14 -0400

    fix test

--- tests/api/test_referencefile_mod.py
+++ tests/api/test_referencefile_mod.py
@@ -75,9 +75,11 @@ class TestReferencefileMod:
                                            f"{test_referencefile_mod.new_referencefile_mod_id}")
             response_file = client.get(url=f"/reference/referencefile/{response_file_mod.json()['referencefile_id']}")
             response = client.get(url=f"/reference/referencefile/show_all/{response_file.json()['reference_curie']}")
-            print(response.json())
-            assert 0==1
-            assert response.json()[0]["referencefile_mods"][1]["mod_abbreviation"] == "WB"
+            ok=False
+            for mod in response.json():
+                if mod["referencefile_mods"][1]["mod_abbreviation"] == "WB":
+                    ok=True
+            assert ok
 
     def test_add_referencefile_to_mod(self, test_referencefile_mod, auth_headers): # noqa
         with TestClient(app) as client:
