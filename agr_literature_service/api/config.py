a75a07776fed33adeb9658d7a1de5c646d3ac6c6
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-28 16:36:28 -0400

    try this

--- agr_literature_service/api/config.py
+++ agr_literature_service/api/config.py
@@ -13,32 +13,32 @@ class GlobalConfig(BaseSettings):
     # shell environment variable having the same name, that will take precedence.
 
     # the class Field is necessary while defining the global variables
-    ENV_STATE: EnvStateSchema = 'ENV_STATE'
-    HOST: Optional[str] = 'HOST'
-    PROD_HOST: Optional[str] = 'HOST'
-    BUCKET_NAME: str = 'BUCKET_NAME'
+    ENV_STATE: EnvStateSchema = Field(..., validation_alias='ENV_STATE')
+    HOST: Optional[str] = Field(..., validation_alias='HOST')
+    PROD_HOST: Optional[str] = Field(..., validation_alias='HOST')
+    BUCKET_NAME: str = Field(..., validation_alias='BUCKET_NAME')
 
     # AWS Creds
-    AWS_SECRET_ACCESS_KEY: Optional[str] = 'AWS_SECRET_ACCESS_KEY'
-    AWS_ACCESS_KEY_ID: Optional[str] = 'AWS_ACCESS_KEY_ID'
+    AWS_SECRET_ACCESS_KEY: Optional[str] = Field(..., validation_alias='AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID: Optional[str] = Field(..., validation_alias='AWS_ACCESS_KEY_ID')
 
     # environment specific configs
     API_USERNAME: Optional[str] = None
     API_PASSWORD: Optional[str] = None
 
-    OKTA_DOMAIN: str = Field(..., env="OKTA_DOMAIN")
-    OKTA_API_AUDIENCE: str = Field(..., env="OKTA_API_AUDIENCE")
+    OKTA_DOMAIN: str = Field(..., validation_alias="OKTA_DOMAIN")
+    OKTA_API_AUDIENCE: str = Field(..., validation_alias="OKTA_API_AUDIENCE")
 
-    PSQL_USERNAME: str = Field(..., env="PSQL_USERNAME")
-    PSQL_PASSWORD: str = Field(..., env="PSQL_PASSWORD")
-    PSQL_HOST: str = Field(..., env="PSQL_HOST")
-    PSQL_PORT: str = Field(..., env="PSQL_PORT")
-    PSQL_DATABASE: str = Field(..., env="PSQL_DATABASE")
+    PSQL_USERNAME: str = Field(..., validation_alias="PSQL_USERNAME")
+    PSQL_PASSWORD: str = Field(..., validation_alias="PSQL_PASSWORD")
+    PSQL_HOST: str = Field(..., validation_alias="PSQL_HOST")
+    PSQL_PORT: str = Field(..., validation_alias="PSQL_PORT")
+    PSQL_DATABASE: str = Field(..., validation_alias="PSQL_DATABASE")
 
-    RESOURCE_DESCRIPTOR_URL: str = Field(..., env="RESOURCE_DESCRIPTOR_URL")
-    ELASTICSEARCH_HOST: str = Field(..., env="ELASTICSEARCH_HOST")
-    ELASTICSEARCH_PORT: str = Field(..., env="ELASTICSEARCH_PORT")
-    ELASTICSEARCH_INDEX: str = Field(..., env="ELASTICSEARCH_INDEX")
+    RESOURCE_DESCRIPTOR_URL: str = Field(..., validation_alias="RESOURCE_DESCRIPTOR_URL")
+    ELASTICSEARCH_HOST: str = Field(..., validation_alias="ELASTICSEARCH_HOST")
+    ELASTICSEARCH_PORT: str = Field(..., validation_alias="ELASTICSEARCH_PORT")
+    ELASTICSEARCH_INDEX: str = Field(..., validation_alias="ELASTICSEARCH_INDEX")
 
 
 config = GlobalConfig()
