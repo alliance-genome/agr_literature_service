5bab7595e8e7ee432ebdf96f71477ceddb8bf273
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-29 13:05:47 -0400

    hmmm

--- tests/api/test_workflow_tag.py
+++ tests/api/test_workflow_tag.py
@@ -34,6 +34,7 @@ def test_workflow_tag(db, auth_headers, test_reference, test_mod): # noqa
                   "workflow_tag_id": "ont1"
                   }
         print(f"curie: {test_reference.new_ref_curie}  mod_abbr: {test_mod.new_mod_abbreviation}")
+        print(f"mod: {test_mod}")
         response = client.post(url="/workflow_tag/", json=new_wt, headers=auth_headers)
         yield TestWTData(response, response.json(), test_reference.new_ref_curie, test_mod.new_mod_id,
                          test_mod.new_mod_abbreviation)
