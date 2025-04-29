8f0e734f25370b23f2a139c464f9c8300a2fd3d2
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-29 16:46:00 -0400

    add curation_status table

---
+++ agr_literature_service/api/models/curation_status_model.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from agr_literature_service.api.database.base import Base
+from agr_literature_service.api.models.audited_model import AuditedModel
+
+
+class CurationStatusModel(Base, AuditedModel):
+    __tablename__ = 'curation_status'
+
+    curation_status_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+
+    topic = Column(
+        String,
+        index=True,
+        nullable=False
+    )
+
+    reference_id = Column(
+        Integer,
+        ForeignKey("reference.reference_id", ondelete="CASCADE"),
+        nullable=False,
+        index=True
+    )
+
+    reference = relationship(
+        "ReferenceModel",
+        foreign_keys="CurationStatusModel.reference_id"
+    )
+
+    mod_id = Column(
+        Integer,
+        ForeignKey("mod.mod_id", ondelete="CASCADE"),
+        index=True,
+        nullable=False
+    )
+
+    mod = relationship(
+        "ModModel",
+        foreign_keys="CurationStatusModel.mod_id"
+    )
+
+    curation_status = Column(
+        String,
+        nullable=True
+    )
+
+    controlled_note = Column(
+        String,
+        nullable=True
+    )
+
+    note = Column(
+        String,
+        nullable=True
+    )
