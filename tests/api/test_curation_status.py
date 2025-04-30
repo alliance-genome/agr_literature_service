febb1e53c8d1d63a5084a9f7aa38ff5698c525b0
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-30 10:12:01 -0400

    fix test

--- tests/api/test_curation_status.py
+++ tests/api/test_curation_status.py
@@ -5,7 +5,7 @@ from starlette.testclient import TestClient
 from fastapi import status
 
 from agr_literature_service.api.main import app
-from agr_literature_service.api.models import CurationStatusModel
+# from agr_literature_service.api.models import CurationStatusModel
 from ..fixtures import db # noqa
 from .fixtures import auth_headers # noqa
 from .test_mod import test_mod # noqa
@@ -23,7 +23,8 @@ def test_curation_status(db, auth_headers, test_reference, test_mod): # noqa
             "reference_curie": test_reference.new_ref_curie
         }
         response = client.post(url="/curation_status/", json=new_curation_status, headers=auth_headers)
-        yield TestCurationStatusData(response, response.json()['new_curation_status_id'], test_reference.new_ref_curie)
+        yield TestCurationStatusData(response, response.json(), test_reference.new_ref_curie)
+
 
 class TestCurationStatus:
 
