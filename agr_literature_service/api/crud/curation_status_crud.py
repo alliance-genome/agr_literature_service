a45aad5e0222e854275d2c7cf8c289984c71189b
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-30 09:02:42 -0400

    Basic router, schema and crud added

---
+++ agr_literature_service/api/crud/curation_status_crud.py
@@ -0,0 +1,96 @@
+"""
+curation_status_crud.py
+=============
+"""
+
+from datetime import datetime
+
+
+from fastapi import HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from agr_literature_service.api.crud.reference_resource import add, create_obj, stripout
+from agr_literature_service.api.models import CurationStatusModel
+from agr_literature_service.api.schemas import CurationStatusSchemaPost
+
+
+def create(db: Session, curation_status: CurationStatusSchemaPost) -> int:
+    """
+
+    :param db:
+    :param curation_status:
+    :return:
+    """
+
+    curation_status_data = jsonable_encoder(curation_status)
+
+    db_obj = create_obj(db, CurationStatusModel, curation_status_data)
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+
+    return db_obj.curation_status_id
+
+
+def destroy(db: Session, curation_status_id: int) -> None:
+    """
+
+    :param db:
+    :param curation_status_id:
+    :return:
+    """
+
+    curation_status = db.query(CurationStatusModel).filter(CurationStatusModel.curation_status_id == curation_status_id).first()
+    if not curation_status:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"CurationStatus with curation_status_id {curation_status_id} not found")
+    db.delete(curation_status)
+    db.commit()
+
+    return None
+
+
+def patch(db: Session, curation_status_id: int, curation_status_update) -> dict:
+    """
+
+    :param db:
+    :param curation_status_id:
+    :param curation_status_update:
+    :return:
+    """
+
+    curation_status_data = jsonable_encoder(curation_status_update)
+    curation_status_db_obj = db.query(CurationStatusModel).filter(CurationStatusModel.curation_status_id == curation_status_id).first()
+    if not curation_status_db_obj:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"CurationStatus with curation_status_id {curation_status_id} not found")
+    res_ref = stripout(db, curation_status_update, non_fatal=True)
+    add(res_ref, curation_status_db_obj)
+
+    for field, value in curation_status_data.items():
+        setattr(curation_status_db_obj, field, value)
+
+    curation_status_db_obj.dateUpdated = datetime.utcnow()
+    db.add(curation_status_db_obj)
+    db.commit()
+
+    return {"message": "updated"}
+
+
+def show(db: Session, curation_status_id: int) -> dict:
+    """
+
+    :param db:
+    :param curation_status_id:
+    :return:
+    """
+
+    curation_status = db.query(CurationStatusModel).filter(CurationStatusModel.curation_status_id == curation_status_id).first()
+    curation_status_data = jsonable_encoder(curation_status)
+
+    if not curation_status:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"CurationStatus with the curation_status_id {curation_status_id} is not available")
+
+    return curation_status_data
---
+++ agr_literature_service/api/schemas/curation_status_schemas.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from pydantic import BaseModel, constr
+
+from agr_literature_service.api.schemas import AuditedObjectModelSchema
+
+
+class CurationStatusSchemaBase(AuditedObjectModelSchema):
+    mod_abbreviation: str
+    reference_curie: str
+
+
+class CurationStatusSchemaPost(CurationStatusSchemaBase):
+    topic: str
+    curation_status: Optional[str]
+    controlled_note: Optional[str]
+    note: Optional[str]
+
+class CurationStatusSchemaUpdate(BaseModel):
+    curation_status: Optional[str]
+    controlled_note: Optional[str]
+    note: Optional[str]
+    date_created: Optional[constr(min_length=1)]  # type: ignore
+    date_updated: Optional[constr(min_length=1)]  # type: ignore
+    created_by: Optional[constr(min_length=1)]  # type: ignore
+    updated_by: Optional[constr(min_length=1)]  # type: ignore
+
+    class Config:
+        orm_mode = True
+        extra = "forbid"
