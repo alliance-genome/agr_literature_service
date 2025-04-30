04e9e73f8a1e45615911784e0c7d4157f26b7b03
ianlongden <ianlongden@morgan.harvard.edu>
2025-04-30 12:26:02 -0400

    more debug

--- agr_literature_service/api/crud/curation_status_crud.py
+++ agr_literature_service/api/crud/curation_status_crud.py
@@ -28,21 +28,24 @@ def create(db: Session, curation_status: CurationStatusSchemaPost) -> int:
     if reference_curie is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="reference_curie not within curation_status_data")
-    # get ref_id from curie
-    print(f"Looking up reference_curie: {reference_curie}")
-    ref_id = db.query(ReferenceModel).filter(ReferenceModel.curie == curation_status_data.reference_curie).one().reference_id
-    print(f"ref_id: {ref_id}")
-    curation_status_data["reference_id"] = ref_id
-    # look up mod
-    abbreviation = curation_status_data.pop("mod_abbreviation", None)
-    mod_id = db.query(ModModel).filter(ModModel.abbreviation == abbreviation).one().mod_id
-    curation_status_data["mod_id"] = mod_id
-    curation_status_data["date_created"] = datetime.now().isoformat()
-    db_obj = CurationStatusModel(**curation_status_data)
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-
+    try:
+        # get ref_id from curie
+        print(f"Looking up reference_curie: {reference_curie}")
+        ref_id = db.query(ReferenceModel).filter(ReferenceModel.curie == curation_status_data.reference_curie).one().reference_id
+        print(f"ref_id: {ref_id}")
+        curation_status_data["reference_id"] = ref_id
+        # look up mod
+        abbreviation = curation_status_data.pop("mod_abbreviation", None)
+        mod_id = db.query(ModModel).filter(ModModel.abbreviation == abbreviation).one().mod_id
+        curation_status_data["mod_id"] = mod_id
+        curation_status_data["date_created"] = datetime.now().isoformat()
+        db_obj = CurationStatusModel(**curation_status_data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+    except Exception as err:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail=f"Error creating curation_status: {err}")
     return db_obj.curation_status_id
 
 
