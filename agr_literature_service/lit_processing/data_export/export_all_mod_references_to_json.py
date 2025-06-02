db19e25c6df4f303b3f4a22e1b574bfad8c4a982
sweng66 <sweng@stanford.edu>
2025-06-01 21:43:23 -0700

    fixed to call the same function for exporting papers for all mods

--- agr_literature_service/lit_processing/data_export/export_all_mod_references_to_json.py
+++ agr_literature_service/lit_processing/data_export/export_all_mod_references_to_json.py
@@ -1,25 +1,42 @@
 import logging
 import time
+from os import environ, makedirs, path
+from dotenv import load_dotenv
+import shutil
 
 from agr_literature_service.lit_processing.data_export.export_single_mod_references_to_json import \
-    dump_data, dump_all_mods_references
+    dump_data
 from agr_literature_service.lit_processing.utils.db_read_utils import get_mod_abbreviations
 
 logging.basicConfig(format='%(message)s')
-log = logging.getLogger()
-log.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def dump_all_data():
 
     for mod in get_mod_abbreviations():
-        log.info("Dumping json data for " + mod)
+        logger.info("Dumping json data for " + mod)
         try:
-            dump_data(mod, None, None)
+            dump_data(mod=mod, email=None, ondemand=None)
         except Exception as e:
-            log.info("Error occurred when dumping json data for " + mod + ": " + str(e))
+            logger.info("Error occurred when dumping json data for " + mod + ": " + str(e))
         time.sleep(5)
-    dump_all_mods_references(email=None, ondemand=False, ui_root_url=None)
+    # dump papers for all mods
+    dump_data(mod=None, email=None, ondemand=None)
+    cleanup_temp_directory()
+
+
+def cleanup_temp_directory():  # pragma: no cover
+    load_dotenv()
+    base_path = environ.get('XML_PATH', "")
+    json_data_path = base_path + "json_data/"
+    try:
+        if path.exists(json_data_path):
+            shutil.rmtree(json_data_path)
+    except OSError as e:
+        logger.info("Error deleting old json reference files: %s" % (e.strerror))
+    makedirs(json_data_path)
 
 
 if __name__ == "__main__":
