d8713164b991d981181ce7fa8e6ec9803c7663fe
sweng66 <sweng@stanford.edu>
2025-07-15 14:28:15 -0700

    updated to work with pydantic version 2

--- agr_literature_service/api/config.py
+++ agr_literature_service/api/config.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from agr_literature_service.api.schemas import EnvStateSchema
 
@@ -8,36 +9,39 @@ from agr_literature_service.api.schemas import EnvStateSchema
 class GlobalConfig(BaseSettings):
     """Global configurations."""
 
-    # This variable will be loaded from the .env file. However, if there is a
-    # shell environment variable having the same name, that will take precedence.
+    # load .env automatically; adjust path or encoding if needed
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+    )
 
-    # the class Field is necessary while defining the global variables
-    ENV_STATE: EnvStateSchema = Field(..., env="ENV_STATE")
-    HOST: Optional[str] = Field(..., env="HOST")
-    PROD_HOST: Optional[str] = Field(..., env="HOST")
-    BUCKET_NAME: str = Field(default="agr-literature", env="BUCKET_NAME")
+    ENV_STATE: EnvStateSchema = Field(..., env='ENV_STATE')
+    HOST: Optional[str] = Field(..., env='HOST')
+    PROD_HOST: Optional[str] = Field(..., env='PROD_HOST')
+    BUCKET_NAME: str = Field('agr-literature', env='BUCKET_NAME')
 
     # AWS Creds
-    AWS_SECRET_ACCESS_KEY: Optional[str] = Field(..., env="AWS_SECRET_ACCESS_KEY")
-    AWS_ACCESS_KEY_ID: Optional[str] = Field(..., env="AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY: Optional[str] = Field(..., env='AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID: Optional[str] = Field(..., env='AWS_ACCESS_KEY_ID')
 
     # environment specific configs
     API_USERNAME: Optional[str] = None
     API_PASSWORD: Optional[str] = None
 
-    OKTA_DOMAIN: str = Field(..., env="OKTA_DOMAIN")
-    OKTA_API_AUDIENCE: str = Field(..., env="OKTA_API_AUDIENCE")
+    OKTA_DOMAIN: str = Field(..., env='OKTA_DOMAIN')
+    OKTA_API_AUDIENCE: str = Field(..., env='OKTA_API_AUDIENCE')
 
-    PSQL_USERNAME: str = Field(..., env="PSQL_USERNAME")
-    PSQL_PASSWORD: str = Field(..., env="PSQL_PASSWORD")
-    PSQL_HOST: str = Field(..., env="PSQL_HOST")
-    PSQL_PORT: str = Field(..., env="PSQL_PORT")
-    PSQL_DATABASE: str = Field(..., env="PSQL_DATABASE")
+    PSQL_USERNAME: str = Field(..., env='PSQL_USERNAME')
+    PSQL_PASSWORD: str = Field(..., env='PSQL_PASSWORD')
+    PSQL_HOST: str = Field(..., env='PSQL_HOST')
+    PSQL_PORT: str = Field(..., env='PSQL_PORT')
+    PSQL_DATABASE: str = Field(..., env='PSQL_DATABASE')
 
-    RESOURCE_DESCRIPTOR_URL: str = Field(..., env="RESOURCE_DESCRIPTOR_URL")
-    ELASTICSEARCH_HOST: str = Field(..., env="ELASTICSEARCH_HOST")
-    ELASTICSEARCH_PORT: str = Field(..., env="ELASTICSEARCH_PORT")
-    ELASTICSEARCH_INDEX: str = Field(..., env="ELASTICSEARCH_INDEX")
+    RESOURCE_DESCRIPTOR_URL: str = Field(..., env='RESOURCE_DESCRIPTOR_URL')
+    ELASTICSEARCH_HOST: str = Field(..., env='ELASTICSEARCH_HOST')
+    ELASTICSEARCH_PORT: str = Field(..., env='ELASTICSEARCH_PORT')
+    ELASTICSEARCH_INDEX: str = Field(..., env='ELASTICSEARCH_INDEX')
 
 
+# instantiate once for application-wide use
 config = GlobalConfig()
