b2e7448f2ce81b4bed0d3ccbe8ba2bff9992a4e9
ianlongden <ianlongden@morgan.harvard.edu>
2025-09-18 16:21:08 -0400

    remove typo

--- agr_literature_service/api/routers/ml_model_router.py
+++ agr_literature_service/api/routers/ml_model_router.py
@@ -113,7 +113,7 @@ def destroy(ml_model_id: int,
             response_model=MLModelSchemaShow,
             status_code=200)
 @router.get('/metadata/{task_type}/{mod_abbreviation}/{topic}',
-            response_model=MLModelSchemaShow,1
+            response_model=MLModelSchemaShow,
             status_code=200)
 @router.get('/metadata/{task_type}/{mod_abbreviation}',
             response_model=MLModelSchemaShow,
