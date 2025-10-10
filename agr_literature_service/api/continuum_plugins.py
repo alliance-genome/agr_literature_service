c6e3396c7067233b37020cf529d52dd16ac4e65b
sweng66 <sweng@stanford.edu>
2025-10-09 19:23:54 -0700

    fixed to work with primary key change for users table

--- agr_literature_service/api/continuum_plugins.py
+++ agr_literature_service/api/continuum_plugins.py
@@ -1,11 +1,14 @@
 from sqlalchemy_continuum.plugins import Plugin
 
-from agr_literature_service.api.user import get_global_user_id
+from agr_literature_service.api.user import get_current_user_pk
 
 
 class UserPlugin(Plugin):
-
     def transaction_args(self, uow, session):
+        """
+        Provide integer users.user_id for the transaction row.
+        Falls back to NULL if no current user is set.
+        """
         return {
-            'user_id': get_global_user_id()
+            'user_id': get_current_user_pk(session)
         }
