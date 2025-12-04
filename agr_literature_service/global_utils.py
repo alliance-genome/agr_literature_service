e448e37e051dd2070ec16edb67e4184eba393e4d
sweng66 <sweng@stanford.edu>
2025-12-04 08:52:47 -0800

    reverted back to use okta for Mati ID generation

--- agr_literature_service/global_utils.py
+++ agr_literature_service/global_utils.py
@@ -5,7 +5,7 @@ import requests
 from sqlalchemy import text
 # from agr_literature_service.lit_processing.utils.sqlalchemy_utils import \
 #    create_postgres_session
-from agr_cognito_py import (
+from fastapi_okta.okta_utils import (
     get_authentication_token,
     generate_headers
 )
