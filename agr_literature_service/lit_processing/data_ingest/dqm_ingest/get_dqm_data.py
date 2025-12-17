cdc5c20c8f7beeccb136d8427f66f47d1b0d4955
Christopher Tabone <christabone@gmail.com>
2025-12-17 16:08:58 +0000

    feat: add S3 download support for FlyBase DQM files
    FlyBase data files moved from public HTTP endpoint to private S3 bucket
    (flybase-alliance-data) for AWS Open Data Program compliance. The
    s3ftp.flybase.org bucket is now part of the AGR Open Data program and
    should only contain public datasets.
    
    - Add download_s3_file() utility function using boto3
    - Update get_dqm_data to use S3 for FlyBase reference/resource files
    - Keep HTTP downloads for other MODs unchanged
    - Uses IAM instance credentials for cross-account S3 access

--- agr_literature_service/lit_processing/data_ingest/dqm_ingest/get_dqm_data.py
+++ agr_literature_service/lit_processing/data_ingest/dqm_ingest/get_dqm_data.py
@@ -7,7 +7,7 @@ from os import environ, makedirs, path, remove, rename
 from dotenv import load_dotenv
 from agr_literature_service.lit_processing.utils.tmp_files_utils import init_tmp_dir
 from agr_literature_service.lit_processing.data_ingest.utils.file_processing_utils import \
-    download_file
+    download_file, download_s3_file
 
 load_dotenv()
 init_tmp_dir()
@@ -34,27 +34,43 @@ def download_dqm_reference_json():  # pragma: no cover
     mod_to_reference_url = {
         "WB": "https://caltech-curation.textpressolab.com/files/pub/agr_upload/pap_papers/agr_wb_literature.json",
         "ZFIN": "https://zfin.org/downloads/ZFIN_1.0.1.4_Reference.json",
-        "FB": "https://s3ftp.flybase.org/flybase/associated_files/alliance/FB_reference.json.gz",
         "XB": "https://ftp.xenbase.org/pub/DataExchange/AGR/XB_REFERENCE.json.zip",
         "MGI": "http://www.informatics.jax.org/downloads/alliance/reference.json.gz",
         "RGD": "https://download.rgd.mcw.edu/data_release/agr/REFERENCE_RGD.json"
+    }
+
+    # FlyBase uses S3 for secure cross-account data transfer
+    mod_to_reference_s3 = {
+        "FB": {"bucket": "flybase-alliance-data", "key": "pub-exports/FB_reference.json.gz"}
     }
 
     for mod in mod_to_reference_url:
         logger.info("Download REFERENCE json file for " + mod)
         download_dqm_file(mod, mod_to_reference_url[mod], "REFERENCE")
 
+    for mod in mod_to_reference_s3:
+        logger.info("Download REFERENCE json file for " + mod + " from S3")
+        download_dqm_s3_file(mod, mod_to_reference_s3[mod], "REFERENCE")
+
 
 def download_dqm_resource_json():  # pragma: no cover
 
     mod_to_resource_url = {
-        "ZFIN": "https://zfin.org/downloads/ZFIN_1.0.1.4_Resource.json",
-        "FB": "https://s3ftp.flybase.org/flybase/associated_files/alliance/FB_resource.json.gz"
+        "ZFIN": "https://zfin.org/downloads/ZFIN_1.0.1.4_Resource.json"
+    }
+
+    # FlyBase uses S3 for secure cross-account data transfer
+    mod_to_resource_s3 = {
+        "FB": {"bucket": "flybase-alliance-data", "key": "pub-exports/FB_resource.json.gz"}
     }
 
     for mod in mod_to_resource_url:
         logger.info("Download RESOURCE json file for " + mod)
         download_dqm_file(mod, mod_to_resource_url[mod], "RESOURCE")
+
+    for mod in mod_to_resource_s3:
+        logger.info("Download RESOURCE json file for " + mod + " from S3")
+        download_dqm_s3_file(mod, mod_to_resource_s3[mod], "RESOURCE")
 
 
 def download_dqm_file(mod, url, datatype):  # pragma: no cover
@@ -84,6 +100,32 @@ def download_dqm_file(mod, url, datatype):  # pragma: no cover
             logger.error(e)
     else:
         download_file(url, json_file)
+
+
+def download_dqm_s3_file(mod, s3_config, datatype):  # pragma: no cover
+    """
+    Download a DQM file from S3 bucket.
+
+    :param mod: MOD abbreviation (e.g., 'FB')
+    :param s3_config: Dict with 'bucket' and 'key' for S3 location
+    :param datatype: Type of data (e.g., 'REFERENCE', 'RESOURCE')
+    """
+    json_file = dqm_json_path + datatype + '_' + mod + '.json'
+    bucket = s3_config['bucket']
+    key = s3_config['key']
+
+    if key.endswith('.gz'):
+        gzip_json_file = json_file + ".gz"
+        download_s3_file(bucket, key, gzip_json_file)
+        try:
+            with gzip.open(gzip_json_file, 'rb') as f_in:
+                with open(json_file, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            remove(gzip_json_file)
+        except Exception as e:
+            logger.error(e)
+    else:
+        download_s3_file(bucket, key, json_file)
 
 
 if __name__ == "__main__":
--- agr_literature_service/lit_processing/data_ingest/utils/file_processing_utils.py
+++ agr_literature_service/lit_processing/data_ingest/utils/file_processing_utils.py
@@ -9,6 +9,8 @@ from os import environ, path, listdir, remove
 import html
 import re
 
+import boto3
+
 from agr_literature_service.lit_processing.utils.tmp_files_utils import init_tmp_dir
 
 init_tmp_dir()
@@ -97,6 +99,22 @@ def download_file(url, file):
             fh.write(data)
     except Exception as e:
         logger.error("Error downloading the file: " + file + ". Error=" + str(e))
+
+
+def download_s3_file(bucket, key, file):
+    """
+    Download a file from S3 bucket using IAM instance credentials.
+
+    :param bucket: S3 bucket name
+    :param key: S3 object key (path within bucket)
+    :param file: Local file path to save to
+    """
+    try:
+        logger.info(f"Downloading s3://{bucket}/{key}")
+        s3 = boto3.client('s3')
+        s3.download_file(bucket, key, file)
+    except Exception as e:
+        logger.error(f"Error downloading S3 file: {bucket}/{key}. Error={str(e)}")
 
 
 def gunzip_file(file_with_path, to_file_dir):
