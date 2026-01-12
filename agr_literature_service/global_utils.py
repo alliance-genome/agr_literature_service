97377d53eb8306e74ab3f4ebce0632d30ba1af40
sweng66 <sweng@stanford.edu>
2026-01-12 09:48:55 -0800

    fixed import to use cognito

--- agr_literature_service/global_utils.py
+++ agr_literature_service/global_utils.py
@@ -5,10 +5,7 @@ import requests
 from sqlalchemy import text
 # from agr_literature_service.lit_processing.utils.sqlalchemy_utils import \
 #    create_postgres_session
-from fastapi_okta.okta_utils import (
-    get_authentication_token,
-    generate_headers
-)
+from agr_cognito_py import get_authentication_token, generate_headers
 
 
 class memoized(object):
