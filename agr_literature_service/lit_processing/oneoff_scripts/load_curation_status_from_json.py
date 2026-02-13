9b3e3fe701027686149efd3121efa5371bc2b80b
ianlongden <ianlongden@morgan.harvard.edu>
2026-02-13 13:33:34 -0500

    redo args

--- agr_literature_service/lit_processing/oneoff_scripts/load_curation_status_from_json.py
+++ agr_literature_service/lit_processing/oneoff_scripts/load_curation_status_from_json.py
@@ -62,6 +62,6 @@ def load_data(datafile):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Load curation status data from a JSON file")
-    parser.add_argument("json_file", help="Path to the JSON file containing curation status data")
+    parser.add_argument("-f", "--json_file", help="Path to the JSON file containing curation status data")
     args = parser.parse_args()
     load_data(args.json_file)
